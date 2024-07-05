49a0af816531bd7a275513a3d69bec5acf2dd461
Adam Hathcock <adamhathcock@users.noreply.github.com>
2024-07-05 15:31:36 +0100

    Add Rhino tests and refactor current ones (#4)
    * add tests with base test class
    
    * update locks
    
    * pin coverage to 4.4.0
    
    * this is a subjob with uses, pass the secrets
    
    * fix secret passing
    
    * try quotes
    
    * try a different glob
    
    * Try simplier glob
    
    * try something else
    
    * try ubuntu for uploads
    
    * update build.sh
    
    * fix target
    
    * update test only target

---
+++ Sdk/Speckle.Testing/MoqTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Moq;
+using NUnit.Framework;
+
+namespace Speckle.Testing;
+
+[ExcludeFromCodeCoverage]
+public abstract class MoqTest
+{
+  [SetUp]
+  public void Setup() => Repository = new(MockBehavior.Strict);
+
+  [TearDown]
+  public void Verify() => Repository.VerifyAll();
+
+  protected MockRepository Repository { get; private set; } = new(MockBehavior.Strict);
+
+  protected Mock<T> Create<T>()
+    where T : class => Repository.Create<T>();
+}
