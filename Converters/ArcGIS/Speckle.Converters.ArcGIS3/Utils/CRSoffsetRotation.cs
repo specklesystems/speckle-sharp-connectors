59e52069c2a5af35cc12bb587a2af93a1bd08932
Adam Hathcock <adam@speckle.systems>
2024-07-18 10:05:10 +0100

    connectors also need docs

--- Converters/ArcGIS/Speckle.Converters.ArcGIS3/Utils/CRSoffsetRotation.cs
+++ Converters/ArcGIS/Speckle.Converters.ArcGIS3/Utils/CRSoffsetRotation.cs
@@ -121,7 +121,6 @@ public struct CRSoffsetRotation
   /// <summary>
   /// Initializes a new instance of <see cref="CRSoffsetRotation"/>.
   /// </summary>
-  /// <param name="spatialReference">SpatialReference to apply offsets and rotation to.</param>
   /// <param name="map">Map to read metadata from.</param>
   public CRSoffsetRotation(Map map)
   {
