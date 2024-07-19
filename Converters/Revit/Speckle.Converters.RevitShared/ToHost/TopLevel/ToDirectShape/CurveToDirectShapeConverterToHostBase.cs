f6ae5ea1fcb6bada66b18d75b3c10c56975bb272
connorivy <43247197+connorivy@users.noreply.github.com>
2024-07-19 07:06:58 -0500

    add curve to direct shape conversions (#37)
    * add curve to direct shape conversions
    
    * rename base converter
    
    * update projitems
    
    * Fix XML comments
    
    ---------
    
    Co-authored-by: Connor Ivy <connor@speckle.systems>
    Co-authored-by: Adam Hathcock <adam@speckle.systems>

---
+++ Converters/Revit/Speckle.Converters.RevitShared/ToHost/TopLevel/ToDirectShape/CurveToDirectShapeConverterToHostBase.cs
@@ -0,0 +1,50 @@
+using Objects;
+using Speckle.Converters.Common.Objects;
+using Speckle.Converters.RevitShared.Helpers;
+using Speckle.Core.Models;
+
+namespace Speckle.Converters.RevitShared.ToHost.TopLevel;
+
+public abstract class CurveToDirectShapeConverterToHostBase<TCurve>
+  : ITypedConverter<TCurve, List<DB.GeometryObject>>,
+    IToHostTopLevelConverter
+  where TCurve : Base, ICurve
+{
+  private readonly IRevitConversionContextStack _contextStack;
+  private readonly ITypedConverter<ICurve, DB.CurveArray> _curveConverter;
+
+  public CurveToDirectShapeConverterToHostBase(
+    IRevitConversionContextStack contextStack,
+    ITypedConverter<ICurve, DB.CurveArray> curveConverter
+  )
+  {
+    _contextStack = contextStack;
+    _curveConverter = curveConverter;
+  }
+
+  public List<DB.GeometryObject> Convert(TCurve target)
+  {
+    var converted = new List<DB.GeometryObject>();
+
+    DB.CurveArray curveArray = _curveConverter.Convert(target);
+    converted.AddRange(curveArray.Cast<DB.Curve>());
+
+    var genericModelCategory = _contextStack.Current.Document.Settings.Categories.get_Item(
+      DB.BuiltInCategory.OST_GenericModel
+    );
+
+    var revitDs = DB.DirectShape.CreateElement(_contextStack.Current.Document, genericModelCategory.Id);
+    if (target is Base speckleObject && speckleObject.applicationId != null)
+    {
+      revitDs.ApplicationId = speckleObject.applicationId;
+    }
+
+    revitDs.ApplicationDataId = Guid.NewGuid().ToString();
+    revitDs.SetShape(converted);
+    revitDs.Name = "CurveAsDirectShape";
+
+    return converted;
+  }
+
+  public object Convert(Base target) => Convert((TCurve)target);
+}
