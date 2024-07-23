7adf524d3a45dc11340918268bdad865fc9e3c82
Dimitrie Stefanescu <didimitrie@gmail.com>
2024-07-23 11:28:09 +0100

    fix(dui3): post dev merge edits

--- Connectors/Autocad/Speckle.Connectors.AutocadShared/HostApp/AutocadGroupUnpacker.cs
+++ Connectors/Autocad/Speckle.Connectors.AutocadShared/HostApp/AutocadGroupUnpacker.cs
@@ -1,50 +1,47 @@
-using System.Collections;
 using Autodesk.AutoCAD.DatabaseServices;
 using Speckle.Connectors.Autocad.Operations.Send;
 using Speckle.Core.Models.Instances;
 
-namespace Speckle.Connectors.Autocad.HostApp
+namespace Speckle.Connectors.Autocad.HostApp;
+
+/// <summary>
+/// Unpacks a selection of atomic objects into their groups. This resource expects to be injected "fresh" in each send operation (scoped lifetime).
+/// </summary>
+public class AutocadGroupUnpacker
 {
-  /// <summary>
-  /// Unpacks a selection of atomic objects into their groups. This resource expects to be injected "fresh" in each send operation (scoped lifetime).
-  /// </summary>
-  public class AutocadGroupUnpacker
+  public List<GroupProxy> UnpackGroups(IEnumerable<AutocadRootObject> autocadObjects)
   {
-    public List<GroupProxy> UnpackGroups(IEnumerable<AutocadRootObject> autocadObjects)
+    var groupProxies = new Dictionary<string, GroupProxy>();
+
+    using var transaction = Application.DocumentManager.CurrentDocument.Database.TransactionManager.StartTransaction();
+
+    foreach (var (dbObject, applicationId) in autocadObjects)
     {
-      var groupProxies = new Dictionary<string, GroupProxy>();
-
-      using var transaction =
-        Application.DocumentManager.CurrentDocument.Database.TransactionManager.StartTransaction();
-
-      foreach (var (dbObject, applicationId) in autocadObjects)
+      var persistentReactorIds = dbObject.GetPersistentReactorIds();
+      foreach (ObjectId oReactorId in persistentReactorIds)
       {
-        var persistentReactorIds = dbObject.GetPersistentReactorIds();
-        foreach (ObjectId oReactorId in persistentReactorIds)
+        var obj = transaction.GetObject(oReactorId, OpenMode.ForRead);
+        if (obj is not Group group)
         {
-          var obj = transaction.GetObject(oReactorId, OpenMode.ForRead);
-          if (obj is not Group group)
+          continue;
+        }
+        var groupAppId = group.Handle.ToString();
+        if (groupProxies.TryGetValue(groupAppId, out GroupProxy groupProxy))
+        {
+          groupProxy.objects.Add(applicationId);
+        }
+        else
+        {
+          groupProxies[groupAppId] = new()
           {
-            continue;
-          }
-          var groupAppId = group.Handle.ToString();
-          if (groupProxies.TryGetValue(groupAppId, out GroupProxy groupProxy))
-          {
-            groupProxy.objects.Add(applicationId);
-          }
-          else
-          {
-            groupProxies[groupAppId] = new()
-            {
-              applicationId = groupAppId,
-              name = group.Name,
-              objects = [applicationId]
-            };
-          }
+            applicationId = groupAppId,
+            name = group.Name,
+            objects = [applicationId]
+          };
         }
       }
-
-      return groupProxies.Values.ToList();
     }
+
+    return groupProxies.Values.ToList();
   }
 }
--- Connectors/Autocad/Speckle.Connectors.AutocadShared/Operations/Receive/AutocadHostObjectBuilder.cs
+++ Connectors/Autocad/Speckle.Connectors.AutocadShared/Operations/Receive/AutocadHostObjectBuilder.cs
@@ -1,4 +1,3 @@
-using System.Collections.ObjectModel;
 using Autodesk.AutoCAD.DatabaseServices;
 using Speckle.Connectors.Autocad.HostApp;
 using Speckle.Connectors.Autocad.HostApp.Extensions;
