b07a61aaef11df47e0c4045269daaf4df13075c2
Claire Kuang <kuang.claire@gmail.com>
2024-07-23 15:31:53 +0100

    feat(rhino): DUI3-142 adds render materials to objects on send and receive (#63)
    * adds rhino material table to send commit
    
    * adds render materials on send
    
    * adds render materials on receive
    
    * Update RhinoHostObjectBuilder.cs
    
    * fixes dictionary trygetvalue errors
    
    * Update RhinoMaterialManager.cs

--- Connectors/Rhino/Speckle.Connectors.Rhino7/Operations/Send/RhinoRootObjectBuilder.cs
+++ Connectors/Rhino/Speckle.Connectors.Rhino7/Operations/Send/RhinoRootObjectBuilder.cs
@@ -24,6 +24,7 @@ public class RhinoRootObjectBuilder : IRootObjectBuilder<RhinoObject>
   private readonly RhinoGroupManager _rhinoGroupManager;
   private readonly IConversionContextStack<RhinoDoc, UnitSystem> _contextStack;
   private readonly RhinoLayerManager _layerManager;
+  private readonly RhinoMaterialManager _materialManager;
 
   public RhinoRootObjectBuilder(
     ISendConversionCache sendConversionCache,
@@ -31,7 +32,8 @@ public class RhinoRootObjectBuilder : IRootObjectBuilder<RhinoObject>
     RhinoLayerManager layerManager,
     RhinoInstanceObjectsManager instanceObjectsManager,
     RhinoGroupManager rhinoGroupManager,
-    IRootToSpeckleConverter rootToSpeckleConverter
+    IRootToSpeckleConverter rootToSpeckleConverter,
+    RhinoMaterialManager materialManager
   )
   {
     _sendConversionCache = sendConversionCache;
@@ -40,6 +42,7 @@ public class RhinoRootObjectBuilder : IRootObjectBuilder<RhinoObject>
     _instanceObjectsManager = instanceObjectsManager;
     _rhinoGroupManager = rhinoGroupManager;
     _rootToSpeckleConverter = rootToSpeckleConverter;
+    _materialManager = materialManager;
   }
 
   public RootObjectBuilderResult Build(
@@ -49,7 +52,7 @@ public class RhinoRootObjectBuilder : IRootObjectBuilder<RhinoObject>
     CancellationToken cancellationToken = default
   )
   {
-    var rootObjectCollection = new Collection { name = _contextStack.Current.Document.Name ?? "Unnamed document" };
+    Collection rootObjectCollection = new() { name = _contextStack.Current.Document.Name ?? "Unnamed document" };
     int count = 0;
 
     var (atomicObjects, instanceProxies, instanceDefinitionProxies) = _instanceObjectsManager.UnpackSelection(
@@ -64,14 +67,30 @@ public class RhinoRootObjectBuilder : IRootObjectBuilder<RhinoObject>
 
     // POC: Handle blocks.
     List<SendConversionResult> results = new(atomicObjects.Count);
+    List<Objects.Other.RenderMaterial> renderMaterials = new();
     foreach (RhinoObject rhinoObject in atomicObjects)
     {
       cancellationToken.ThrowIfCancellationRequested();
-      // RhinoDoc.ActiveDoc.Layers
-      var layer = _contextStack.Current.Document.Layers[rhinoObject.Attributes.LayerIndex];
 
-      var collectionHost = _layerManager.GetHostObjectCollection(layer, rootObjectCollection);
-      var applicationId = rhinoObject.Id.ToString();
+      // handle render material
+      // TODO: need to add render material to layers
+      // POC: need to check object changed event captures material changes for object invalidation
+      // POC: we are adding the renderMaterialId to every atomic object, even if their material inheritence is set to ByLayer or ByParent
+      // POC: this means this material inheritence will not be preserved on receive
+      Rhino.DocObjects.Material material = _contextStack.Current.Document.Materials[
+        rhinoObject.Attributes.MaterialIndex
+      ];
+      if (!_materialManager.Contains(material))
+      {
+        Objects.Other.RenderMaterial speckleRenderMaterial = _materialManager.CreateSpeckleRenderMaterial(material);
+        renderMaterials.Add(speckleRenderMaterial);
+      }
+
+      // handle layer
+      Rhino.DocObjects.Layer layer = _contextStack.Current.Document.Layers[rhinoObject.Attributes.LayerIndex];
+      Collection collectionHost = _layerManager.GetHostObjectCollection(layer, rootObjectCollection);
+
+      string applicationId = rhinoObject.Id.ToString();
 
       try
       {
@@ -91,6 +110,8 @@ public class RhinoRootObjectBuilder : IRootObjectBuilder<RhinoObject>
         {
           converted = _rootToSpeckleConverter.Convert(rhinoObject);
           converted.applicationId = applicationId;
+          converted["renderMaterialId"] = material.Id.ToString();
+          ;
         }
 
         // add to host
@@ -110,7 +131,29 @@ public class RhinoRootObjectBuilder : IRootObjectBuilder<RhinoObject>
       // Thread.Sleep(550);
     }
 
+    // POC: Add render materials to root collection
+    rootObjectCollection["renderMaterials"] = renderMaterials;
+
     // 5. profit
     return new(rootObjectCollection, results);
+  }
+
+  private void AddRenderMaterialsToRootCollection(Collection root)
+  {
+    List<Base> convertedMaterials = new();
+    foreach (Material rhinoMaterial in _contextStack.Current.Document.Materials)
+    {
+      string applicationId = rhinoMaterial.Id.ToString();
+      Base? converted = _rootToSpeckleConverter.Convert(rhinoMaterial);
+      if (converted is null)
+      {
+        // TODO: report
+        continue;
+      }
+
+      converted.applicationId = applicationId;
+    }
+
+    root["renderMaterials"] = convertedMaterials;
   }
 }
