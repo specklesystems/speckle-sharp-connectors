8b1f5277eeb4dc10e8ad2b3c661ef1b0059ba884
Adam Hathcock <adam@speckle.systems>
2024-08-01 10:15:00 +0100

    Adjust code to build

--- Sdk/Speckle.Connectors.Utils/ContainerRegistration.cs
+++ Sdk/Speckle.Connectors.Utils/ContainerRegistration.cs
@@ -1,12 +1,76 @@
 ﻿using Autofac;
 using Microsoft.Extensions.Logging;
-using Serilog;
 using Speckle.Autofac.DependencyInjection;
 using Speckle.Connectors.Utils.Cancellation;
 using Speckle.Connectors.Utils.Operations;
-using Speckle.Core.Logging;
+using Speckle.Logging;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Speckle.Connectors.Utils;
+
+#pragma warning disable CA1063
+public class SpeckleLoggerFactory : ILoggerFactory
+#pragma warning restore CA1063
+{
+#pragma warning disable CA1816
+#pragma warning disable CA1063
+  public void Dispose() { }
+#pragma warning restore CA1063
+#pragma warning restore CA1816
+
+  public ILogger CreateLogger(string categoryName) => new SpeckleLogger(SpeckleLog.Create(categoryName));
+
+  public void AddProvider(ILoggerProvider provider) => throw new NotImplementedException();
+}
+
+public class SpeckleLogger : ILogger
+{
+  private readonly ISpeckleLogger _logger;
+
+  public SpeckleLogger(ISpeckleLogger logger)
+  {
+    _logger = logger;
+  }
+
+  public void Log<TState>(
+    LogLevel logLevel,
+    EventId eventId,
+    TState state,
+    Exception? exception,
+    Func<TState, Exception, string> formatter
+  )
+  {
+    switch (logLevel)
+    {
+      case LogLevel.Critical:
+        _logger.Fatal(exception!, formatter(state, exception!));
+        break;
+      case LogLevel.Trace:
+        _logger.Debug(exception!, formatter(state, exception!));
+        break;
+      case LogLevel.Debug:
+        _logger.Debug(exception!, formatter(state, exception!));
+        break;
+      case LogLevel.Information:
+        _logger.Information(exception!, formatter(state, exception!));
+        break;
+      case LogLevel.Warning:
+        _logger.Warning(exception!, formatter(state, exception!));
+        break;
+      case LogLevel.Error:
+        _logger.Error(exception!, formatter(state, exception!));
+        break;
+      case LogLevel.None:
+      default:
+        throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
+    }
+  }
+
+  public bool IsEnabled(LogLevel logLevel) => true;
+
+  public IDisposable BeginScope<TState>(TState state)
+    where TState : notnull => throw new NotImplementedException();
+}
 
 public static class ContainerRegistration
 {
@@ -18,13 +82,7 @@ public static class ContainerRegistration
     builder.AddSingleton<AccountService>();
     builder.ScanAssemblyOfType<SendHelper>();
 
-    //TODO: Logger will likely be removed from Core, we'll plan to figure out the config later...
-    var serilogLogger = SpeckleLog.Logger;
-
-    ILoggerFactory loggerFactory = new LoggerFactory().AddSerilog(serilogLogger);
-    builder.ContainerBuilder.Register(_ => loggerFactory).As<ILoggerFactory>().SingleInstance().AutoActivate();
-
+    builder.AddSingleton<ILoggerFactory>(new SpeckleLoggerFactory());
     builder.ContainerBuilder.RegisterGeneric(typeof(Logger<>)).As(typeof(ILogger<>)).SingleInstance();
-    builder.AddSingleton(loggerFactory);
   }
 }
