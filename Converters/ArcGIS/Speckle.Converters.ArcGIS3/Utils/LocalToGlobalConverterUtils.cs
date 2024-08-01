440d17388122b4d4acef148a4745e6a88c565745
KatKatKateryna <kateryna@speckle.systems>
2024-08-01 14:13:35 +0100

    rebuild PR

--- Converters/ArcGIS/Speckle.Converters.ArcGIS3/Utils/LocalToGlobalConverterUtils.cs
+++ Converters/ArcGIS/Speckle.Converters.ArcGIS3/Utils/LocalToGlobalConverterUtils.cs
@@ -1,6 +1,7 @@
-﻿using Speckle.Core.Logging;
+﻿using System.Collections;
+using Objects;
+using Speckle.Converters.Common;
 using Speckle.Core.Models;
-using Speckle.Core.Models.Extensions;
 using Speckle.DoubleNumerics;
 using Speckle.InterfaceGenerator;
 
@@ -28,102 +29,43 @@ public class LocalToGlobalConverterUtils : ILocalToGlobalConverterUtils
       return atomicObject;
     }
 
-    // This is a temp hack. We would add transformations to conversions later instead try to copy objects like this.
-    // Jedd also has opinions on this.
-    Base newObject = Core.Api.Operations.Deserialize(Core.Api.Operations.Serialize(atomicObject));
+    List<Objects.Other.Transform> transforms = matrix.Select(x => new Objects.Other.Transform(x, "none")).ToList();
 
-    if (newObject.TryGetDisplayValue() is IReadOnlyList<Base> listVals)
+    if (atomicObject is ITransformable c)
     {
-      if (listVals.ToList().FindAll(x => x is not SOG.Mesh).Count == 0)
+      foreach (var transform in transforms)
       {
-        return TransformObjWithDisplayValues(newObject, matrix);
+        c.TransformTo(transform, out ITransformable newObj);
+        c = newObj;
       }
-      throw new SpeckleException(
-        $"Display Values of types '{listVals.ToList().FindAll(x => x is not SOG.Mesh).Select(y => y.speckle_type).Distinct().ToList()}' for {newObject.speckle_type} are not supported for local to global coordinate transformation"
-      );
+
+      if (c is not Base)
+      {
+        throw new SpeckleConversionException(
+          $"Blocks transformation of type {atomicObject.speckle_type} did not return a valid object"
+        );
+      }
+
+      atomicObject = (Base)c;
+
+      // .TransformTo only transfers typed properties, we need to add back the dynamic ones:
+      foreach (var prop in atomicObject.GetMembers(DynamicBaseMemberType.Dynamic))
+      {
+        if (prop.Value is not Base)
+        {
+          atomicObject[prop.Key] = prop.Value;
+        }
+        else
+        {
+          // TODO: add more granular warnings if needed that the property was skipped for blocks
+        }
+      }
+
+      return atomicObject;
     }
-    // TODO: here or preferably in converters elegantly!
-    // else if (newObject is SOG.Polyline polyline)
-    // {
-    //
-    // }
-    // else if (newObject is SOG.Line line)
-    // {
-    //
-    // }
-    // else if (newObject is SOG.Point point)
-    // {
-    //
-    // }
-    if (newObject is SOG.Mesh mesh)
-    {
-      return TransformMesh(mesh, matrix);
-    }
-    throw new SpeckleException(
-      $"{newObject.speckle_type} is not supported for local to global coordinate transformation"
+
+    throw new SpeckleConversionException(
+      $"{atomicObject.speckle_type} is not supported for local to global coordinate transformation"
     );
-  }
-
-  private Base TransformObjWithDisplayValues(Base baseObj, List<Matrix4x4> matrix)
-  {
-    Base newObject = Core.Api.Operations.Deserialize(Core.Api.Operations.Serialize(baseObj));
-    List<SOG.Mesh> newDisplayValue = new();
-
-    var displayValue = newObject.TryGetDisplayValue();
-    if (displayValue is null) // will not happen due to the check in "TransformObjects"
-    {
-      throw new SpeckleException($"{newObject.speckle_type} blocks contains no display value");
-    }
-
-    foreach (Base displayVal in displayValue)
-    {
-      if (displayVal is SOG.Mesh displayMesh)
-      {
-        var newMesh = TransformMesh(displayMesh, matrix);
-        newDisplayValue.Add(newMesh);
-      }
-      else // will not happen due to the check in "TransformObjects"
-      {
-        throw new SpeckleException($"Blocks containing {baseObj.speckle_type} are not supported");
-      }
-    }
-
-    newObject["displayValue"] = newDisplayValue;
-    return newObject;
-  }
-
-  private SOG.Mesh TransformMesh(SOG.Mesh displayMesh, List<Matrix4x4> matrix)
-  {
-    List<List<double>> oldVertices = new();
-    for (int i = 0; i < displayMesh.vertices.Count; i += 3)
-    {
-      List<double> group = new();
-      for (int j = 0; j < 3 && i + j < displayMesh.vertices.Count; j++)
-      {
-        group.Add(displayMesh.vertices[i + j]);
-      }
-      oldVertices.Add(group);
-    }
-
-    List<double> newVertices = new();
-    foreach (List<double> vertex in oldVertices)
-    {
-      var ptVector = new Vector3(vertex[0], vertex[1], vertex[2]);
-
-      foreach (var matr in matrix)
-      {
-        ptVector = TransformPt(ptVector, matr);
-      }
-      newVertices.AddRange([ptVector.X, ptVector.Y, ptVector.Z]);
-    }
-
-    SOG.Mesh newMesh =
-      new()
-      {
-        vertices = newVertices,
-        faces = new List<int>(displayMesh.faces),
-        colors = new List<int>(displayMesh.colors)
-      };
-    return newMesh;
   }
 }
