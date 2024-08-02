8b0c63627d680c5063f17252d269f70007d2baa5
Adam Hathcock <adam@speckle.systems>
2024-08-02 11:09:36 +0100

    oops, two classes

--- Sdk/Speckle.Connectors.Utils/Common/SpeckleLoggerFactory.cs
+++ Sdk/Speckle.Connectors.Utils/Common/SpeckleLoggerFactory.cs
@@ -3,65 +3,11 @@ using Speckle.Logging;
 
 namespace Speckle.Connectors.Utils.Common;
 
-#pragma warning disable CA1063
-public class SpeckleLoggerFactory : ILoggerFactory
-#pragma warning restore CA1063
+public sealed class SpeckleLoggerFactory : ILoggerFactory
 {
-#pragma warning disable CA1816
-#pragma warning disable CA1063
   public void Dispose() { }
-#pragma warning restore CA1063
-#pragma warning restore CA1816
 
   public ILogger CreateLogger(string categoryName) => new SpeckleLogger(SpeckleLog.Create(categoryName));
 
   public void AddProvider(ILoggerProvider provider) => throw new NotImplementedException();
-}
-
-public class SpeckleLogger : ILogger
-{
-  private readonly ISpeckleLogger _logger;
-
-  public SpeckleLogger(ISpeckleLogger logger)
-  {
-    _logger = logger;
-  }
-
-  public void Log<TState>(
-    LogLevel logLevel,
-    EventId eventId,
-    TState state,
-    Exception exception,
-    Func<TState, Exception, string> formatter
-  )
-  {
-    switch (logLevel)
-    {
-      case LogLevel.Critical:
-        _logger.Write(SpeckleLogLevel.Fatal, exception, formatter(state, exception));
-        break;
-      case LogLevel.Trace:
-        _logger.Write(SpeckleLogLevel.Verbose, exception, formatter(state, exception));
-        break;
-      case LogLevel.Debug:
-        _logger.Write(SpeckleLogLevel.Debug, formatter(state, exception));
-        break;
-      case LogLevel.Information:
-        _logger.Write(SpeckleLogLevel.Information, exception, formatter(state, exception));
-        break;
-      case LogLevel.Warning:
-        _logger.Write(SpeckleLogLevel.Warning, exception, formatter(state, exception));
-        break;
-      case LogLevel.Error:
-        _logger.Write(SpeckleLogLevel.Error, exception, formatter(state, exception));
-        break;
-      case LogLevel.None:
-      default:
-        throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
-    }
-  }
-
-  public bool IsEnabled(LogLevel logLevel) => true;
-
-  public IDisposable BeginScope<TState>(TState state) => throw new NotImplementedException();
 }
