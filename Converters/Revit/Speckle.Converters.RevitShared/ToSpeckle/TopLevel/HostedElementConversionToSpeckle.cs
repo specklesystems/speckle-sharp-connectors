99f29326d151d918ad5d39559e8d175275e71c68
Dimitrie Stefanescu <didimitrie@gmail.com>
2024-08-13 12:16:37 +0100

    chore: makes HostedElementConversionToSpeckle fully obsolete

--- Converters/Revit/Speckle.Converters.RevitShared/ToSpeckle/TopLevel/HostedElementConversionToSpeckle.cs
+++ Converters/Revit/Speckle.Converters.RevitShared/ToSpeckle/TopLevel/HostedElementConversionToSpeckle.cs
@@ -5,7 +5,10 @@ using Speckle.Sdk.Models;
 
 namespace Speckle.Converters.RevitShared.ToSpeckle;
 
-[Obsolete("Do not use while as we're rethinking hosted element relationships. This class will most likely go away.")]
+[Obsolete(
+  "Do not use while as we're rethinking hosted element relationships. This class will most likely go away.",
+  true
+)]
 public class HostedElementConversionToSpeckle
 {
   private readonly IRootToSpeckleConverter _converter;
