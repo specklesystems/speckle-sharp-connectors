cd3944cbb8ce33ea63186c31afc7540fc3d36cd6
Adam Hathcock <adamhathcock@users.noreply.github.com>
2024-08-15 13:07:30 +0100

    Adam/cxpla 16 dry alignment of connector settings for hostapp slugs (#144)
    * Revit redone
    
    * ArcGIS and Rhino
    
    * Remove settings
    
    * Fixed warnings

--- Sdk/Speckle.Connectors.Utils/Connector.cs
+++ Sdk/Speckle.Connectors.Utils/Connector.cs
@@ -8,8 +8,21 @@ namespace Speckle.Connectors.Utils;
 
 public static class Connector
 {
+  public static readonly string TabName = "Speckle";
+  public static readonly string TabTitle = "Speckle New UI";
+
+  public static HostAppVersion Version { get; private set; } = HostAppVersion.v3;
+  public static string VersionString { get; private set; } = string.Empty;
+  public static string Name => HostApp.Name;
+  public static string Slug => HostApp.Slug;
+
+  public static HostApplication HostApp { get; private set; }
+
   public static IDisposable? Initialize(HostApplication application, HostAppVersion version)
   {
+    Version = version;
+    VersionString = HostApplications.GetVersion(version);
+    HostApp = application;
     TypeLoader.Initialize(typeof(Base).Assembly, typeof(Point).Assembly);
 #if DEBUG || LOCAL
     var config = new SpeckleConfiguration(
