24adaee4dfe5e4b15287b346031b8cdf110f33c5
Claire Kuang <kuang.claire@gmail.com>
2024-08-16 12:03:25 +0100

    removes unused method

--- Connectors/ArcGIS/Speckle.Connectors.ArcGIS3/HostApp/ArcGISColorManager.cs
+++ Connectors/ArcGIS/Speckle.Connectors.ArcGIS3/HostApp/ArcGISColorManager.cs
@@ -395,46 +395,6 @@ public class ArcGISColorManager
     return RbgToInt(255, red, green, blue);
   }
 
-  /// <summary>
-  /// Converts HSV color values to RGB
-  /// </summary>
-  /// <param name="h">0 - 360</param>
-  /// <param name="s">0 - 100</param>
-  /// <param name="v">0 - 100</param>
-  /// <param name="r">0 - 255</param>
-  /// <param name="g">0 - 255</param>
-  /// <param name="b">0 - 255</param>
-  private int HSVToRGB(int h, int s, int v, out int r, out int g, out int b)
-  {
-    var rgb = new int[3];
-
-    var baseColor = (h + 60) % 360 / 120;
-    var shift = (h + 60) % 360 - (120 * baseColor + 60);
-    var secondaryColor = (baseColor + (shift >= 0 ? 1 : -1) + 3) % 3;
-
-    //Setting Hue
-    rgb[baseColor] = 255;
-    rgb[secondaryColor] = (int)((Math.Abs(shift) / 60.0f) * 255.0f);
-
-    //Setting Saturation
-    for (var i = 0; i < 3; i++)
-    {
-      rgb[i] += (int)((255 - rgb[i]) * ((100 - s) / 100.0f));
-    }
-
-    //Setting Value
-    for (var i = 0; i < 3; i++)
-    {
-      rgb[i] -= (int)(rgb[i] * (100 - v) / 100.0f);
-    }
-
-    r = rgb[0];
-    g = rgb[1];
-    b = rgb[2];
-
-    return RbgToInt(255, r, g, b);
-  }
-
   private bool TryGetUniqueRendererColor(
     CIMUniqueValueRenderer uniqueRenderer,
     Row row,
