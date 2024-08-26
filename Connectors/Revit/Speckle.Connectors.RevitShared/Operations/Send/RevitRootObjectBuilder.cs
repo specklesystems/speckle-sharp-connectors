10cefffaf16f8fd65efc90dd00f336ae0e7548f4
oguzhankoral <oguzhankoral@gmail.com>
2024-08-26 16:13:36 +0300

    Fail fast if all conversion status is ERROR

--- Connectors/Revit/Speckle.Connectors.RevitShared/Operations/Send/RevitRootObjectBuilder.cs
+++ Connectors/Revit/Speckle.Connectors.RevitShared/Operations/Send/RevitRootObjectBuilder.cs
@@ -116,6 +116,11 @@ public class RevitRootObjectBuilder : IRootObjectBuilder<ElementId>
         onOperationProgressed?.Invoke("Converting", (double)++countProgress / atomicObjects.Count);
       }
 
+      if (results.All(x => x.Status == Status.ERROR))
+      {
+        throw new SpeckleConversionException("Failed to convert all objects."); // fail fast instead creating empty commit! It will appear as model card error with red color.
+      }
+
       var idsAndSubElementIds = _elementUnpacker.GetElementsAndSubelementIdsFromAtomicObjects(atomicObjects);
       var materialProxies = _conversionContextStack.RenderMaterialProxyCache.GetRenderMaterialProxyListForObjects(
         idsAndSubElementIds
