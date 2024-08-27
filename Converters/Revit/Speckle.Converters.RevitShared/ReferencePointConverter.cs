57bea32f6e6f56928f19e42b86d930870d1c5c27
Claire Kuang <kuang.claire@gmail.com>
2024-08-27 10:26:28 +0100

    adds reference point transform to xyz, and refactors

--- Converters/Revit/Speckle.Converters.RevitShared/ReferencePointConverter.cs
+++ Converters/Revit/Speckle.Converters.RevitShared/ReferencePointConverter.cs
@@ -1,17 +1,14 @@
-using System.Diagnostics.CodeAnalysis;
-using Autodesk.Revit.DB;
-using Speckle.Converters.RevitShared.Helpers;
 using Speckle.Sdk.Common;
 
-namespace Speckle.Converters.RevitShared;
+namespace Speckle.Converters.RevitShared.Helpers;
 
-// POC: this could perhaps becomes some RevitDocumentService but also...
-// This reference point feature needs review. We could do with knowing whether this feature is widely used.
-// there's also some bogus disposal happening
-// https://spockle.atlassian.net/browse/CNX-9357
+/// <summary>
+/// POC: reference point functionality needs to be revisited (we are currently baking in these transforms into all geometry using the point and vector converters, and losing the transform).
+/// This converter stores the selected reference point setting value and provides a method to get out the transform for this reference point.
+/// POC: uses context stack current doc on instantiation and assumes context stack doc won't change. Linked docs will NOT be supported atm.
+/// </summary>
 public class ReferencePointConverter : IReferencePointConverter
 {
-  // POC: probably not the best place for this
   private const string REFPOINT_INTERNAL_ORIGIN = "Internal Origin (default)";
   private const string REFPOINT_PROJECT_BASE = "Project Base";
   private const string REFPOINT_SURVEY = "Survey";
@@ -19,92 +16,78 @@ public class ReferencePointConverter : IReferencePointConverter
   private readonly RevitConversionSettings _revitSettings;
   private readonly IRevitConversionContextStack _contextStack;
 
-  private readonly Dictionary<string, DB.Transform> _docTransforms = new();
+  private DB.Transform? Transform { get; }
 
-  public ReferencePointConverter(IRevitConversionContextStack contextStack, RevitConversionSettings revitSettings)
+  public ReferencePointConverter(RevitConversionSettings revitSettings, IRevitConversionContextStack contextStack)
   {
-    _contextStack = contextStack;
     _revitSettings = revitSettings;
+    _contextStack = contextStack;
+    Transform = GetReferencePointTransform();
   }
 
-  // POC: the original allowed for the document to be passed in
-  // if required, we would probably need to push the stack with a new document if the
-  // doc can change during the lifeycycle of the conversions. This may need some looking into
-  public DB.XYZ ConvertToExternalCoordindates(DB.XYZ p, bool isPoint)
+  public DB.XYZ ConvertToExternalCoordinates(DB.XYZ p, bool isPoint)
   {
-    var rpt = GetDocReferencePointTransform(_contextStack.Current.Document);
-    return isPoint ? rpt.Inverse.OfPoint(p) : rpt.Inverse.OfVector(p);
-  }
-
-  /// <summary>
-  /// For importing in Revit, moves and rotates a point according to this document BasePoint
-  /// </summary>
-  /// <param name="p"></param>
-  /// <returns></returns>
-  public XYZ ToInternalCoordinates(XYZ p, bool isPoint)
-  {
-    var rpt = GetDocReferencePointTransform(_contextStack.Current.Document);
-    return isPoint ? rpt.OfPoint(p) : rpt.OfVector(p);
-  }
-
-  // POC: this might be better in some RevitDocumentService
-  // we could probably return that instance instead of the Doc from the context, maybe...
-  public DB.Transform GetDocReferencePointTransform(DB.Document doc)
-  {
-    //linked files are always saved to disc and will have a path name
-    //if the current doc is unsaved it will not, but then it'll be the only one :)
-    var id = doc.PathName;
-
-    if (!_docTransforms.TryGetValue(id, out DB.Transform? transform))
+    if (Transform is DB.Transform transform)
     {
-      // get from settings
-      var referencePointSetting = _revitSettings.TryGetSettingString("reference-point", out string? value)
-        ? value.NotNull()
-        : string.Empty;
-      transform = GetReferencePointTransform(referencePointSetting);
-      _docTransforms[id] = transform;
+      return isPoint ? transform.Inverse.OfPoint(p) : transform.Inverse.OfVector(p);
     }
 
-    return transform;
+    return p;
   }
 
-  [SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope")]
-  public DB.Transform GetReferencePointTransform(string referencePointSetting)
+  public DB.XYZ ConvertToInternalCoordinates(DB.XYZ p, bool isPoint)
   {
+    if (Transform is DB.Transform transform)
+    {
+      return isPoint ? transform.OfPoint(p) : transform.OfVector(p);
+    }
+
+    return p;
+  }
+
+  private DB.Transform? GetReferencePointTransform()
+  {
+    string referencePointSetting = _revitSettings.TryGetSettingString("reference-point", out string? value)
+      ? value.NotNull()
+      : string.Empty;
+
+    DB.Transform? referencePointTransform = null;
+
     // first get the main doc base points and reference setting transform
-    var referencePointTransform = DB.Transform.Identity;
+    using DB.FilteredElementCollector filteredElementCollector = new(_contextStack.Current.Document);
+    var points = filteredElementCollector.OfClass(typeof(DB.BasePoint)).Cast<DB.BasePoint>().ToList();
+    DB.BasePoint? projectPoint = points.FirstOrDefault(o => !o.IsShared);
+    DB.BasePoint? surveyPoint = points.FirstOrDefault(o => o.IsShared);
 
-    // POC: bogus disposal below
-    var points = new DB.FilteredElementCollector(_contextStack.Current.Document)
-      .OfClass(typeof(DB.BasePoint))
-      .Cast<DB.BasePoint>()
-      .ToList();
-
-    var projectPoint = points.FirstOrDefault(o => !o.IsShared).NotNull();
-    var surveyPoint = points.FirstOrDefault(o => o.IsShared).NotNull();
-
-    // POC: it's not clear what support is needed for this
     switch (referencePointSetting)
     {
-      case REFPOINT_PROJECT_BASE: // note that the project base (ui) rotation is registered on the survey pt, not on the base point
-        referencePointTransform = DB.Transform.CreateTranslation(projectPoint.Position);
+      // note that the project base (ui) rotation is registered on the survey pt, not on the base point
+      case REFPOINT_PROJECT_BASE:
+        if (projectPoint is not null)
+        {
+          referencePointTransform = DB.Transform.CreateTranslation(projectPoint.Position);
+          // TODO: return error, couldn't retrieve project point
+        }
         break;
 
       case REFPOINT_SURVEY:
         // note that the project base (ui) rotation is registered on the survey pt, not on the base point
         // retrieve the survey point rotation from the project point
+        if (surveyPoint is not null)
+        {
+          // POC: should a null angle resolve to 0?
+          var angle = surveyPoint.get_Parameter(DB.BuiltInParameter.BASEPOINT_ANGLETON_PARAM)?.AsDouble() ?? 0;
 
-        // POC: should a null angle resolve to 0?
-        var angle = projectPoint.get_Parameter(DB.BuiltInParameter.BASEPOINT_ANGLETON_PARAM)?.AsDouble() ?? 0;
-
-        // POC: following disposed incorrectly or early or maybe a false negative?
-        referencePointTransform = DB
-          .Transform.CreateTranslation(surveyPoint.Position)
-          .Multiply(DB.Transform.CreateRotation(DB.XYZ.BasisZ, angle));
-
+          // POC: following disposed incorrectly or early or maybe a false negative?
+          using DB.Transform translation = DB.Transform.CreateTranslation(surveyPoint.Position);
+          referencePointTransform = translation.Multiply(DB.Transform.CreateRotation(DB.XYZ.BasisZ, angle));
+        }
         break;
 
       case REFPOINT_INTERNAL_ORIGIN:
+        break;
+
+      default:
         break;
     }
 
