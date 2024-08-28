2d345e606b18397ca49971c5482083a206c4285e
Oğuzhan Koral <45078678+oguzhankoral@users.noreply.github.com>
2024-08-28 15:00:11 +0300

    Fix(revit): Reference to CefSharp.Wpf.NETCore for Revit 2025 (#204)
    * Reference to CefSharp.Wpf.NETCore for revit 2025
    
    * Remove commented out code

--- Connectors/Revit/Speckle.Connectors.RevitShared/Plugin/RevitCefPlugin.cs
+++ Connectors/Revit/Speckle.Connectors.RevitShared/Plugin/RevitCefPlugin.cs
@@ -1,4 +1,3 @@
-#if !REVIT2025
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
@@ -93,7 +92,7 @@ internal sealed class RevitCefPlugin : IRevitPlugin
     dui3Button.SetContextualHelp(new ContextualHelp(ContextualHelpType.Url, "https://speckle.systems"));
   }
 
-  private void OnApplicationInitialized(object sender, Autodesk.Revit.DB.Events.ApplicationInitializedEventArgs e)
+  private void OnApplicationInitialized(object? sender, Autodesk.Revit.DB.Events.ApplicationInitializedEventArgs e)
   {
     var uiApplication = new UIApplication(sender as Application);
     _revitContext.UIApplication = uiApplication;
@@ -130,12 +129,16 @@ internal sealed class RevitCefPlugin : IRevitPlugin
       {
         IBridge bridge = binding.Parent;
 
+#if REVIT2025_OR_GREATER
+        _cefSharpPanel.Browser.JavascriptObjectRepository.Register(bridge.FrontendBoundName, bridge, _bindingOptions);
+#else
         _cefSharpPanel.Browser.JavascriptObjectRepository.Register(
           bridge.FrontendBoundName,
           bridge,
           true,
           _bindingOptions
         );
+#endif
       }
     };
   }
@@ -171,4 +174,3 @@ internal sealed class RevitCefPlugin : IRevitPlugin
     return null;
   }
 }
-#endif
