f1e065a77a265f8281df2c6801f27b35b2c66cf4
oguzhankoral <oguzhankoral@gmail.com>
2024-09-11 23:06:35 +0300

    Fun 101

---
+++ Speckle.Connector.Tekla2024/Plugin/TeklaPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Reflection;
+using Speckle.Autofac.DependencyInjection;
+using Speckle.Sdk.Common;
+using Tekla.Structures.Plugins;
+
+namespace Speckle.Connector.Tekla2024.Plugin;
+
+[Plugin("Speckle.Connectors.Tekla")]
+[PluginUserInterface("Speckle.Connector.Tekla2024.SpeckleTeklaPanelHost")]
+public class TeklaPlugin : PluginBase
+{
+  public static SpeckleContainer Container { get; private set; }
+#pragma warning disable IDE1006
+  public override bool Run(List<InputDefinition> Input)
+  {
+    var builder = SpeckleContainerBuilder.CreateInstance();
+    Container = builder
+      .LoadAutofacModules(
+        Assembly.GetExecutingAssembly(),
+        [Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).NotNull()]
+      )
+      .Build();
+    return true;
+  }
+#pragma warning restore IDE1006
+
+  public override List<InputDefinition> DefineInput() => new();
+}
---
+++ Speckle.Connector.Tekla2024/SpeckleTeklaPanelHost.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms.Integration;
+using Speckle.Connector.Tekla2024.Plugin;
+using Speckle.Connectors.DUI.WebView;
+using Tekla.Structures.Dialog;
+using Tekla.Structures.Model;
+
+namespace Speckle.Connector.Tekla2024;
+
+public class SpeckleTeklaPanelHost : PluginFormBase
+{
+  private ElementHost Host { get; }
+  public Model Model { get; private set; }
+
+  public SpeckleTeklaPanelHost()
+  {
+    Model = new Model(); // don't know what is this..
+    var webview = TeklaPlugin.Container.Resolve<DUI3ControlWebView>();
+    Host = new() { Child = webview };
+    Controls.Add(Host);
+  }
+}
