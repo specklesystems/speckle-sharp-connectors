6ab98c5cea6f349a6e9c80c7b10fa962852bf27e
oguzhankoral <oguzhankoral@gmail.com>
2024-09-25 16:37:21 +0300

    Remove old notes

--- Converters/Revit/Speckle.Converters.RevitShared/Helpers/RevitToSpeckleCacheSingleton.cs
+++ Converters/Revit/Speckle.Converters.RevitShared/Helpers/RevitToSpeckleCacheSingleton.cs
@@ -8,14 +8,9 @@ namespace Speckle.Converters.RevitShared.Helpers;
 /// Lifetime of this singleton should be per document.
 /// </para>
 /// <para>
-/// Persistent cache (across conversions) for all generated render material proxies.
-/// This cache stores a list of render material proxies per element id, and provides a method to get out the merged render material proxy list from a set of object ids for setting on the root commit object post conversion phase.
-/// </para>
-/// <para>
 /// Why is this needed? Because two reasons: send caching bypasses converter and revit conversions typically generate multiple display values per element.
 /// Ask dim for more and he might start crying.
 /// </para>
-/// TODO: this dude needs to be split into single responsability (render materials and material quantities), and removed from the context - as it's not needed for it to be there. It can be DI'ed as appropriate (see ParameterDefinitionHandler)
 /// </summary>
 public class RevitToSpeckleCacheSingleton
 {
