b086d16faf93dd1080e700a2171c2d08a588669a
Oğuzhan Koral <45078678+oguzhankoral@users.noreply.github.com>
2024-09-26 13:10:06 +0300

    Feat(tekla): plugin structure and bind UI (#282)
    * Fun 101
    
    * Update package.lock for tekla project
    
    * Align tekla with new DI
    
    * More alignment
    
    * Add launch setting
    
    * Message for init
    
    * Move project to connectors folder
    
    * Show, activate, focus
    
    * Post build action
    
    * NOT_DEPENDENT for inputs
    
    on slides said that it useful on imports and exports and faster around 20x
    
    * Add doc for InputObjectDependency.NOT_DEPENDENT
    
    * Disable conditions on post build action
    
    * Make it work!
    
    * Move packages to Directory.Packages.props
    
    * Bump tekla package.json to sdk 3.1.0-dev.146
    
    * Add tekla project to Local.sln too
    
    * Update packages.lock

---
+++ Connectors/Tekla/Speckle.Connector.Tekla2024/Plugin/TeklaPlugin.cs
@@ -0,0 +1,18 @@
+﻿using Tekla.Structures.Plugins;
+
+namespace Speckle.Connector.Tekla2024.Plugin;
+
+[Plugin("Speckle.Connectors.Tekla")]
+[PluginUserInterface("Speckle.Connector.Tekla2024.SpeckleTeklaPanelHost")]
+[InputObjectDependency(InputObjectDependency.NOT_DEPENDENT)] // See DevDocs/InputObjectDependency.NOT_DEPENDENT.png
+public class TeklaPlugin : PluginBase
+{
+#pragma warning disable IDE1006
+  public override bool Run(List<InputDefinition> Input)
+  {
+    return true;
+  }
+#pragma warning restore IDE1006
+
+  public override List<InputDefinition> DefineInput() => new();
+}
