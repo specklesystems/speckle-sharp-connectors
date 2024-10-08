9780cb6e35a87877f17003f18be06028f09ee757
Dimitrie Stefanescu <didimitrie@gmail.com>
2024-10-08 11:00:26 +0100

    feat: minor cleanup

--- Connectors/Revit/Speckle.Connectors.RevitShared/Operations/Receive/RevitHostObjectBuilder.cs
+++ Connectors/Revit/Speckle.Connectors.RevitShared/Operations/Receive/RevitHostObjectBuilder.cs
@@ -89,25 +89,21 @@ internal sealed class RevitHostObjectBuilder : IHostObjectBuilder, IDisposable
 
     onOperationProgressed?.Invoke("Converting", null);
     using var activity = _activityFactory.Start("Build");
-
+    
     // 0 - Clean then Rock n Roll! 🎸
-    using TransactionGroup preReceiveCleanTransaction = new(_converterSettings.Current.Document, "Pre-receive clean");
-    preReceiveCleanTransaction.Start();
-    _transactionManager.StartTransaction(true);
-
-    try
     {
-      PreReceiveDeepClean(baseGroupName);
-    }
-    catch (Exception ex) when (!ex.IsFatal())
-    {
-      _logger.LogError(ex, "Failed to clean up before receive in Revit");
-    }
-
-    using (var _ = _activityFactory.Start("Commit"))
-    {
+      _activityFactory.Start("Pre receive clean");
+      _transactionManager.StartTransaction(true, "Pre receive clean");
+      try
+      {
+        PreReceiveDeepClean(baseGroupName);
+      }
+      catch (Exception ex) when (!ex.IsFatal())
+      {
+        _logger.LogError(ex, "Failed to clean up before receive in Revit");
+      }
+      
       _transactionManager.CommitTransaction();
-      preReceiveCleanTransaction.Assimilate();
     }
 
     // 1 - Unpack objects and proxies from root commit object
@@ -116,36 +112,94 @@ internal sealed class RevitHostObjectBuilder : IHostObjectBuilder, IDisposable
       unpackedRoot.DefinitionProxies,
       unpackedRoot.ObjectsToConvert.ToList()
     );
-
-    using TransactionGroup transactionGroup =
-      new(_converterSettings.Current.Document, $"Received data from {projectName}");
-    transactionGroup.Start();
-    _transactionManager.StartTransaction();
-
+    
+    // 2 - Bake materials
+    _transactionManager.StartTransaction(true, "Baking materials");
     if (unpackedRoot.RenderMaterialProxies != null)
     {
       _materialBaker.MapLayersRenderMaterials(unpackedRoot);
-      // NOTE: do not set _contextStack.RenderMaterialProxyCache directly, things stop working. Ogu/Dim do not know why :) not a problem as we hopefully will refactor some of these hacks out.
-      var map = _materialBaker.BakeMaterials(unpackedRoot.RenderMaterialProxies, baseGroupName);
+      var map = _materialBaker.BakeMaterials(unpackedRoot.RenderMaterialProxies, baseGroupName); // NOTE: do not set _contextStack.RenderMaterialProxyCache directly, things stop working. Ogu/Dim do not know why :) not a problem as we hopefully will refactor some of these hacks out.
       foreach (var kvp in map)
       {
         _revitToHostCacheSingleton.MaterialsByObjectId.Add(kvp.Key, kvp.Value);
       }
     }
+    _transactionManager.CommitTransaction();
 
+    // 3 - Bake objects
+    _transactionManager.StartTransaction(true, "Baking objects");
     var conversionResults = BakeObjects(localToGlobalMaps, onOperationProgressed, cancellationToken);
+    _transactionManager.CommitTransaction();
+    
+    
+    _transactionManager.StartTransaction(true, "Painting");
+    PostBakePaint(conversionResults.postBakePaintTargets);
+    _transactionManager.CommitTransaction();
+    
+    _transactionManager.StartTransaction(true, "Grouping");
+    _groupBaker.BakeGroupForTopLevel(baseGroupName);
+    _transactionManager.CommitTransaction();
+    
+    
+    return conversionResults.builderResult;
+  }
 
-    using (var _ = _activityFactory.Start("Commit"))
+  private (HostObjectBuilderResult builderResult, List<(DirectShape res, string applicationId)> postBakePaintTargets) BakeObjects(
+    List<LocalToGlobalMap> localToGlobalMaps,
+    Action<string, double?>? onOperationProgressed,
+    CancellationToken cancellationToken
+  )
+  {
+    using var _ = _activityFactory.Start("BakeObjects");
+    var conversionResults = new List<ReceiveConversionResult>();
+    var bakedObjectIds = new List<string>();
+    int count = 0;
+
+    var postBakePaintTargets = new List<(DirectShape res, string applicationId)>();
+
+    foreach (LocalToGlobalMap localToGlobalMap in localToGlobalMaps)
     {
-      _transactionManager.CommitTransaction();
-      transactionGroup.Assimilate();
+      cancellationToken.ThrowIfCancellationRequested();
+      try
+      {
+        using var activity = _activityFactory.Start("BakeObject");
+        var result = _converter.Convert(localToGlobalMap.AtomicObject);
+        onOperationProgressed?.Invoke("Converting", (double)++count / localToGlobalMaps.Count);
+
+        if (result is DirectShapeDefinitionWrapper)
+        {
+          DirectShape directShapes = _localToGlobalDirectShapeConverter.Convert(
+            (localToGlobalMap.AtomicObject, localToGlobalMap.Matrix)
+          );
+
+          bakedObjectIds.Add(directShapes.UniqueId);
+          _groupBaker.AddToTopLevelGroup(directShapes);
+         
+          if (localToGlobalMap.AtomicObject is IRawEncodedObject && localToGlobalMap.AtomicObject is Base myBase)
+          {
+            postBakePaintTargets.Add((directShapes, myBase.applicationId ?? myBase.id));
+          }
+          
+          conversionResults.Add(
+            new(Status.SUCCESS, localToGlobalMap.AtomicObject, directShapes.UniqueId, "Direct Shape")
+          );
+        }
+        else
+        {
+          throw new SpeckleConversionException($"Failed to cast {result.GetType()} to direct shape definition wrapper.");
+        }
+      }
+      catch (Exception ex) when (!ex.IsFatal())
+      {
+        conversionResults.Add(new(Status.ERROR, localToGlobalMap.AtomicObject, null, null, ex));
+      }
     }
+    return (new(bakedObjectIds, conversionResults), postBakePaintTargets);
+  }
 
-    using TransactionGroup createGroupTransaction = new(_converterSettings.Current.Document, "Creating group");
-    createGroupTransaction.Start();
-    _transactionManager.StartTransaction(true);
-
-    foreach (var (res, applicationId) in conversionResults.Item2)
+  private void PostBakePaint(List<(DirectShape res, string applicationId)> paintTargets)
+  {
+    foreach (var (res, applicationId) in paintTargets)
     {
       var elGeometry = res.get_Geometry(new Options() { DetailLevel = ViewDetailLevel.Undefined });
       var materialId = ElementId.InvalidElementId;
@@ -164,7 +218,6 @@ internal sealed class RevitHostObjectBuilder : IHostObjectBuilder, IDisposable
       {
         if (geo is Solid s)
         {
-          // SolidUtils.CreateTransformed();
           foreach (Face face in s.Faces)
           {
             _converterSettings.Current.Document.Paint(res.Id, face, materialId);
@@ -172,83 +225,14 @@ internal sealed class RevitHostObjectBuilder : IHostObjectBuilder, IDisposable
         }
       }
     }
-
-    try
-    {
-      _groupBaker.BakeGroupForTopLevel(baseGroupName);
-    }
-    catch (Exception ex) when (!ex.IsFatal())
-    {
-      _logger.LogError(ex, "Failed to create group after receiving elements in Revit");
-    }
-
-    using (var _ = _activityFactory.Start("Commit"))
-    {
-      _transactionManager.CommitTransaction();
-      createGroupTransaction.Assimilate();
-    }
-
-    _revitToHostCacheSingleton.MaterialsByObjectId.Clear(); // Massive hack!
-
-    return conversionResults.Item1;
   }
-
-  private (HostObjectBuilderResult, List<(DirectShape res, string applicationId)>) BakeObjects(
-    List<LocalToGlobalMap> localToGlobalMaps,
-    Action<string, double?>? onOperationProgressed,
-    CancellationToken cancellationToken
-  )
-  {
-    using var _ = _activityFactory.Start("BakeObjects");
-    var conversionResults = new List<ReceiveConversionResult>();
-    var bakedObjectIds = new List<string>();
-    int count = 0;
-
-    var toPaintLater = new List<(DirectShape res, string applicationId)>();
-
-    foreach (LocalToGlobalMap localToGlobalMap in localToGlobalMaps)
-    {
-      cancellationToken.ThrowIfCancellationRequested();
-      try
-      {
-        using var activity = _activityFactory.Start("BakeObject");
-        var result = _converter.Convert(localToGlobalMap.AtomicObject);
-        onOperationProgressed?.Invoke("Converting", (double)++count / localToGlobalMaps.Count);
-
-        if (result is FakeDirectShapeDefinition)
-        {
-          DirectShape directShapes = _localToGlobalDirectShapeConverter.Convert(
-            (localToGlobalMap.AtomicObject, localToGlobalMap.Matrix)
-          );
-
-          bakedObjectIds.Add(directShapes.UniqueId.ToString());
-          _groupBaker.AddToTopLevelGroup(directShapes);
-          if (localToGlobalMap.AtomicObject is IRawEncodedObject && localToGlobalMap.AtomicObject is Base myBase)
-          {
-            toPaintLater.Add((directShapes, myBase.applicationId ?? myBase.id));
-          }
-          conversionResults.Add(
-            new(Status.SUCCESS, localToGlobalMap.AtomicObject, directShapes.UniqueId, "Direct Shape")
-          );
-        }
-        else
-        {
-          throw new SpeckleConversionException($"Failed to cast {result.GetType()} to Direct Shape.");
-        }
-      }
-      catch (Exception ex) when (!ex.IsFatal())
-      {
-        conversionResults.Add(new(Status.ERROR, localToGlobalMap.AtomicObject, null, null, ex));
-      }
-    }
-    return (new(bakedObjectIds, conversionResults), toPaintLater);
-  }
-
+  
   private void PreReceiveDeepClean(string baseGroupName)
   {
     DirectShapeLibrary
-      .GetDirectShapeLibrary(_converterSettings.Current.Document).Reset();
-    _revitToHostCacheSingleton.MaterialsByObjectId.Clear();
+      .GetDirectShapeLibrary(_converterSettings.Current.Document).Reset(); // Note: this needs to be cleared, as it is being used in the converter
+    
+    _revitToHostCacheSingleton.MaterialsByObjectId.Clear(); // Massive hack!
     _groupBaker.PurgeGroups(baseGroupName);
     _materialBaker.PurgeMaterials(baseGroupName);
   }
