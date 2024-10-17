91785327872cdd72e45c3e8d4101331715be0d4d
Claire Kuang <kuang.claire@gmail.com>
2024-10-17 12:11:19 +0100

    fixes more version api issues

--- Converters/Civil3d/Speckle.Converters.Civil3dShared/Helpers/DisplayValueExtractor.cs
+++ Converters/Civil3d/Speckle.Converters.Civil3dShared/Helpers/DisplayValueExtractor.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Logging;
 using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
+using Speckle.Sdk.Models;
 
 namespace Speckle.Converters.Civil3dShared.Helpers;
 
@@ -9,6 +10,7 @@ public sealed class DisplayValueExtractor
   private readonly ITypedConverter<ADB.Solid3d, SOG.Mesh> _solidConverter;
   private readonly ITypedConverter<CDB.TinSurface, SOG.Mesh> _tinSurfaceConverter;
   private readonly ITypedConverter<CDB.GridSurface, SOG.Mesh> _gridSurfaceConverter;
+  private readonly ITypedConverter<AG.Point3dCollection, SOG.Polyline> _pointCollectionConverter;
   private readonly ILogger<DisplayValueExtractor> _logger;
   private readonly IConverterSettingsStore<Civil3dConversionSettings> _converterSettings;
 
@@ -16,6 +18,7 @@ public sealed class DisplayValueExtractor
     ITypedConverter<ADB.Solid3d, SOG.Mesh> solidConverter,
     ITypedConverter<CDB.TinSurface, SOG.Mesh> tinSurfaceConverter,
     ITypedConverter<CDB.GridSurface, SOG.Mesh> gridSurfaceConverter,
+    ITypedConverter<AG.Point3dCollection, SOG.Polyline> pointCollectionConverter,
     ILogger<DisplayValueExtractor> logger,
     IConverterSettingsStore<Civil3dConversionSettings> converterSettings
   )
@@ -23,32 +26,37 @@ public sealed class DisplayValueExtractor
     _solidConverter = solidConverter;
     _tinSurfaceConverter = tinSurfaceConverter;
     _gridSurfaceConverter = gridSurfaceConverter;
+    _pointCollectionConverter = pointCollectionConverter;
     _logger = logger;
     _converterSettings = converterSettings;
   }
 
-  public List<SOG.Mesh> GetDisplayValue(CDB.Entity entity)
+  public List<Base> GetDisplayValue(CDB.Entity entity)
   {
-    List<SOG.Mesh> result = new();
     switch (entity)
     {
+      case CDB.FeatureLine featureline:
+        SOG.Polyline featurelinePolyline = _pointCollectionConverter.Convert(
+          featureline.GetPoints(Autodesk.Civil.FeatureLinePointType.PIPoint)
+        );
+        return new() { featurelinePolyline };
+
       // pipe networks: https://help.autodesk.com/view/CIV3D/2025/ENU/?guid=ade47b62-debf-f899-9b94-5645a620ab4f
       case CDB.Part part:
         SOG.Mesh partMesh = _solidConverter.Convert(part.Solid3dBody);
-        result.Add(partMesh);
-        break;
+        return new() { partMesh };
 
       // surfaces: https://help.autodesk.com/view/CIV3D/2025/ENU/?guid=d741aa49-e7da-9513-6b0b-226ebe3fa43f
       // POC: volume surfaces not supported
       case CDB.TinSurface tinSurface:
         SOG.Mesh tinSurfaceMesh = _tinSurfaceConverter.Convert(tinSurface);
-        result.Add(tinSurfaceMesh);
-        break;
+        return new() { tinSurfaceMesh };
       case CDB.GridSurface gridSurface:
         SOG.Mesh gridSurfaceMesh = _gridSurfaceConverter.Convert(gridSurface);
-        result.Add(gridSurfaceMesh);
-        break;
+        return new() { gridSurfaceMesh };
+
+      default:
+        return new();
     }
-    return result;
   }
 }
