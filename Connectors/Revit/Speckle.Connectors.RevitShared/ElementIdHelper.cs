a37d1c5f85590734a48b950208978008fef4d0a1
Dimitrie Stefanescu <didimitrie@gmail.com>
2024-10-18 12:22:27 +0100

    fix: card preview not working on deleted element ids

--- Connectors/Revit/Speckle.Connectors.RevitShared/ElementIdHelper.cs
+++ Connectors/Revit/Speckle.Connectors.RevitShared/ElementIdHelper.cs
@@ -5,14 +5,9 @@ namespace Speckle.Connectors.RevitShared;
 
 public static class ElementIdHelper
 {
-  public static ElementId GetElementIdFromUniqueId(Document doc, string uniqueId)
+  public static ElementId? GetElementIdFromUniqueId(Document doc, string uniqueId)
   {
     Element element = doc.GetElement(uniqueId);
-    if (element == null)
-    {
-      throw new SpeckleException($"Cannot find element with UniqueId: {uniqueId}");
-    }
-
-    return element.Id;
+    return element?.Id;
   }
 }
