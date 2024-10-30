640905f752d4662fc5fc9c457b41672146d76805
Dogukan Karatas <karatasdogukan@gmail.com>
2024-10-30 16:33:40 +0100

    adds property extractor

--- Converters/Tekla/Speckle.Converter.Tekla2024/ServiceRegistration.cs
+++ Converters/Tekla/Speckle.Converter.Tekla2024/ServiceRegistration.cs
@@ -1,10 +1,14 @@
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Speckle.Converter.Tekla2024.ToSpeckle.Helpers;
+using Speckle.Converter.Tekla2024.ToSpeckle.Raw;
+using Speckle.Converter.Tekla2024.ToSpeckle.TopLevel;
 using Speckle.Converters.Common;
+using Speckle.Converters.Common.Objects;
 using Speckle.Converters.Common.Registration;
 using Speckle.Sdk;
 using Tekla.Structures.Drawing;
+using SOG = Speckle.Objects.Geometry;
 
 namespace Speckle.Converter.Tekla2024;
 
@@ -13,16 +17,20 @@ public static class ServiceRegistration
   public static IServiceCollection AddTeklaConverters(this IServiceCollection serviceCollection)
   {
     var converterAssembly = Assembly.GetExecutingAssembly();
-    serviceCollection.AddMatchingInterfacesAsTransient(converterAssembly);
+    
+    serviceCollection.AddTransient<ModelObjectToSpeckleConverter>();
+    
+    serviceCollection.AddTransient<ITypedConverter<TG.Point, SOG.Point>, TeklaPointConverter>();
+    
+    serviceCollection.AddScoped<DisplayValueExtractor>();
+    serviceCollection.AddScoped<PropertyExtractor>();
+    
     serviceCollection.AddRootCommon<TeklaRootToSpeckleConverter>(converterAssembly);
-
     serviceCollection.AddApplicationConverters<TeklaToSpeckleUnitConverter, Units>(converterAssembly);
     serviceCollection.AddScoped<
       IConverterSettingsStore<TeklaConversionSettings>,
       ConverterSettingsStore<TeklaConversionSettings>
     >();
-    
-    serviceCollection.AddScoped<DisplayValueExtractor>();
     
     serviceCollection.AddMatchingInterfacesAsTransient(converterAssembly);
 
--- Converters/Tekla/Speckle.Converter.Tekla2024/ToSpeckle/TopLevel/ModelObjectToSpeckleConverter.cs
+++ Converters/Tekla/Speckle.Converter.Tekla2024/ToSpeckle/TopLevel/ModelObjectToSpeckleConverter.cs
@@ -1,6 +1,6 @@
+using Speckle.Converter.Tekla2024.ToSpeckle.Helpers;
 using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
-using Speckle.Sdk.Common.Exceptions;
 using Speckle.Sdk.Models;
 
 namespace Speckle.Converter.Tekla2024.ToSpeckle.TopLevel;
@@ -8,43 +8,47 @@ namespace Speckle.Converter.Tekla2024.ToSpeckle.TopLevel;
 [NameAndRankValue(nameof(TSM.ModelObject), NameAndRankValueAttribute.SPECKLE_DEFAULT_RANK)]
 public class ModelObjectToSpeckleConverter : IToSpeckleTopLevelConverter
 {
-    private readonly IConverterSettingsStore<TeklaConversionSettings> _settingsStore;
-    private readonly ITypedConverter<TSM.Beam, Base> _beamConverter;
-    private readonly ITypedConverter<TSM.ContourPlate, Base> _plateConverter;
-    public ModelObjectToSpeckleConverter(
-        IConverterSettingsStore<TeklaConversionSettings> settingsStore,
-        ITypedConverter<TSM.Beam, Base> beamConverter,
-        ITypedConverter<TSM.ContourPlate, Base> plateConverter)
+  private readonly IConverterSettingsStore<TeklaConversionSettings> _settingsStore;
+  private readonly DisplayValueExtractor _displayValueExtractor;
+  private readonly PropertyExtractor _propertyExtractor;
+
+  public ModelObjectToSpeckleConverter(
+    IConverterSettingsStore<TeklaConversionSettings> settingsStore,
+    DisplayValueExtractor displayValueExtractor,
+    PropertyExtractor propertyExtractor)
+  {
+    _settingsStore = settingsStore;
+    _displayValueExtractor = displayValueExtractor;
+    _propertyExtractor = propertyExtractor;
+  }
+
+  public Base Convert(object target)
+  {
+    if (target is not TSM.ModelObject modelObject)
     {
-        _settingsStore = settingsStore;
-        _beamConverter = beamConverter;
-        _plateConverter = plateConverter;
+      throw new ArgumentException($"Target object is not a ModelObject. It's a {target.GetType()}");
     }
 
-    public Base Convert(object target)
+    var result = new Base
     {
-        if (target is not TSM.ModelObject modelObject)
-        {
-            throw new ArgumentException($"Target object is not a ModelObject. It's a {target.GetType()}");
-        }
+      ["type"] = modelObject.GetType().ToString().Split('.').Last(),
+      ["units"] = _settingsStore.Current.SpeckleUnits
+    };
 
-        Base result;
-        switch (modelObject)
-        {
-            case TSM.Beam beam:
-                result =  _beamConverter.Convert(beam);
-                break;
-            case TSM.ContourPlate plate:
-              result = _plateConverter.Convert(plate);
-              break;
-            default:
-              throw new ConversionNotSupportedException(
-                $"Conversion of {target.GetType().Name} to Speckle is not supported.");
-        }
-        
-        result["type"] = modelObject.GetType().ToString().Split('.').Last();
-        result["units"] = _settingsStore.Current.SpeckleUnits;
-        
-        return result;
+    // get properties
+    var properties = _propertyExtractor.GetProperties(modelObject);
+    foreach (var prop in properties)
+    {
+      result[prop.Key] = prop.Value;
     }
+
+    // get display value
+    var displayValue = _displayValueExtractor.GetDisplayValue(modelObject).ToList();
+    if (displayValue.Count > 0)
+    {
+      result["displayValue"] = displayValue;
+    }
+
+    return result;
+  }
 }
