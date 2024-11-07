c1293ff395aa205337f92f31374c534de077107b
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-11-07 18:49:47 +0000

    Fixed autocad async (#370)
    Co-authored-by: Alan Rynne <alan@speckle.systems>

--- Connectors/Autocad/Speckle.Connectors.AutocadShared/Bindings/AutocadSendBaseBinding.cs
+++ Connectors/Autocad/Speckle.Connectors.AutocadShared/Bindings/AutocadSendBaseBinding.cs
@@ -111,10 +111,13 @@ public abstract class AutocadSendBaseBinding : ISendBinding
   private void OnChangeChangedObjectIds(DBObject dBObject)
   {
     ChangedObjectIds[dBObject.GetSpeckleApplicationId()] = 1;
-    _idleManager.SubscribeToIdle(nameof(AutocadSendBinding), RunExpirationChecks);
+    _idleManager.SubscribeToIdle(
+      nameof(AutocadSendBinding),
+      async () => await RunExpirationChecks().ConfigureAwait(false)
+    );
   }
 
-  private void RunExpirationChecks()
+  private async Task RunExpirationChecks()
   {
     var senders = _store.GetSenders();
     string[] objectIdsList = ChangedObjectIds.Keys.ToArray();
@@ -132,7 +135,7 @@ public abstract class AutocadSendBaseBinding : ISendBinding
       }
     }
 
-    Commands.SetModelsExpired(expiredSenderIds);
+    await Commands.SetModelsExpired(expiredSenderIds).ConfigureAwait(false);
     ChangedObjectIds = new();
   }
 
