f53b77e1a5ff4875623a50681a69c3fdbc9e10e5
Björn <steinhagen.bjoern@gmail.com>
2024-11-18 17:11:31 +0100

    Distance.CurrentUnitType
    As discussed

--- Converters/Tekla/Speckle.Converters.TeklaShared/TeklaConversionSettingsFactory.cs
+++ Converters/Tekla/Speckle.Converters.TeklaShared/TeklaConversionSettingsFactory.cs
@@ -7,14 +7,15 @@ namespace Speckle.Converter.Tekla2024;
 
 [GenerateAutoInterface]
 public class TeklaConversionSettingsFactory(
-  IHostToSpeckleUnitConverter<string> unitsConverter,
+  IHostToSpeckleUnitConverter<Distance.UnitType> unitsConverter,
   IConverterSettingsStore<TeklaConversionSettings> settingsStore
 ) : ITeklaConversionSettingsFactory
 {
   public TeklaConversionSettings Current => settingsStore.Current;
 
-  // NOTE: Distance.CurrentUnitType reflects Settings > Options > Units and decimals, but internal units always returns mm
-  // Therefore, hard-coding Distance.MILLIMETERS (for now)
+  // NOTE: Distance.CurrentUnitType reflects Settings > Options > Units and decimals
+  // Internal units (mm) are, however, always returned.
+  // If model units != internal units, user can rely on units appended to each report parameter
   public TeklaConversionSettings Create(Model document, bool sendRebarsAsSolid) =>
-    new(document, sendRebarsAsSolid, unitsConverter.ConvertOrThrow(Distance.MILLIMETERS));
+    new(document, sendRebarsAsSolid, unitsConverter.ConvertOrThrow(Distance.CurrentUnitType));
 }
