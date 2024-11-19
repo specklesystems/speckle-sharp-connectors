f9b81f4c83730a44eb0f31ee4de3f094bba3a5ca
Dogukan Karatas <karatasdogukan@gmail.com>
2024-11-19 15:03:26 +0100

    adds highlighting methods

--- Connectors/Tekla/Speckle.Connector.TeklaShared/Bindings/TeklaBasicConnectorBinding.cs
+++ Connectors/Tekla/Speckle.Connector.TeklaShared/Bindings/TeklaBasicConnectorBinding.cs
@@ -1,8 +1,10 @@
+using Microsoft.Extensions.Logging;
 using Speckle.Connectors.DUI.Bindings;
 using Speckle.Connectors.DUI.Bridge;
 using Speckle.Connectors.DUI.Models;
 using Speckle.Connectors.DUI.Models.Card;
 using Speckle.Sdk;
+using Tekla.Structures;
 
 namespace Speckle.Connector.Tekla2024.Bindings;
 
@@ -12,16 +14,22 @@ public class TeklaBasicConnectorBinding : IBasicConnectorBinding
   private readonly DocumentModelStore _store;
   public string Name => "baseBinding";
   public IBrowserBridge Parent { get; }
+  private readonly ILogger<TeklaBasicConnectorBinding> _logger;
+  private readonly TSM.Model _model;
 
   public TeklaBasicConnectorBinding(
     IBrowserBridge parent,
     ISpeckleApplication speckleApplication,
-    DocumentModelStore store
+    DocumentModelStore store,
+    ILogger<TeklaBasicConnectorBinding> logger,
+    TSM.Model model
   )
   {
     _speckleApplication = speckleApplication;
     _store = store;
     Parent = parent;
+    _logger = logger;
+    _model = model;
   }
 
   public string GetSourceApplicationName() => _speckleApplication.Slug;
@@ -40,9 +48,64 @@ public class TeklaBasicConnectorBinding : IBasicConnectorBinding
 
   public void RemoveModel(ModelCard model) => _store.RemoveModel(model);
 
-  public Task HighlightModel(string modelCardId) => throw new NotImplementedException();
+  public async Task HighlightModel(string modelCardId)
+  {
+    try
+    {
+      var model = _store.GetModelById(modelCardId);
+      if (model == null)
+      {
+        _logger.LogError("Model was null when highlighting received model");
+        return;
+      }
 
-  public Task HighlightObjects(IReadOnlyList<string> objectIds) => throw new NotImplementedException();
+      List<string> objectIds = new();
+      if (model is SenderModelCard senderModel)
+      {
+        objectIds = senderModel.SendFilter?.RefreshObjectIds() ?? new List<string>();
+      }
+      else if (model is ReceiverModelCard receiverModel)
+      {
+        objectIds = receiverModel.BakedObjectIds?.ToList() ?? new List<string>();
+      }
+
+      if (objectIds.Count == 0)
+      {
+        await Commands
+          .SetModelError(modelCardId, new OperationCanceledException("No objects found to highlight."))
+          .ConfigureAwait(false);
+        return;
+      }
+
+      await HighlightObjects(objectIds).ConfigureAwait(false);
+    }
+    catch (InvalidOperationException ex)
+    {
+      _logger.LogError(ex, "Failed to highlight model");
+      await Commands.SetModelError(modelCardId, ex).ConfigureAwait(false);
+    }
+  }
+
+  public async Task HighlightObjects(IReadOnlyList<string> objectIds)
+  {
+    try
+    {
+      await Task.Run(() =>
+        {
+          var modelObjects = objectIds
+            .Select(id => _model.SelectModelObject(new Identifier(new Guid(id))))
+            .Where(obj => obj != null)
+            .ToList();
+
+          TSM.Operations.Operation.Highlight(modelObjects);
+        })
+        .ConfigureAwait(false);
+    }
+    catch (InvalidOperationException ex)
+    {
+      _logger.LogError(ex, "Failed to highlight objects");
+    }
+  }
 
   public BasicConnectorBindingCommands Commands { get; }
 }
