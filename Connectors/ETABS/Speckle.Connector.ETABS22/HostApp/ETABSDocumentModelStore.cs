036ab00b6ef826f6635defe3d88ecc4129ceb2d9
Dogukan Karatas <karatasdogukan@gmail.com>
2024-11-25 15:44:55 +0100

    updates packages.lock

--- Connectors/ETABS/Speckle.Connector.ETABS22/HostApp/ETABSDocumentModelStore.cs
+++ Connectors/ETABS/Speckle.Connector.ETABS22/HostApp/ETABSDocumentModelStore.cs
@@ -1,11 +1,11 @@
 ﻿using Speckle.Connectors.DUI.Models;
-using Speckle.Newtonsoft.Json;
+using Speckle.Connectors.DUI.Utils;
 
 namespace Speckle.Connector.ETABS22.HostApp;
 
 public class ETABSDocumentModelStore : DocumentModelStore
 {
-  public ETABSDocumentModelStore(JsonSerializerSettings jsonSerializerSettings)
+  public ETABSDocumentModelStore(IJsonSerializer jsonSerializerSettings)
     : base(jsonSerializerSettings, true) { }
 
   public override void WriteToFile() { }
