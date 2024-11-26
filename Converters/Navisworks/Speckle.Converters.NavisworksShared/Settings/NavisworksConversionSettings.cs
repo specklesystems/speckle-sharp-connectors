934f829b1bef5e98a5697f6ce0371a3479ca43d6
Jonathon Broughton <jonathon@stardotbmp.com>
2024-11-26 01:03:34 +0000

    Refactor NavisworksConversionSettings class to use properties instead of fields
    - Convert fields to properties in NavisworksConversionSettings class
    - Update constructor to initialize properties instead of fields

--- Converters/Navisworks/Speckle.Converters.NavisworksShared/Settings/NavisworksConversionSettings.cs
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/Settings/NavisworksConversionSettings.cs
@@ -1,3 +1,13 @@
 ﻿namespace Speckle.Converter.Navisworks.Settings;
 
-public record NavisworksConversionSettings(NAV.Document Document, string SpeckleUnits);
+public record NavisworksConversionSettings
+{
+  public NavisworksConversionSettings(NAV.Document document, string speckleUnits)
+  {
+    this.Document = document;
+    this.SpeckleUnits = speckleUnits;
+  }
+
+  public NAV.Document Document { get; }
+  public string SpeckleUnits { get; }
+}
