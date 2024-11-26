486ec478b79827e81118c06ea8e3e030f4e457fd
Jonathon Broughton <jonathon@stardotbmp.com>
2024-11-26 01:52:58 +0000

    Add Navisworks model item to Speckle converter
    - Add `ModelItemToSpeckleConverter.cs` file for converting Navisworks model items to Speckle objects.
    - Update `GlobalUsing.cs` to include `SSM` namespace.
    - Update project file `Speckle.Converters.NavisworksShared.projitems` to include the new converter file.

--- Converters/Navisworks/Speckle.Converters.NavisworksShared/GlobalUsing.cs
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/GlobalUsing.cs
@@ -1,4 +1,4 @@
 global using NAV = Autodesk.Navisworks.Api;
 global using NavisworksApp = Autodesk.Navisworks.Api.Application;
-// global using SOG = Speckle.Objects.Geometry;
 global using SSC = Speckle.Sdk.Common;
+global using SSM = Speckle.Sdk.Models;
---
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/ToSpeckle/ModelItemToSpeckleConverter.cs
@@ -0,0 +1,11 @@
+﻿using Speckle.Converter.Navisworks.Settings;
+using Speckle.Converters.Common;
+using Speckle.Converters.Common.Objects;
+
+namespace Speckle.Converter.Navisworks.ToSpeckle;
+
+public class ModelItemToSpeckleTopLevelConverter(IConverterSettingsStore<NavisworksConversionSettings> settingsStore)
+  : IToSpeckleTopLevelConverter
+{
+  public SSM.Base Convert(object target) => new() { ["units"] = settingsStore.Current.SpeckleUnits };
+}
