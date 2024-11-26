a7699f0bc50e8b5f91fbe3a3020597e0881895b2
Adam Hathcock <adam@hathcock.uk>
2024-11-26 11:48:23 +0000

    Revit needs new run async

--- Connectors/Revit/Speckle.Connectors.RevitShared/Plugin/RevitThreadContext.cs
+++ Connectors/Revit/Speckle.Connectors.RevitShared/Plugin/RevitThreadContext.cs
@@ -5,6 +5,8 @@ namespace Speckle.Connectors.Revit.Plugin;
 
 public class RevitThreadContext : ThreadContext
 {
+  public override Task RunContext(Func<Task> action) => RevitTask.RunAsync(action);
+
   public override void RunContext(Action action) => RevitTask.RunAsync(action);
 
   public override Task<T> RunContext<T>(Func<Task<T>> action) => RevitTask.RunAsync(action);
