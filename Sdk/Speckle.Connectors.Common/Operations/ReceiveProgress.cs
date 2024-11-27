9d6c7decbcac16c150b4a9ec302db8de55ba64ce
Adam Hathcock <adamhathcock@users.noreply.github.com>
2024-11-27 14:44:48 +0000

    Rework progress to try to be better 😬  (#405)
    * Use SuspendingObservableCollection
    
    * updates
    
    * Use thread-safe models
    
    * handle loading
    
    * fix up tekla
    
    * remove option
    
    * fix suspension
    
    * Revit shouldn't register changed twice.  top level goes to base class
    
    * use suspension when loading
    
    * forgot include
    
    * lock saving like loading
    
    * Remove public accessor
    
    * Require a document store for each DUI usage
    
    * simpify load and state saving since we don't need to observe the collection externally
    
    * added back property for javascript UI
    
    * fmt
    
    * move progress handling to own classes
    
    * fmt
    
    * fmt
    
    * fix up diff
    
    * Fix merge
    
    * adjust sending progress
    
    ---------
    
    Co-authored-by: Oğuzhan Koral <45078678+oguzhankoral@users.noreply.github.com>

---
+++ Sdk/Speckle.Connectors.Common/Operations/ReceiveProgress.cs
@@ -0,0 +1,46 @@
+﻿using Speckle.InterfaceGenerator;
+using Speckle.Sdk.Transports;
+
+namespace Speckle.Connectors.Common.Operations;
+
+[GenerateAutoInterface]
+public sealed class ReceiveProgress(IProgressDisplayManager progressDisplayManager) : IReceiveProgress
+{
+  private double? _previousPercentage;
+  private string? _previousSpeed;
+
+  public void Begin() => progressDisplayManager.Begin();
+
+  public void Report(IProgress<CardProgress> onOperationProgressed, ProgressArgs args)
+  {
+    {
+      switch (args.ProgressEvent)
+      {
+        case ProgressEvent.CacheCheck:
+          _previousPercentage = progressDisplayManager.CalculatePercentage(args);
+          break;
+        case ProgressEvent.DownloadBytes:
+          _previousSpeed = progressDisplayManager.CalculateSpeed(args);
+          break;
+      }
+
+      if (!progressDisplayManager.ShouldUpdate())
+      {
+        return;
+      }
+
+      switch (args.ProgressEvent)
+      {
+        case ProgressEvent.CacheCheck:
+          onOperationProgressed.Report(new("Checking cache... ", _previousPercentage));
+          break;
+        case ProgressEvent.DownloadBytes:
+          onOperationProgressed.Report(new($"Downloading... ({_previousSpeed})", null));
+          break;
+        case ProgressEvent.DeserializeObject:
+          onOperationProgressed.Report(new("Deserializing ...", progressDisplayManager.CalculatePercentage(args)));
+          break;
+      }
+    }
+  }
+}
