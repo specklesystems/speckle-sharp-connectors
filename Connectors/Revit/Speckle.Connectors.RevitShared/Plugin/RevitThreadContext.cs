d826a3e9a8f62f31b2adb295aa4138e65c0d6913
Adam Hathcock <adam@hathcock.uk>
2024-11-28 09:33:23 +0000

    Revit threading is simplier?

--- Connectors/Revit/Speckle.Connectors.RevitShared/Plugin/RevitThreadContext.cs
+++ Connectors/Revit/Speckle.Connectors.RevitShared/Plugin/RevitThreadContext.cs
@@ -3,13 +3,13 @@ using Speckle.Connectors.Common.Threading;
 
 namespace Speckle.Connectors.Revit.Plugin;
 
-public class RevitThreadContext : DefaultThreadContext
+public class RevitThreadContext : ThreadContext
 {
-  protected override Task<T> RunContext<T>(Func<T> action) => RevitTask.RunAsync(action);
+  protected override Task<T> MainToWorkerAsync<T>(Func<Task<T>> action) => action();
 
-  protected override Task RunContext(Func<Task> action) => RevitTask.RunAsync(action);
+  protected override Task<T> WorkerToMainAsync<T>(Func<Task<T>> action) => RevitTask.RunAsync(action);
 
-  protected override void RunContext(Action action) => RevitTask.RunAsync(action);
+  protected override Task<T> MainToWorker<T>(Func<T> action)  => Task.FromResult(action());
 
-  protected override Task<T> RunContext<T>(Func<Task<T>> action) => RevitTask.RunAsync(action);
+  protected override Task<T> WorkerToMain<T>(Func<T> action) => RevitTask.RunAsync(action);
 }
