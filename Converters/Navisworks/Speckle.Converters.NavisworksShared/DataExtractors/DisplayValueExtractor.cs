cc5a4022de1dee9ba3e02657caa23957111663fc
Jonathon Broughton <jonathon@stardotbmp.com>
2024-11-30 23:25:34 +0000

    Add ClassPropertiesExtractor, DisplayValueExtractor, and PropertySetsExtractor classes
    - Added ClassPropertiesExtractor class to extract class properties from a NAV.ModelItem
    - Added DisplayValueExtractor class to extract display values from a NAV.ModelItem
    - Added PropertySetsExtractor class to extract property sets from a NAV.ModelItem

---
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/DataExtractors/DisplayValueExtractor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Speckle.Converter.Navisworks.Settings;
+using Speckle.Converters.Common;
+using Speckle.Objects.Geometry;
+using Speckle.Sdk;
+
+namespace Speckle.Converter.Navisworks.ToSpeckle;
+
+public class DisplayValueExtractor
+{
+  private readonly IConverterSettingsStore<NavisworksConversionSettings> _converterSettings;
+  private readonly ILogger<DisplayValueExtractor> _logger;
+
+  public DisplayValueExtractor(
+    IConverterSettingsStore<NavisworksConversionSettings> converterSettings,
+    ILogger<DisplayValueExtractor> logger
+  )
+  {
+    _converterSettings = converterSettings;
+    _logger = logger;
+  }
+
+  public List<SSM.Base> GetDisplayValue(NAV.ModelItem _) => [];
+}
---
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/DataExtractors/PropertySetsExtractor.cs
@@ -0,0 +1,52 @@
+﻿using Speckle.Converter.Navisworks.Settings;
+using Speckle.Converters.Common;
+
+namespace Speckle.Converter.Navisworks.ToSpeckle;
+
+public class PropertySetsExtractor
+{
+  private readonly IConverterSettingsStore<NavisworksConversionSettings> _settingsStore;
+
+  public PropertySetsExtractor(IConverterSettingsStore<NavisworksConversionSettings> settingsStore)
+  {
+    _settingsStore = settingsStore;
+  }
+
+  public Dictionary<string, object?>? GetPropertySets(NAV.ModelItem modelItem)
+  {
+    if (_settingsStore.Current.ExcludeProperties)
+    {
+      return null;
+    }
+
+    var propertyDictionary = ExtractPropertySets(modelItem);
+
+    return propertyDictionary;
+  }
+
+  /// <summary>
+  /// Extracts property sets from a NAV.ModelItem and adds them to a dictionary,
+  /// PropertySets are specific to the host application source appended to Navisworks and therefore
+  /// arbitrary in nature.
+  /// </summary>
+  /// <param name="modelItem">The NAV.ModelItem from which property sets are extracted.</param>
+  /// <returns>A dictionary containing property sets of the modelItem.</returns>
+  private static Dictionary<string, object?> ExtractPropertySets(NAV.ModelItem modelItem)
+  {
+    var propertySetDictionary = new Dictionary<string, object?>();
+
+    modelItem.PropertyCategories.ForEach(propertyCategory =>
+    {
+      var propertySet = new Dictionary<string, object?>();
+
+      propertyCategory.Properties.ForEach(property =>
+      {
+        propertySet.Add(property.Name, property.Value);
+      });
+
+      propertySetDictionary.Add(propertyCategory.Name, propertySet);
+    });
+
+    return propertySetDictionary;
+  }
+}
