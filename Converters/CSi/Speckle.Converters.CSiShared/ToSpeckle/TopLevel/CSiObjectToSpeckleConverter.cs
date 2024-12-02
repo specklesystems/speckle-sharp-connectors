af1d8725b2748885b526580f57d9e606862b6985
Björn <steinhagen.bjoern@gmail.com>
2024-12-02 16:54:37 +0100

    newline

--- Converters/CSi/Speckle.Converters.CSiShared/ToSpeckle/TopLevel/CSiObjectToSpeckleConverter.cs
+++ Converters/CSi/Speckle.Converters.CSiShared/ToSpeckle/TopLevel/CSiObjectToSpeckleConverter.cs
@@ -25,7 +25,7 @@ public class CSiObjectToSpeckleConverter : IToSpeckleTopLevelConverter
   }
 
   public Base Convert(object target) => Convert((CSiWrapperBase)target);
-  
+
   private Base Convert(CSiWrapperBase target)
   {
     var result = new Base
