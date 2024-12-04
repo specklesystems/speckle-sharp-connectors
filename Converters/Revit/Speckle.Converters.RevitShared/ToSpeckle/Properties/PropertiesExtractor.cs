f1f54031327abc82b7f180300fe300409ad26e10
Claire Kuang <kuang.claire@gmail.com>
2024-12-04 16:30:21 +0000

    bumps sdk version

--- Converters/Revit/Speckle.Converters.RevitShared/ToSpeckle/Properties/PropertiesExtractor.cs
+++ Converters/Revit/Speckle.Converters.RevitShared/ToSpeckle/Properties/PropertiesExtractor.cs
@@ -1,6 +1,4 @@
-using Microsoft.Extensions.Logging;
 using Speckle.Converters.Common.Objects;
-using Speckle.Sdk;
 
 namespace Speckle.Converters.RevitShared.ToSpeckle.Properties;
 
@@ -8,47 +6,30 @@ public class PropertiesExtractor
 {
   private readonly ParameterExtractor _parameterExtractor;
   private readonly ITypedConverter<DB.Element, Dictionary<string, object>> _materialQuantityConverter;
-  private readonly ILogger<RevitRootToSpeckleConverter> _logger;
 
   public PropertiesExtractor(
     ParameterExtractor parameterExtractor,
-    ITypedConverter<DB.Element, Dictionary<string, object>> materialQuantityConverter,
-    ILogger<RevitRootToSpeckleConverter> logger
+    ITypedConverter<DB.Element, Dictionary<string, object>> materialQuantityConverter
   )
   {
     _parameterExtractor = parameterExtractor;
     _materialQuantityConverter = materialQuantityConverter;
-    _logger = logger;
   }
 
   public Dictionary<string, object?> GetProperties(DB.Element element)
   {
     Dictionary<string, object?> properties = new();
 
-    try
+    Dictionary<string, object> matQuantities = _materialQuantityConverter.Convert(element);
+    if (matQuantities.Count > 0)
     {
-      Dictionary<string, object> matQuantities = _materialQuantityConverter.Convert(element);
-      if (matQuantities.Count > 0)
-      {
-        properties.Add("Material Quantities", matQuantities);
-      }
-    }
-    catch (Exception e) when (!e.IsFatal())
-    {
-      _logger.LogWarning(e, $"Failed to extract material quantities from element {element.GetType().Name}");
+      properties.Add("Material Quantities", matQuantities);
     }
 
-    try
+    Dictionary<string, object?> parameters = _parameterExtractor.GetParameters(element);
+    if (parameters.Count > 0)
     {
-      Dictionary<string, object?> parameters = _parameterExtractor.GetParameters(element);
-      if (parameters.Count > 0)
-      {
-        properties.Add("Parameters", parameters);
-      }
-    }
-    catch (Exception e) when (!e.IsFatal())
-    {
-      _logger.LogWarning(e, $"Failed to extract parameters from element {element.GetType().Name}");
+      properties.Add("Parameters", parameters);
     }
 
     return properties;
