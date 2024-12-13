dcb6c6f038a34383d8803546f0a1ec8b26e7359b
Adam Hathcock <adam@hathcock.uk>
2024-12-13 09:45:28 +0000

    Caching the converter type results in a big speed up as the loop as a lot of wasted cycles

--- Sdk/Speckle.Converters.Common/Registration/ConverterManager.cs
+++ Sdk/Speckle.Converters.Common/Registration/ConverterManager.cs
@@ -7,11 +7,21 @@ namespace Speckle.Converters.Common.Registration;
 public class ConverterManager<T>(ConcurrentDictionary<string, Type> converterTypes, IServiceProvider serviceProvider)
   : IConverterManager<T>
 {
+  private readonly Dictionary<Type, string?> _foundTypes = new();
   public string Name => typeof(T).Name;
 
   public T ResolveConverter(Type type, bool recursive = true)
   {
     var currentType = type;
+    if (_foundTypes.TryGetValue(type, out var foundType))
+    {
+      if (foundType is null)
+      {
+        throw new ConversionNotSupportedException($"No conversion found for {type.Name}");
+      }
+      return GetConverterByType(foundType)
+        ?? throw new ConversionNotSupportedException($"No conversion found for {type.Name}");
+    }
     while (true)
     {
       var typeName = currentType.Name;
@@ -23,15 +33,18 @@ public class ConverterManager<T>(ConcurrentDictionary<string, Type> converterTyp
 
         if (currentType == null)
         {
+          _foundTypes.Add(type, null);
           throw new ConversionNotSupportedException($"No conversion found for {type.Name} or any of its base types");
         }
       }
       else if (converter is null)
       {
+        _foundTypes.Add(type, null);
         throw new ConversionNotSupportedException($"No conversion found for {type.Name}");
       }
       else
       {
+        _foundTypes.Add(type, typeName);
         return converter;
       }
     }
