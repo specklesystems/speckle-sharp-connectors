4f9ae54e0ca06b11584b53f098d9a3da2ee664f6
Adam Hathcock <adam@hathcock.uk>
2024-12-13 15:27:43 +0000

    remove compile error

--- Connectors/Rhino/Speckle.Connectors.RhinoShared/Tests/EllipseToSpeckleConverterTests.cs
+++ Connectors/Rhino/Speckle.Connectors.RhinoShared/Tests/EllipseToSpeckleConverterTests.cs
@@ -4,9 +4,7 @@ using Xunit;
 
 namespace Speckle.Connectors.Rhino;
 
-#pragma warning disable xUnit1041
 public class EllipseToSpeckleConverterTests(IServiceProvider serviceProvider)
-#pragma warning restore xUnit1041
 {
   [Fact]
   public void Convert_Test()
