0b265a8a79859bc319fb18b0e8378c4358ee7dba
Adam Hathcock <adam@hathcock.uk>
2024-12-16 13:46:36 +0000

    Fix Rhino tests

--- Connectors/Rhino/Speckle.Connectors.RhinoShared/Tests/ArcToSpeckleConverterTests.cs
+++ Connectors/Rhino/Speckle.Connectors.RhinoShared/Tests/ArcToSpeckleConverterTests.cs
@@ -1,37 +1,13 @@
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
-using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
-using Speckle.Converters.Rhino;
-using Speckle.HostApps;
 using Xunit;
 
 namespace Speckle.Connectors.Rhino;
 
-public class RhinoSetup
-{
-  // ReSharper disable once InconsistentNaming
-#pragma warning disable IDE1006
-  public const string RhinoCollection = "Rhino collection";
-#pragma warning restore IDE1006
-  public RhinoSetup(IServiceProvider serviceProvider)
-  {
-    serviceProvider.GetRequiredService<IConverterSettingsStore<RhinoConversionSettings>>().Initialize(new RhinoConversionSettings(null!, "units"));
-  }
-}
-
-[CollectionDefinition(RhinoSetup.RhinoCollection)]
-#pragma warning disable CA1711
-public class RhinoCollection : ICollectionFixture<RhinoSetup>
-#pragma warning restore CA1711
-{
-  // This class has no code, and is never created. Its purpose is simply
-  // to be the place to apply [CollectionDefinition] and all the
-  // ICollectionFixture<> interfaces.
-}
 
 [Collection(RhinoSetup.RhinoCollection)]
-public class ArcToSpeckleConverterTests(IServiceProvider serviceProvider) : MoqTest
+public class ArcToSpeckleConverterTests(IServiceProvider serviceProvider)
 {
   [Fact]
   public void Convert_ShouldConvertArcCorrectly()
---
+++ Connectors/Rhino/Speckle.Connectors.RhinoShared/Tests/RhinoCollection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Speckle.Converters.Common;
+using Speckle.Converters.Rhino;
+using Xunit;
+
+namespace Speckle.Connectors.Rhino;
+
+[CollectionDefinition(RhinoSetup.RhinoCollection)]
+#pragma warning disable CA1711
+public class RhinoCollection : ICollectionFixture<RhinoSetup>
+#pragma warning restore CA1711
+{
+  // This class has no code, and is never created. Its purpose is simply
+  // to be the place to apply [CollectionDefinition] and all the
+  // ICollectionFixture<> interfaces.
+}
+
+public class RhinoSetup
+{
+  // ReSharper disable once InconsistentNaming
+#pragma warning disable IDE1006
+  public const string RhinoCollection = "Rhino collection";
+#pragma warning restore IDE1006
+  public RhinoSetup(IServiceProvider serviceProvider)
+  {
+    serviceProvider.GetRequiredService<IConverterSettingsStore<RhinoConversionSettings>>().Initialize(new RhinoConversionSettings(null!, "units"));
+  }
+}
