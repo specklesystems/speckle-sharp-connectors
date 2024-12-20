9964ec9ff5181fb6de0932cb44b398e0fdaa453b
Jonathon Broughton <jonathon@stardotbmp.com>
2024-12-20 11:50:52 +0000

    Update AppUtils.cs to include support for Navisworks as the host application.
    - Add a new property `App` that returns the host application based on the platform.
    - Update the `Version` property to return the correct version for Navisworks 2020.

--- Connectors/Navisworks/Speckle.Connectors.NavisworksShared/Plugin/AppUtils.cs
+++ Connectors/Navisworks/Speckle.Connectors.NavisworksShared/Plugin/AppUtils.cs
@@ -4,6 +4,12 @@ namespace Speckle.Connector.Navisworks.NavisPlugin;
 
 public static class AppUtils
 {
+  public static HostApplication App =>
+#if NAVIS
+    HostApplications.Navisworks;
+#else
+    throw new NotSupportedException();
+#endif
   public static HostAppVersion Version =>
 #if NAVIS2020
     HostAppVersion.v2020;
