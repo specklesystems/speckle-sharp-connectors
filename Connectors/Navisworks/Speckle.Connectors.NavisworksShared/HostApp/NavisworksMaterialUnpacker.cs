341dc86c2cc694f9a34b57fbcfd4bfc88629b5f0
Jonathon Broughton <jonathon@stardotbmp.com>
2024-12-24 14:40:08 +0000

    Refactor NavisworksMaterialUnpacker to improve color conversion
    - Update NavisworksColorToColor method to include alpha value for transparency
    - Improve code readability by explicitly specifying parameter names in System.Drawing.Color.FromArgb method

--- Connectors/Navisworks/Speckle.Connectors.NavisworksShared/HostApp/NavisworksMaterialUnpacker.cs
+++ Connectors/Navisworks/Speckle.Connectors.NavisworksShared/HostApp/NavisworksMaterialUnpacker.cs
@@ -157,8 +157,9 @@ public class NavisworksMaterialUnpacker(
 
   private static System.Drawing.Color NavisworksColorToColor(NAV.Color color) =>
     System.Drawing.Color.FromArgb(
-      Convert.ToInt32(color.R * 255),
-      Convert.ToInt32(color.G * 255),
-      Convert.ToInt32(color.B * 255)
+      alpha: 255,
+      red: Convert.ToInt32(color.R * 255),
+      green: Convert.ToInt32(color.G * 255),
+      blue: Convert.ToInt32(color.B * 255)
     );
 }
