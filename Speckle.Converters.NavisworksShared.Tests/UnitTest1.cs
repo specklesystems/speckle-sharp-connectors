7cc48cc445c11b9f3d4f27d706cfefef94b10b2a
Jonathon Broughton <jonathon@stardotbmp.com>
2025-01-14 00:34:49 +0000

    Add packages for unit testing in NavisworksShared and NavsiworksShared.Tests

---
+++ Speckle.Converters.NavisworksShared.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Speckle.Converters.NavisworksShared.Tests;
+
+public class ConverterTests
+{
+  [SetUp]
+  public void Setup() { }
+
+  [Test]
+  public void Test1()
+  {
+    Assert.Pass();
+  }
+}
