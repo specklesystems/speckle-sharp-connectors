8be8cc645cd21442b3e354ef9ceafec4ef042363
Jonathon Broughton <jonathon@stardotbmp.com>
2025-01-18 22:36:31 +0000

    Remove debug code and comments in Utilities.cs
    - Remove debug code that prints available plugins to the Debug output window
    - Remove unused using statement for System.Text

--- Connectors/Navisworks/Speckle.Connectors.NavisworksShared/Plugin/Utilities.cs
+++ Connectors/Navisworks/Speckle.Connectors.NavisworksShared/Plugin/Utilities.cs
@@ -1,9 +1,6 @@
 ﻿using System.Windows.Forms;
 using Autodesk.Navisworks.Api.Plugins;
 using Speckle.Connector.Navisworks.Plugin.Tools;
-#if DEBUG
-using System.Text;
-#endif
 
 namespace Speckle.Connector.Navisworks.Plugin;
 
@@ -16,15 +13,6 @@ internal static class PluginUtilities
   {
     var pluginRecords = NavisworksApp.Plugins.PluginRecords;
     var v2Plugin = pluginRecords.FirstOrDefault(p => p.Id == SpeckleV2Tool.RIBBON_TAB_ID);
-
-#if DEBUG
-    var sb = new StringBuilder("Available Plugins:\n");
-    foreach (var plugin in pluginRecords)
-    {
-      sb.AppendLine($"Name: {plugin.Name}, Type: {plugin.GetType().Name}, Id: {plugin.Id}");
-    }
-    System.Diagnostics.Debug.WriteLine(sb.ToString());
-#endif
 
     return v2Plugin ?? null;
   }
