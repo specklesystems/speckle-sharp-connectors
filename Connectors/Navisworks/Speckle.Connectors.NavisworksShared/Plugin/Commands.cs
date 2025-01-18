c27ade0d2cc7ca0d745cb6b8b92831aad11ab855
Jonathon Broughton <jonathon@stardotbmp.com>
2025-01-18 16:26:17 +0000

    Delete AppUtils.cs and Commands.cs, add Utilities.cs
    - Delete AppUtils.cs and Commands.cs (Replaced by v2 and v3 Tools)
    - Add Utilities.cs for plugin utilities functionality

--- Connectors/Navisworks/Speckle.Connectors.NavisworksShared/Plugin/Commands.cs
+++
@@ -1,9 +0,0 @@
-namespace Speckle.Connector.Navisworks.Plugin;
-
-public static class LaunchSpeckleConnector
-{
-  public const string COMMAND = "Speckle_Launch";
-  public const string PLUGIN = "SpeckleUI3";
-  public const string COMMAND_V2 = "Speckle_Launch_V2";
-  public const string PLUGIN_V2 = "SpeckleUI2";
-}
---
+++ Connectors/Navisworks/Speckle.Connectors.NavisworksShared/Plugin/Utilities.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows.Forms;
+using Autodesk.Navisworks.Api.Plugins;
+using Speckle.Connector.Navisworks.Plugin.Tools;
+
+namespace Speckle.Connector.Navisworks.Plugin;
+
+internal static class PluginUtilities
+{
+  public static bool ShouldSkipLoad(string plugin, string command, bool notAutomatedCheck) =>
+    notAutomatedCheck && NavisworksApp.IsAutomated || string.IsNullOrEmpty(plugin) || string.IsNullOrEmpty(command);
+
+  internal static PluginRecord? FindV2Plugin()
+  {
+    var pluginRecords = NavisworksApp.Plugins.PluginRecords;
+    var v2Plugin = pluginRecords.FirstOrDefault(p => p.Id == SpeckleV2Tool.RIBBON_TAB_ID);
+
+#if DEBUG
+    var sb = new StringBuilder("Available Plugins:\n");
+    foreach (var plugin in pluginRecords)
+    {
+      sb.AppendLine($"Name: {plugin.Name}, Type: {plugin.GetType().Name}, Id: {plugin.Id}");
+    }
+    System.Diagnostics.Debug.WriteLine(sb.ToString());
+#endif
+
+    return v2Plugin ?? null;
+  }
+
+  public static void ActivatePluginPane(PluginRecord? pluginRecord, string command)
+  {
+    if (pluginRecord is null || !pluginRecord.IsEnabled || !pluginRecord.IsLoaded)
+    {
+#if DEBUG
+      MessageBox.Show($"Command '{command}' cannot activate plugin pane. Plugin state invalid.");
+#endif
+      return;
+    }
+
+    if (pluginRecord.LoadedPlugin is DockPanePlugin dockPanePlugin)
+    {
+      dockPanePlugin.ActivatePane();
+    }
+  }
+}
