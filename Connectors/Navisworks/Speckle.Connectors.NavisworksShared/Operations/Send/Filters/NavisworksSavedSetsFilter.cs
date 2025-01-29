98118c32597e912e5fc37c77ef7b3ae01714de5d
Jonathon Broughton <jonathon@stardotbmp.com>
2025-01-29 09:29:35 +0100

    !

--- Connectors/Navisworks/Speckle.Connectors.NavisworksShared/Operations/Send/Filters/NavisworksSavedSetsFilter.cs
+++ Connectors/Navisworks/Speckle.Connectors.NavisworksShared/Operations/Send/Filters/NavisworksSavedSetsFilter.cs
@@ -81,7 +81,8 @@ public class NavisworksSavedSetsFilter : DiscriminatedObject, ISendFilter
   private void GetSavedSets()
   {
     List<NAV.SavedItem> savedSetRecords = NavisworksApp
-      .ActiveDocument.SelectionSets.RootItem.Children.Where(set => set.IsGroup).ToList();
+      .ActiveDocument.SelectionSets.RootItem.Children.Where(set => set.IsGroup)
+      .ToList();
 
     AvailableSavedSets = savedSetRecords.Select(setRecord =>
     {
