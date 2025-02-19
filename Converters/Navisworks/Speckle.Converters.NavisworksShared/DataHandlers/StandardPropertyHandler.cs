4c450448bf646424876750b65995eee6ef5777bd
Jonathon Broughton <jonathon@stardotbmp.com>
2025-02-19 19:24:30 +0000

    Refactor property handling methods
    - Removed the AssignPropertySets method for simplification.
    - Streamlined GetProperties to directly return processed property sets.
    - Cleaned up unnecessary checks and loops for better readability.

--- Converters/Navisworks/Speckle.Converters.NavisworksShared/DataHandlers/StandardPropertyHandler.cs
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/DataHandlers/StandardPropertyHandler.cs
@@ -8,53 +8,5 @@ public class StandardPropertyHandler(
   ModelPropertiesExtractor modelPropertiesExtractor
 ) : BasePropertyHandler(propertySetsExtractor, modelPropertiesExtractor)
 {
-  protected override void AssignPropertySets(SSM.Base speckleObject, NAV.ModelItem modelItem)
-  {
-    if (speckleObject == null)
-    {
-      throw new ArgumentNullException(nameof(speckleObject));
-    }
-    var propertyDictionary = speckleObject["properties"] as Dictionary<string, object?> ?? [];
-
-    var categoryDictionaries = ProcessPropertySets(modelItem);
-
-    foreach (var kvp in categoryDictionaries)
-    {
-      categoryDictionaries[$"{kvp.Key}"] = kvp.Value;
-    }
-
-    speckleObject["properties"] = propertyDictionary;
-  }
-
-  public override Dictionary<string, object?> GetProperties(NAV.ModelItem modelItem)
-  {
-    Dictionary<string, object?> categoryDictionaries = ProcessPropertySets(modelItem);
-
-    foreach (var kvp in categoryDictionaries)
-    {
-      categoryDictionaries[$"{kvp.Key}"] = kvp.Value;
-    }
-
-    var classProperties = ClassPropertiesExtractor.GetClassProperties(modelItem);
-
-    // for each of the the keys of classProperties, add them to the propertyDict
-    if (classProperties == null)
-    {
-      return categoryDictionaries;
-    }
-
-    foreach (var kvp in classProperties)
-    {
-      if (categoryDictionaries.ContainsKey(kvp.Key))
-      {
-        categoryDictionaries[kvp.Key] = kvp.Value;
-      }
-      else
-      {
-        categoryDictionaries.Add(kvp.Key, kvp.Value);
-      }
-    }
-
-    return categoryDictionaries;
-  }
+  public override Dictionary<string, object?> GetProperties(NAV.ModelItem modelItem) => ProcessPropertySets(modelItem);
 }
