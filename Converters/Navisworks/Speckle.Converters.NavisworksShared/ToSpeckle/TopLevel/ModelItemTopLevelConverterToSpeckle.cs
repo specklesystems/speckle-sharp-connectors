233f56b0a9bb824d69e572f79e5624f1fee943d2
Jonathon Broughton <jonathon@stardotbmp.com>
2025-02-19 15:54:20 +0000

    Fix import statement formatting
    Updated the import statement for consistency by removing an invisible character.

--- Converters/Navisworks/Speckle.Converters.NavisworksShared/ToSpeckle/TopLevel/ModelItemTopLevelConverterToSpeckle.cs
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/ToSpeckle/TopLevel/ModelItemTopLevelConverterToSpeckle.cs
@@ -1,4 +1,4 @@
-using Speckle.Converter.Navisworks.Settings;
+﻿using Speckle.Converter.Navisworks.Settings;
 using Speckle.Converter.Navisworks.ToSpeckle.PropertyHandlers;
 using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
