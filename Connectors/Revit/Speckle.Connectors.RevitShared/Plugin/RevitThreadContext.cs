a285b72de68bec20042f522640a6c27f29957ede
Adam Hathcock <adam@hathcock.uk>
2025-02-24 14:42:24 +0000

    Catch exceptions and reprocess them around RevitTask

--- Connectors/Revit/Speckle.Connectors.RevitShared/Plugin/RevitThreadContext.cs
+++ Connectors/Revit/Speckle.Connectors.RevitShared/Plugin/RevitThreadContext.cs
@@ -1,5 +1,6 @@
 using Revit.Async;
 using Speckle.Connectors.Common.Threading;
+using Speckle.Sdk;
 
 namespace Speckle.Connectors.Revit.Plugin;
 
@@ -7,16 +8,85 @@ public class RevitThreadContext : ThreadContext
 {
   protected override Task<T> MainToWorkerAsync<T>(Func<Task<T>> action) => action();
 
-  protected override Task<T> WorkerToMainAsync<T>(Func<Task<T>> action) =>
-    RevitTask.RunAsync(async () => await action());
+  protected override Task<T> WorkerToMainAsync<T>(Func<Task<T>> action) => CatchExceptions(action);
 
   protected override Task<T> MainToWorker<T>(Func<T> action) => Task.FromResult(action());
 
-  protected override Task<T> WorkerToMain<T>(Func<T> action) => RevitTask.RunAsync(action);
+  protected override Task<T> WorkerToMain<T>(Func<T> action) => CatchExceptions(action);
 
-  protected override Task RunMainAsync(Func<Task> action) => RevitTask.RunAsync(action);
+  protected override Task RunMainAsync(Func<Task> action) => CatchExceptions(action);
 
-  protected override Task<T> RunMainAsync<T>(Func<T> action) => RevitTask.RunAsync(action);
+  protected override Task<T> RunMainAsync<T>(Func<T> action) => CatchExceptions(action);
 
-  protected override Task<T> RunMainAsync<T>(Func<Task<T>> action) => RevitTask.RunAsync(action);
+  protected override Task<T> RunMainAsync<T>(Func<Task<T>> action) => CatchExceptions(action);
+
+  private static async Task<T> CatchExceptions<T>(Func<T> action)
+  {
+    Exception? ex = null;
+    var ret = await RevitTask.RunAsync(_ =>
+    {
+      try
+      {
+        return action();
+      }
+#pragma warning disable CA1031
+      catch (Exception e)
+#pragma warning restore CA1031
+      {
+        ex = e;
+        return default;
+      }
+    });
+    if (ex is not null)
+    {
+      throw new SpeckleException("Revit operation failed", ex);
+    }
+    return ret!;
+  }
+
+  private static async Task<T> CatchExceptions<T>(Func<Task<T>> action)
+  {
+    Exception? ex = null;
+    var ret = await RevitTask.RunAsync(async _ =>
+    {
+      try
+      {
+        return await action();
+      }
+#pragma warning disable CA1031
+      catch (Exception e)
+#pragma warning restore CA1031
+      {
+        ex = e;
+        return default;
+      }
+    });
+    if (ex is not null)
+    {
+      throw new SpeckleException("Revit operation failed", ex);
+    }
+    return ret!;
+  }
+
+  private static async Task CatchExceptions(Func<Task> action)
+  {
+    Exception? ex = null;
+    await RevitTask.RunAsync(async _ =>
+    {
+      try
+      {
+        await action();
+      }
+#pragma warning disable CA1031
+      catch (Exception e)
+#pragma warning restore CA1031
+      {
+        ex = e;
+      }
+    });
+    if (ex is not null)
+    {
+      throw new SpeckleException("Revit operation failed", ex);
+    }
+  }
 }
