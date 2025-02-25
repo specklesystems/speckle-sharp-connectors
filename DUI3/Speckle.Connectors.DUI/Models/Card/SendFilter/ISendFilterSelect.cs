ba8e5eaf45dd8d5a1cd719c69cd4dfaa3d48d6b4
oguzhankoral <oguzhankoral@gmail.com>
2025-02-25 13:41:07 +0300

    Add notes

--- DUI3/Speckle.Connectors.DUI/Models/Card/SendFilter/ISendFilterSelect.cs
+++ DUI3/Speckle.Connectors.DUI/Models/Card/SendFilter/ISendFilterSelect.cs
@@ -2,6 +2,9 @@ namespace Speckle.Connectors.DUI.Models.Card.SendFilter;
 
 public record SendFilterSelectItem(string Id, string Name);
 
+/// <summary>
+/// UI data type to make have FilterFormSelect component as send filter.
+/// </summary>
 public interface ISendFilterSelect : ISendFilter
 {
   public bool IsMultiSelectable { get; set; }
