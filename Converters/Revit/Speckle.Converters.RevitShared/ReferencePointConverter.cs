6d2c7e1bc0c344fa5b333cadf21bd0a883f66483
Adam Hathcock <adam@hathcock.uk>
2025-03-03 11:53:14 +0000

    change some revit files

--- Converters/Revit/Speckle.Converters.RevitShared/ReferencePointConverter.cs
+++ Converters/Revit/Speckle.Converters.RevitShared/ReferencePointConverter.cs
@@ -1,5 +1,6 @@
 using Speckle.Converters.Common;
 using Speckle.Converters.RevitShared.Settings;
+using Speckle.InterfaceGenerator;
 
 namespace Speckle.Converters.RevitShared.Helpers;
 
@@ -7,14 +8,17 @@ namespace Speckle.Converters.RevitShared.Helpers;
 /// POC: reference point functionality needs to be revisited (we are currently baking in these transforms into all geometry using the point and vector converters, and losing the transform).
 /// This converter uses the transform in the reference point setting and provides methods to transform points
 /// </summary>
+[GenerateAutoInterface]
 public class ReferencePointConverter(IConverterSettingsStore<RevitConversionSettings> converterSettings)
   : IReferencePointConverter
 {
   public DB.XYZ ConvertToExternalCoordinates(DB.XYZ p, bool isPoint)
   {
-    if (converterSettings.Current.ReferencePointTransform is DB.Transform transform)
+    if (converterSettings.Current.ReferencePointTransform is not null)
     {
-      return isPoint ? transform.Inverse.OfPoint(p) : transform.Inverse.OfVector(p);
+      return isPoint
+        ? converterSettings.Current.ReferencePointTransform.Inverse.OfPoint(p)
+        : converterSettings.Current.ReferencePointTransform.Inverse.OfVector(p);
     }
 
     return p;
@@ -22,9 +26,11 @@ public class ReferencePointConverter(IConverterSettingsStore<RevitConversionSett
 
   public DB.XYZ ConvertToInternalCoordinates(DB.XYZ p, bool isPoint)
   {
-    if (converterSettings.Current.ReferencePointTransform is DB.Transform transform)
+    if (converterSettings.Current.ReferencePointTransform is not null)
     {
-      return isPoint ? transform.OfPoint(p) : transform.OfVector(p);
+      return isPoint
+        ? converterSettings.Current.ReferencePointTransform.OfPoint(p)
+        : converterSettings.Current.ReferencePointTransform.OfVector(p);
     }
 
     return p;
