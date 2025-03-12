ae4b1b0ab5a0fc09a374a17fbf841e37695a97d5
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-03-12 08:32:30 +0000

    Merge pull request #655 from specklesystems/jrm/ifc-collections-data-objects
    Send IfcProjects, IfcSites, IfcBuildings, and IfcStoreys as Collections

---
+++ Importers/Ifc/Speckle.Importers.Ifc/Ara3D.IfcParser/Schema/IfcProject.cs
@@ -0,0 +1,10 @@
+﻿using Speckle.Importers.Ifc.Ara3D.StepParser;
+
+namespace Speckle.Importers.Ifc.Ara3D.IfcParser.Schema;
+
+public sealed class IfcProject(IfcGraph graph, StepInstance lineData) : IfcNode(graph, lineData)
+{
+  public string? ObjectType => (LineData[5] as StepString)?.AsString();
+  public string? LongName => (LineData[6] as StepString)?.AsString();
+  public string? Phase => (LineData[7] as StepString)?.AsString();
+}
