6228d4ac921664de9285952bf455445c22dfed5e
Adam Hathcock <adam@hathcock.uk>
2025-03-14 15:45:13 +0000

    try dependencies again

--- Build/Program.cs
+++ Build/Program.cs
@@ -134,7 +134,7 @@ Target(
 
 Target(
   DETECT_AFFECTED,
-  DependsOn(FORMAT),
+  DependsOn(RESTORE_TOOLS),
   await Affected.GetAffectedProjectGroups(),
   group =>
   {
@@ -153,7 +153,7 @@ Target(
 
 Target(
   RESTORE,
-  DependsOn(DETECT_AFFECTED),
+  DependsOn(FORMAT, DETECT_AFFECTED),
   Consts.Solutions,
   async s =>
   {
