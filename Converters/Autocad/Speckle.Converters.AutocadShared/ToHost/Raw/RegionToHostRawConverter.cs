50862a74ceff698fd681c04f61b0d75b0c0da767
KatKatKateryna <kateryna@speckle.systems>
2025-03-17 04:53:21 +0000

    substract regions on receive

--- Converters/Autocad/Speckle.Converters.AutocadShared/ToHost/Raw/RegionToHostRawConverter.cs
+++ Converters/Autocad/Speckle.Converters.AutocadShared/ToHost/Raw/RegionToHostRawConverter.cs
@@ -21,31 +21,52 @@ public class RegionToHostRawConverter : ITypedConverter<SOG.Region, ADB.Region>
 
   public ADB.Region Convert(SOG.Region target)
   {
-    // The curveSegments must contain only Line, Arc, Ellipse, Circle, Spline, Polyline3d, or Polyline2d objects.
+    // Notes: The curveSegments must contain only Line, Arc, Ellipse, Circle, Spline, Polyline3d, or Polyline2d objects.
     // The objects in curveSegments must be opened for read and not for write. If the objects are opened, calling this function will crash AutoCAD.
 
-    // Add coverted boundary and loops segments to the segmentCollection
-    var segmentCollection = new List<ADB.Curve>();
-    segmentCollection.AddRange(ConvertICurveToSegments(target.boundary));
+    // Add converted boundary to the segmentCollection
+    var boundarySegmentCollection = new List<ADB.Curve>();
+    boundarySegmentCollection.AddRange(ConvertICurveToSegments(target.boundary));
 
+    // Add converted loops to the list if segmentCollections
+    var loopsSegmentCollection = new List<List<ADB.Curve>>();
     foreach (var loop in target.innerLoops)
     {
-      segmentCollection.AddRange(ConvertICurveToSegments(loop));
+      loopsSegmentCollection.Add(ConvertICurveToSegments(loop));
     }
 
-    // add all segments to the ADB.DBObjectCollection
-    ADB.DBObjectCollection acDBObjColl = new();
-    foreach (var segment in segmentCollection)
-    {
-      acDBObjColl.Add(segment);
-    }
+    // add all boundary segments to the ADB.DBObjectCollection
+    ADB.DBObjectCollection boundaryDBObjColl = new();
+    boundarySegmentCollection.ForEach(x => boundaryDBObjColl.Add(x));
 
-    // Calculate the regions based on each closed loop
-    // https://help.autodesk.com/view/OARX/2025/ENU/?guid=GUID-9CD22AE5-8F66-4925-A155-95852BAFD565
-    using (ADB.DBObjectCollection myRegionColl = ADB.Region.CreateFromCurves(acDBObjColl))
+    // Calculate the outer region, method should return an array with 1 region
+    // https://help.autodesk.com/view/OARX/2025/ENU/?guid=GUID-684E602E-3555-4370-BCDC-1CE594676C43
+    using (ADB.DBObjectCollection outerRegionColl = ADB.Region.CreateFromCurves(boundaryDBObjColl))
     {
-      if (myRegionColl[0] is ADB.Region adbRegion)
+      if (outerRegionColl[0] is ADB.Region adbRegion)
       {
+        // Create and subtract the inner loops regions
+        // iterate through each loop
+        foreach (var loopSegmentCollection in loopsSegmentCollection)
+        {
+          // add loop segments to the ADB.DBObjectCollection
+          ADB.DBObjectCollection loopDBObjColl = new();
+          foreach (var segment in loopSegmentCollection)
+          {
+            loopDBObjColl.Add(segment);
+          }
+
+          // Same as above: calculate the inner region, method should return an array with 1 region
+          using (ADB.DBObjectCollection innerRegionColl = ADB.Region.CreateFromCurves(loopDBObjColl))
+          {
+            if (innerRegionColl[0] is ADB.Region adbInnerRegion)
+            {
+              adbRegion.BooleanOperation(ADB.BooleanOperationType.BoolSubtract, adbInnerRegion);
+              adbInnerRegion.Dispose();
+            }
+          }
+        }
+
         return adbRegion;
       }
     }
