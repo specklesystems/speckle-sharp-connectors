395569f1da70d46942c0ca4e43975881a11a6918
KatKatKateryna <kateryna@speckle.systems>
2025-03-18 03:18:00 +0000

    send meshes

--- Converters/Rhino/Speckle.Converters.RhinoShared/ToSpeckle/Raw/HatchToSpeckleConverter.cs
+++ Converters/Rhino/Speckle.Converters.RhinoShared/ToSpeckle/Raw/HatchToSpeckleConverter.cs
@@ -1,20 +1,24 @@
 ﻿using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
 using Speckle.Objects;
+using Speckle.Sdk;
 
 namespace Speckle.Converters.Rhino.ToSpeckle.Raw;
 
 public class HatchToSpeckleConverter : ITypedConverter<RG.Hatch, SOG.Region>
 {
   private readonly ITypedConverter<RG.Curve, ICurve> _curveConverter;
+  private readonly ITypedConverter<RG.Mesh, SOG.Mesh> _meshConverter;
   private readonly IConverterSettingsStore<RhinoConversionSettings> _settingsStore;
 
   public HatchToSpeckleConverter(
     ITypedConverter<RG.Curve, ICurve> curveConverter,
+    ITypedConverter<RG.Mesh, SOG.Mesh> meshConverter,
     IConverterSettingsStore<RhinoConversionSettings> settingsStore
   )
   {
     _curveConverter = curveConverter;
+    _meshConverter = meshConverter;
     _settingsStore = settingsStore;
   }
 
@@ -32,12 +36,38 @@ public class HatchToSpeckleConverter : ITypedConverter<RG.Hatch, SOG.Region>
     ICurve boundary = _curveConverter.Convert(rhinoBoundary);
     List<ICurve> innerLoops = rhinoLoops.Select(x => _curveConverter.Convert(x)).ToList();
 
+    // create display mesh from region loops
+    var brep = RG.Brep.TryConvertBrep(target);
+    var displayMesh = GetBrepDisplayMesh(brep);
+    var displayValue = new List<SOG.Mesh>();
+    if (displayMesh != null)
+    {
+      displayValue.Add(_meshConverter.Convert(displayMesh));
+    }
+
     return new SOG.Region
     {
       boundary = boundary,
       innerLoops = innerLoops,
       hasHatchPattern = true,
       units = _settingsStore.Current.SpeckleUnits,
+      displayValue = displayValue
     };
+  }
+
+  private RG.Mesh? GetBrepDisplayMesh(RG.Brep brep)
+  {
+    var joinedMesh = new RG.Mesh();
+    RG.MeshingParameters mySettings = new(0.05, 0.05);
+
+    try
+    {
+      joinedMesh.Append(RG.Mesh.CreateFromBrep(brep, mySettings));
+      return joinedMesh;
+    }
+    catch (Exception ex) when (!ex.IsFatal())
+    {
+      return null;
+    }
   }
 }
