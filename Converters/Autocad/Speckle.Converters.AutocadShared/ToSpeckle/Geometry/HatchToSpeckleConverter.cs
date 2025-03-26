124a026e9549b4efbe8df1d9d39ec208821836c6
KatKatKateryna <kateryna@speckle.systems>
2025-03-26 00:07:38 +0000

    turned 2d Hatch curves into 3d - now Brep creation doesn't fail!

--- Converters/Autocad/Speckle.Converters.AutocadShared/ToSpeckle/Geometry/HatchToSpeckleConverter.cs
+++ Converters/Autocad/Speckle.Converters.AutocadShared/ToSpeckle/Geometry/HatchToSpeckleConverter.cs
@@ -7,15 +7,10 @@ namespace Speckle.Converters.Autocad.Geometry;
 [NameAndRankValue(typeof(ADB.Hatch), NameAndRankValueAttribute.SPECKLE_DEFAULT_RANK)]
 public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConverter<ADB.Hatch, SOG.Region>
 {
-  private readonly ITypedConverter<ABR.Brep, SOG.Mesh> _brepConverter;
   private readonly ITypedConverter<ADB.Region, SOG.Region> _regionConverter;
 
-  public HatchToSpeckleConverter(
-    ITypedConverter<ABR.Brep, SOG.Mesh> brepConverter,
-    ITypedConverter<ADB.Region, SOG.Region> regionConverter
-  )
+  public HatchToSpeckleConverter(ITypedConverter<ADB.Region, SOG.Region> regionConverter)
   {
-    _brepConverter = brepConverter;
     _regionConverter = regionConverter;
   }
 
@@ -24,43 +19,33 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
   public SOG.Region Convert(ADB.Hatch target)
   {
     ADB.DBObjectCollection objCollection = new();
-    // target.Explode method is failing in runtime. e.g. target.Explode(objCollection);
     for (int i = 0; i < target.NumberOfLoops; i++)
     {
       ADB.HatchLoop loop = target.GetLoopAt(i);
-      ADB.Polyline polyline = PolylineFromLoop(loop);
+
+      ADB.Polyline3d polyline = PolylineFromLoop(loop);
       objCollection.Add(polyline);
     }
 
+    // Create Regions from every curve. They are NOT ordered (i.e. first doesn't mean external)
     using (ADB.DBObjectCollection regionCollection = ADB.Region.CreateFromCurves(objCollection))
     {
       if (regionCollection.Count > 1)
       {
         for (int i = 1; i < regionCollection.Count; i++)
         {
+          if (i > 0)
+          {
+            // throw new ConversionException("Composite Hatches are not supported");
+          }
           ADB.Region innerRegion = (ADB.Region)regionCollection[i];
           // substract region from Boundary region
-          ((ADB.Region)regionCollection[0]).BooleanOperation(ADB.BooleanOperationType.BoolSubtract, innerRegion);
+          //((ADB.Region)regionCollection[0]).BooleanOperation(ADB.BooleanOperationType.BoolSubtract, innerRegion);
           innerRegion.Dispose();
         }
       }
 
-      //if (regionCollection.Count != 1)
-      //{
-      //  throw new ConversionException("Composite Hatches are not supported");
-      //}
       ADB.Region adbRegion = (ADB.Region)regionCollection[0];
-      /*
-      using ABR.Brep brep = new(adbRegion);
-      if (brep.IsNull)
-      {
-        throw new ConversionException("Could not retrieve brep from the hatch.");
-      }
-      // convert and store Meshes
-      SOG.Mesh mesh = _brepConverter.Convert(brep);
-      mesh.area = adbRegion.Area;
-      displayValue.Add(mesh);
-      */
 
       // convert and store Regions
       SOG.Region convertedRegion = _regionConverter.Convert(adbRegion);
@@ -83,17 +68,24 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
     return z > 0 ? -1 : 1;
   }
 
-  private ADB.Polyline PolylineFromLoop(ADB.HatchLoop loop)
+  private ADB.Polyline3d PolylineFromLoop(ADB.HatchLoop loop)
   {
     // initialize loop Polyline
-    ADB.Polyline polyline = new() { Closed = true };
+    AG.Point3dCollection vertices = new();
+    //ADB.Polyline polyline = new() { Closed = true };
+
     int count = 0;
     if (loop.IsPolyline)
     {
       foreach (ADB.BulgeVertex bVertex in loop.Polyline)
       {
-        polyline.AddVertexAt(count, bVertex.Vertex, bVertex.Bulge, 0, 0);
-        count++;
+        // last point will repeat the first, ignore it
+        if (count < loop.Polyline.Count - 1)
+        {
+          //polyline.AddVertexAt(count, bVertex.Vertex.Y, bVertex.Bulge, 0, 0);
+          vertices.Add(new AG.Point3d(bVertex.Vertex.X, bVertex.Vertex.Y, 0));
+          count++;
+        }
       }
     }
     else
@@ -103,8 +95,9 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
         switch (loopCurve)
         {
           case AG.LineSegment2d line:
-            polyline.AddVertexAt(count, line.StartPoint, 0, 0, 0);
-            count++;
+            //polyline.AddVertexAt(count, line.StartPoint, 0, 0, 0);
+            vertices.Add(new AG.Point3d(line.StartPoint.X, line.StartPoint.Y, 0));
+            //count++;
             break;
           case AG.CircularArc2d arc:
             AG.Point2d midPoint = arc.EvaluatePoint(arc.StartAngle + (arc.EndAngle - arc.StartAngle) / 2);
@@ -116,16 +109,18 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
               throw new ArgumentOutOfRangeException(nameof(loop), "Cannot convert arc with measure <= 0 or >= 2 pi");
             }
 
-            var bulge = Math.Tan(measure / 4) * BulgeDirection(arc.StartPoint, midPoint, arc.EndPoint);
-            polyline.AddVertexAt(count, arc.StartPoint, 0, 0, 0);
+            //var bulge = Math.Tan(measure / 4) * BulgeDirection(arc.StartPoint, midPoint, arc.EndPoint);
+            //polyline.AddVertexAt(count, arc.StartPoint, 0, 0, 0);
+            vertices.Add(new AG.Point3d(arc.StartPoint.X, arc.StartPoint.Y, 0));
             count++;
-            polyline.AddVertexAt(count, midPoint, bulge, 0, 0);
+            //polyline.AddVertexAt(count, midPoint, bulge, 0, 0);
+            vertices.Add(new AG.Point3d(midPoint.X, midPoint.Y, 0));
             count++;
             break;
         }
       }
     }
-
+    var polyline = new ADB.Polyline3d(ADB.Poly3dType.SimplePoly, vertices, true);
     return polyline;
   }
 }
