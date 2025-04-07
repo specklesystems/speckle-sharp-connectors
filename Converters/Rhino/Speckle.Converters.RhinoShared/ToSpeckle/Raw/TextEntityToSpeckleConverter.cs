4d0beaa6e2e1e4d1617cf01061ec7fdf67b55d0e
KatKatKateryna <kateryna@speckle.systems>
2025-04-07 10:07:45 +0100

    support TextEntity

---
+++ Converters/Rhino/Speckle.Converters.RhinoShared/ToSpeckle/Raw/TextEntityToSpeckleConverter.cs
@@ -0,0 +1,37 @@
+﻿using Speckle.Converters.Common;
+using Speckle.Converters.Common.Objects;
+
+namespace Speckle.Converters.Rhino.ToSpeckle.Raw;
+
+public class TextEntityToSpeckleConverter : ITypedConverter<RG.TextEntity, SO.Text>
+{
+  private readonly ITypedConverter<RG.Point3d, SOG.Point> _pointConverter;
+  private readonly ITypedConverter<RG.Plane, SOG.Plane> _planeConverter;
+  private readonly IConverterSettingsStore<RhinoConversionSettings> _settingsStore;
+
+  public TextEntityToSpeckleConverter(
+    ITypedConverter<RG.Point3d, SOG.Point> pointConverter,
+    ITypedConverter<RG.Plane, SOG.Plane> planeConverter,
+    IConverterSettingsStore<RhinoConversionSettings> settingsStore
+  )
+  {
+    _pointConverter = pointConverter;
+    _planeConverter = planeConverter;
+    _settingsStore = settingsStore;
+  }
+
+  /// <summary>
+  /// Converts a Rhino TextEntity to a Speckle Text object.
+  /// </summary>
+  /// <param name="target">The Rhino TextEntity to convert.</param>
+  /// <returns>The converted Speckle Text object.</returns>
+  public SO.Text Convert(RG.TextEntity target) =>
+    new()
+    {
+      value = target.PlainText,
+      height = target.TextHeight,
+      origin = _pointConverter.Convert(target.Plane.Origin),
+      plane = _planeConverter.Convert(target.Plane),
+      units = _settingsStore.Current.SpeckleUnits
+    };
+}
---
+++ Converters/Rhino/Speckle.Converters.RhinoShared/ToSpeckle/TopLevel/TextObjectToSpeckleTopLevelConverter.cs
@@ -0,0 +1,15 @@
+﻿using Rhino.DocObjects;
+using Speckle.Converters.Common;
+using Speckle.Converters.Common.Objects;
+
+namespace Speckle.Converters.Rhino.ToSpeckle.TopLevel;
+
+[NameAndRankValue(typeof(TextObject), NameAndRankValueAttribute.SPECKLE_DEFAULT_RANK)]
+public class TextObjectToSpeckleTopLevelConverter
+  : RhinoObjectToSpeckleTopLevelConverter<TextObject, RG.TextEntity, SO.Text>
+{
+  public TextObjectToSpeckleTopLevelConverter(ITypedConverter<RG.TextEntity, SO.Text> conversion)
+    : base(conversion) { }
+
+  protected override RG.TextEntity GetTypedGeometry(TextObject input) => input.TextGeometry;
+}
