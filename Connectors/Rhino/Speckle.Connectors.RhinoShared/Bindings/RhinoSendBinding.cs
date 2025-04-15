a8974d73c44ef499821b87791542c0675e4d9bb8
Adam Hathcock <adam@hathcock.uk>
2025-04-15 11:21:21 +0100

    fmt

--- Connectors/Rhino/Speckle.Connectors.RhinoShared/Bindings/RhinoSendBinding.cs
+++ Connectors/Rhino/Speckle.Connectors.RhinoShared/Bindings/RhinoSendBinding.cs
@@ -121,19 +121,19 @@ public sealed class RhinoSendBinding : ISendBinding
     };
 
     // NOTE: BE CAREFUL handling things in this event handler since it is triggered whenever we save something into file!
-    RhinoDoc.DocumentPropertiesChanged +=  (_, e) =>
+    RhinoDoc.DocumentPropertiesChanged += (_, e) =>
     {
-      _topLevelExceptionHandler.CatchUnhandledAsync(async () => { 
-    
-      var newUnit = e.Document.ModelUnitSystem;
-      if (newUnit != PreviousUnitSystem)
+      _topLevelExceptionHandler.CatchUnhandledAsync(async () =>
       {
-        PreviousUnitSystem = newUnit;
+        var newUnit = e.Document.ModelUnitSystem;
+        if (newUnit != PreviousUnitSystem)
+        {
+          PreviousUnitSystem = newUnit;
 
-        await InvalidateAllSender();
-      }
-    });
-  };
+          await InvalidateAllSender();
+        }
+      });
+    };
 
     RhinoDoc.AddRhinoObject += (_, e) =>
       _topLevelExceptionHandler.CatchUnhandled(() =>
