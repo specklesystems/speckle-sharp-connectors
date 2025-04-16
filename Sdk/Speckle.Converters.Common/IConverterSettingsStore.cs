2f8f0d0f6fcb1f0ca881b05c3507dd70d3588d2d
Adam Hathcock <adamhathcock@users.noreply.github.com>
2025-04-16 16:58:22 +0100

    test: add tests for receive operation (#661)
    * add tests for receive operation
    
    * clean up some items and tests
    
    * First initialization changes for SDK 3.2
    
    * Update to SDK 3.2
    
    * merge fixes
    
    * fmt
    
    * ifc changes
    
    * fix tests and remove fakes again
    
    * fmt
    
    * remove extra tests from local sln
    
    * update locks for 3.2
    
    * fix mismerge
    
    ---------
    
    Co-authored-by: Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>

--- Sdk/Speckle.Converters.Common/IConverterSettingsStore.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Speckle.Converters.Common;
-
-public interface IConverterSettingsStore<T>
-  where T : class
-{
-  T Current { get; }
-  IDisposable Push(Func<T, T> nextContext);
-  void Initialize(T context);
-}
