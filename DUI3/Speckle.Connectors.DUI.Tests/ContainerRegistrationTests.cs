3e8a25585feb49a0f74f7ec7a2cf23ad78e549ab
Adam Hathcock <adam@hathcock.uk>
2025-04-23 16:58:34 +0100

    add two initial tests that don't work

---
+++ DUI3/Speckle.Connectors.DUI.Tests/ContainerRegistrationTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Speckle.Connectors.Common.Threading;
+using Speckle.Connectors.DUI.Models;
+using Speckle.Sdk;
+
+namespace Speckle.Connectors.DUI.Tests;
+
+public class ServiceRegistrationTests
+{
+  [Test]
+  public void RegisterDependencies_Validation()
+  {
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddSpeckleSdk(new("Tests", "test"), "v3");
+    serviceCollection.AddDUI<DefaultThreadContext, DocumentModelStore>();
+    var serviceProvider = serviceCollection.BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true });
+    serviceProvider.Should().NotBeNull();
+  }
+
+  [Test]
+  public void RegisterDependencies_Scopes()
+  {
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddSpeckleSdk(new("Tests", "test"), "v3");
+    serviceCollection.AddDUI<DefaultThreadContext, DocumentModelStore>();
+    var serviceProvider = serviceCollection.BuildServiceProvider(
+      new ServiceProviderOptions { ValidateScopes = true, ValidateOnBuild = true }
+    );
+    serviceProvider.Should().NotBeNull();
+  }
+}
---
+++ Sdk/Speckle.Connectors.Common.Tests/ContainerRegistrationTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Speckle.Sdk;
+
+namespace Speckle.Connectors.Common.Tests;
+
+public class ServiceRegistrationTests
+{
+  [Test]
+  public void RegisterDependencies_Validation()
+  {
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddSpeckleSdk(new("Tests", "test"), "v3");
+    serviceCollection.AddConnectors();
+    var serviceProvider = serviceCollection.BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true });
+    serviceProvider.Should().NotBeNull();
+  }
+
+  [Test]
+  public void RegisterDependencies_Scopes()
+  {
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddSpeckleSdk(new("Tests", "test"), "v3");
+    serviceCollection.AddConnectors();
+    var serviceProvider = serviceCollection.BuildServiceProvider(
+      new ServiceProviderOptions { ValidateScopes = true, ValidateOnBuild = true }
+    );
+    serviceProvider.Should().NotBeNull();
+  }
+}
