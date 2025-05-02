eeb7ab681060052b8022ed5985b78054e01dcaae
Claire Kuang <kuang.claire@gmail.com>
2025-05-02 13:14:51 +0100

    feat(grasshopper): filter objects node now outputs list of removed objects (#805)
    * allows for creation of multiple property groups
    
    * adds second output to filter objects component
    
    * removes load on open option
    
    * add path to create speckle object output
    
    * Update CreateSpeckleObject.cs
    
    * Update FilterSpeckleObjects.cs

--- Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Components/Objects/CreateSpeckleObject.cs
+++ Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Components/Objects/CreateSpeckleObject.cs
@@ -56,6 +56,16 @@ public class CreateSpeckleObject : GH_Component
       GH_ParamAccess.item
     );
     Params.Input[5].Optional = true;
+
+    /* POC: disable for now as we are doing anything with this
+    pManager.AddTextParameter(
+      "Path",
+      "p",
+      "The Collection Path of the Speckle Object. Should be delimited with `:`",
+      GH_ParamAccess.item
+    );
+    Params.Input[6].Optional = true;
+    */
   }
 
   protected override void RegisterOutputParams(GH_OutputParamManager pManager)
@@ -83,6 +93,13 @@ public class CreateSpeckleObject : GH_Component
       "The material of the Speckle Object.",
       GH_ParamAccess.item
     );
+
+    pManager.AddTextParameter(
+      "Path",
+      "p",
+      $"The Collection Path of the Speckle Object, delimited with `{Constants.LAYER_PATH_DELIMITER}`",
+      GH_ParamAccess.item
+    );
   }
 
   protected override void SolveInstance(IGH_DataAccess da)
@@ -104,6 +121,9 @@ public class CreateSpeckleObject : GH_Component
 
     IGH_Goo? inputMaterial = null;
     da.GetData(5, ref inputMaterial);
+
+    //string? inputPath = null;
+    //da.GetData(6, ref inputPath);
 
     // keep track of mutation
     // poc: we should not mark mutations on color or material, as this shouldn't affect the appId of the object, and will allow original display values to stay intact on send.
@@ -189,6 +209,12 @@ public class CreateSpeckleObject : GH_Component
       ? Guid.NewGuid().ToString()
       : result.Value.ApplicationId ?? Guid.NewGuid().ToString();
 
+    // get the path
+    string path =
+      result.Value.Path.Count > 1
+        ? string.Join(Constants.LAYER_PATH_DELIMITER, result.Value.Path)
+        : result.Value.Path.FirstOrDefault();
+
     // set all the data
     da.SetData(0, result.Value);
     da.SetData(1, result.Value.GeometryBase);
@@ -196,5 +222,6 @@ public class CreateSpeckleObject : GH_Component
     da.SetData(3, result.Value.Properties);
     da.SetData(4, result.Value.Color);
     da.SetData(5, result.Value.Material);
+    da.SetData(6, path);
   }
 }
