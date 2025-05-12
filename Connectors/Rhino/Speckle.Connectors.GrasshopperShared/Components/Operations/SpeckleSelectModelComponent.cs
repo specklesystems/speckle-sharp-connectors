8bc8dc7b8642dc2081fd95411865aee7d9739c02
oguzhankoral <oguzhankoral@gmail.com>
2025-05-13 00:06:21 +0300

    handle all errors at the top level for wizard component

--- Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Components/Operations/SpeckleSelectModelComponent.cs
+++ Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Components/Operations/SpeckleSelectModelComponent.cs
@@ -78,194 +78,208 @@ public class SpeckleSelectModelComponent : GH_Component
   protected override void SolveInstance(IGH_DataAccess da)
 #pragma warning restore CA1502
   {
-    // Deal with inputs
-    string? urlInput = null;
-
-    // OPTION 1: Component has input wire connected
-    if (da.GetData(0, ref urlInput))
+    try
     {
-      UrlInput = urlInput;
-      //Lock button interactions before anything else, to ensure any input (even invalid ones) lock the state.
-      SpeckleOperationWizard.SetComponentButtonsState(false);
+      // Deal with inputs
+      string? urlInput = null;
 
-      if (urlInput == null || string.IsNullOrEmpty(urlInput))
+      // SCENARIO 1: Component has input wire connected
+      if (da.GetData(0, ref urlInput))
       {
-        AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Input url was empty or null");
-        return;
-      }
+        UrlInput = urlInput;
+        //Lock button interactions before anything else, to ensure any input (even invalid ones) lock the state.
+        SpeckleOperationWizard.SetComponentButtonsState(false);
 
-      try
-      {
-        // NOTE: once we split the logic in Sender and Receiver components, we need to set flag correctly
-        var (resource, hasPermission) = SpeckleOperationWizard.SolveInstanceWithUrlInput(urlInput, true);
-        if (!hasPermission)
+        if (urlInput == null || string.IsNullOrEmpty(urlInput))
         {
-          AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "You do not have enough permission for this project.");
+          AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Input url was empty or null");
+          return;
         }
+
+        try
+        {
+          // NOTE: once we split the logic in Sender and Receiver components, we need to set flag correctly
+          var (resource, hasPermission) = SpeckleOperationWizard.SolveInstanceWithUrlInput(urlInput, true);
+          if (!hasPermission)
+          {
+            AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "You do not have enough permission for this project.");
+          }
+          _storedUserId = SpeckleOperationWizard.SelectedAccount?.id;
+          _storedServer = resource.Server;
+          da.SetData(0, resource);
+        }
+        catch (SpeckleException e)
+        {
+          AddRuntimeMessage(GH_RuntimeMessageLevel.Error, e.Message);
+        }
+        return; // Fast exit!
+      }
+
+      // SCENARIO 2: Component is running with no wires connected to input.
+
+      // Unlock button interactions when no input data is provided (no wires connected)
+      SpeckleOperationWizard.SetComponentButtonsState(true);
+
+      // When user unplugs the URL input, we need to reset all first
+      if (UrlInput != null)
+      {
+        UrlInput = null;
+        SpeckleOperationWizard.ResetHandlers();
         _storedUserId = SpeckleOperationWizard.SelectedAccount?.id;
-        _storedServer = resource.Server;
-        da.SetData(0, resource);
-      }
-      catch (SpeckleException e)
-      {
-        AddRuntimeMessage(GH_RuntimeMessageLevel.Error, e.Message);
-      }
-      return; // Fast exit!
-    }
-
-    // OPTION 2: Component is running with no wires connected to input.
-
-    // Unlock button interactions when no input data is provided (no wires connected)
-    SpeckleOperationWizard.SetComponentButtonsState(true);
-
-    // When user unplugs the URL input, we need to reset all first
-    if (UrlInput != null)
-    {
-      UrlInput = null;
-      SpeckleOperationWizard.ResetHandlers();
-      _storedUserId = SpeckleOperationWizard.SelectedAccount?.id;
-    }
-
-    if (_justPastedIn && _storedUserId != null && !string.IsNullOrEmpty(_storedUserId))
-    {
-      try
-      {
-        SpeckleOperationWizard.SetAccountFromId(_storedUserId);
-      }
-      catch (SpeckleAccountManagerException e)
-      {
-        // Swallow and move onto checking server.
-        Console.WriteLine(e);
       }
 
-      if (_storedServer != null && SpeckleOperationWizard.SelectedAccount == null)
+      if (_justPastedIn && _storedUserId != null && !string.IsNullOrEmpty(_storedUserId))
       {
-        SpeckleOperationWizard.SetAccountFromIdAndUrl(_storedUserId, _storedServer);
+        try
+        {
+          SpeckleOperationWizard.SetAccountFromId(_storedUserId);
+        }
+        catch (SpeckleAccountManagerException e)
+        {
+          // Swallow and move onto checking server.
+          Console.WriteLine(e);
+        }
+
+        if (_storedServer != null && SpeckleOperationWizard.SelectedAccount == null)
+        {
+          SpeckleOperationWizard.SetAccountFromIdAndUrl(_storedUserId, _storedServer);
+        }
       }
-    }
 
-    // Validate backing data
-    if (SpeckleOperationWizard.SelectedAccount == null)
-    {
-      AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Please select an account in the right click menu");
-      WorkspaceContextMenuButton.Enabled = false;
-      ProjectContextMenuButton.Enabled = false;
-      ModelContextMenuButton.Enabled = false;
-      VersionContextMenuButton.Enabled = false;
-      return;
-    }
-
-    // 1- Workspaces
-
-    if (_justPastedIn && !string.IsNullOrEmpty(_storedWorkspaceId))
-    {
-      SpeckleOperationWizard.SetWorkspaceFromSavedIdSync(_storedWorkspaceId!);
-    }
-
-    // NOTE FOR LATER: Need to be handled in SDK... and will come later by Jeddward Morgan...
-    if (SpeckleOperationWizard.WorkspaceMenuHandler.Workspaces == null)
-    {
-      var workspaces = SpeckleOperationWizard.FetchWorkspacesSync("");
-      if (workspaces.items.Count == 0)
+      // Validate backing data
+      if (SpeckleOperationWizard.SelectedAccount == null)
       {
-        // Create a workspace flow
-        SpeckleOperationWizard.CreateNewWorkspaceUIState();
+        AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Please select an account in the right click menu");
+        WorkspaceContextMenuButton.Enabled = false;
+        ProjectContextMenuButton.Enabled = false;
+        ModelContextMenuButton.Enabled = false;
+        VersionContextMenuButton.Enabled = false;
         return;
       }
-    }
 
-    // Unfortunately need to handle personal projects as workspace item for a while
-    if (SpeckleOperationWizard.WorkspaceMenuHandler.IsPersonalProjects)
-    {
-      _storedWorkspaceId = null;
-    }
-    else
-    {
-      SpeckleOperationWizard.SetDefaultWorkspaceSync();
+      // 1- Workspaces
 
-      if (SpeckleOperationWizard.SelectedWorkspace != null)
+      if (_justPastedIn && !string.IsNullOrEmpty(_storedWorkspaceId))
       {
-        _storedWorkspaceId = SpeckleOperationWizard.SelectedWorkspace.id;
+        SpeckleOperationWizard.SetWorkspaceFromSavedIdSync(_storedWorkspaceId!);
+      }
+
+      // NOTE FOR LATER: Need to be handled in SDK... and will come later by Jeddward Morgan...
+      if (SpeckleOperationWizard.WorkspaceMenuHandler.Workspaces == null)
+      {
+        var workspaces = SpeckleOperationWizard.FetchWorkspacesSync("");
+        if (workspaces.items.Count == 0)
+        {
+          // Create a workspace flow
+          SpeckleOperationWizard.CreateNewWorkspaceUIState();
+          return;
+        }
+      }
+
+      // Unfortunately need to handle personal projects as workspace item for a while
+      if (SpeckleOperationWizard.WorkspaceMenuHandler.IsPersonalProjects)
+      {
+        _storedWorkspaceId = null;
       }
       else
       {
+        SpeckleOperationWizard.SetDefaultWorkspaceSync();
+
+        if (SpeckleOperationWizard.SelectedWorkspace != null)
+        {
+          _storedWorkspaceId = SpeckleOperationWizard.SelectedWorkspace.id;
+        }
+        else
+        {
+          return;
+        }
+      }
+
+      // 2- Projects
+      ProjectContextMenuButton.Enabled = true;
+
+      // Get projects for menu
+      SpeckleOperationWizard.FetchProjectsSync("");
+
+      // Stored project id scenario
+      if (_justPastedIn && !string.IsNullOrEmpty(_storedProjectId))
+      {
+        SpeckleOperationWizard.SetProjectFromSavedIdSync(_storedProjectId!);
+      }
+
+      // No selected project scenario
+      if (SpeckleOperationWizard.SelectedProject == null)
+      {
+        ModelContextMenuButton.Enabled = false;
+        VersionContextMenuButton.Enabled = false;
         return;
       }
-    }
 
-    // 2- Projects
-    ProjectContextMenuButton.Enabled = true;
+      // 3- Models
 
-    // Get projects for menu
-    SpeckleOperationWizard.FetchProjectsSync("");
+      ModelContextMenuButton.Enabled = true;
 
-    // Stored project id scenario
-    if (_justPastedIn && !string.IsNullOrEmpty(_storedProjectId))
-    {
-      SpeckleOperationWizard.SetProjectFromSavedIdSync(_storedProjectId!);
-    }
+      // Get models for menu
+      SpeckleOperationWizard.FetchModelsSync("");
 
-    // No selected project scenario
-    if (SpeckleOperationWizard.SelectedProject == null)
-    {
-      ModelContextMenuButton.Enabled = false;
-      VersionContextMenuButton.Enabled = false;
-      return;
-    }
+      if (_justPastedIn && !string.IsNullOrEmpty(_storedModelId))
+      {
+        SpeckleOperationWizard.SetModelFromSavedIdSync(_storedModelId!);
+      }
 
-    // 3- Models
+      // No selected model scenario
+      if (SpeckleOperationWizard.SelectedModel == null)
+      {
+        VersionContextMenuButton.Enabled = false;
+        return;
+      }
 
-    ModelContextMenuButton.Enabled = true;
+      // 4- Models
 
-    // Get models for menu
-    SpeckleOperationWizard.FetchModelsSync("");
+      VersionContextMenuButton.Enabled = true;
+      SpeckleOperationWizard.FetchVersionsSync(10);
 
-    if (_justPastedIn && !string.IsNullOrEmpty(_storedModelId))
-    {
-      SpeckleOperationWizard.SetModelFromSavedIdSync(_storedModelId!);
-    }
+      if (_justPastedIn && !string.IsNullOrEmpty(_storedVersionId))
+      {
+        SpeckleOperationWizard.SetVersionFromSavedIdSync(_storedVersionId!);
+      }
 
-    // No selected model scenario
-    if (SpeckleOperationWizard.SelectedModel == null)
-    {
-      VersionContextMenuButton.Enabled = false;
-      return;
-    }
+      if (SpeckleOperationWizard.SelectedVersion == null)
+      {
+        // If no version selected, output `latest` resource
+        da.SetData(
+          0,
+          new SpeckleUrlLatestModelVersionResource(
+            SpeckleOperationWizard.SelectedAccount.serverInfo.url,
+            SpeckleOperationWizard.SelectedProject.id,
+            SpeckleOperationWizard.SelectedModel.id
+          )
+        );
+        return;
+      }
 
-    // 4- Models
-
-    VersionContextMenuButton.Enabled = true;
-    SpeckleOperationWizard.FetchVersionsSync(10);
-
-    if (_justPastedIn && !string.IsNullOrEmpty(_storedVersionId))
-    {
-      SpeckleOperationWizard.SetVersionFromSavedIdSync(_storedVersionId!);
-    }
-
-    if (SpeckleOperationWizard.SelectedVersion == null)
-    {
-      // If no version selected, output `latest` resource
+      // If all data points are selected, output specific version.
       da.SetData(
         0,
-        new SpeckleUrlLatestModelVersionResource(
+        new SpeckleUrlModelVersionResource(
           SpeckleOperationWizard.SelectedAccount.serverInfo.url,
           SpeckleOperationWizard.SelectedProject.id,
-          SpeckleOperationWizard.SelectedModel.id
+          SpeckleOperationWizard.SelectedModel.id,
+          SpeckleOperationWizard.SelectedVersion.id
         )
       );
-      return;
     }
-
-    // If all data points are selected, output specific version.
-    da.SetData(
-      0,
-      new SpeckleUrlModelVersionResource(
-        SpeckleOperationWizard.SelectedAccount.serverInfo.url,
-        SpeckleOperationWizard.SelectedProject.id,
-        SpeckleOperationWizard.SelectedModel.id,
-        SpeckleOperationWizard.SelectedVersion.id
-      )
-    );
+    catch (AggregateException e) when (!e.IsFatal())
+    {
+      AddRuntimeMessage(
+        GH_RuntimeMessageLevel.Error,
+        string.Join("\n", e.InnerExceptions.Select(innerE => innerE.Message))
+      );
+    }
+    catch (Exception e) when (!e.IsFatal())
+    {
+      AddRuntimeMessage(GH_RuntimeMessageLevel.Error, e.Message);
+    }
   }
 
   protected override void AfterSolveInstance()
