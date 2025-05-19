20440f8a164352531f24ad3f9c3205496482755c
KatKatKateryna <kateryna@speckle.systems>
2025-05-19 18:52:12 +0100

    return multiple line segments without constructing a polyline

--- Converters/Autocad/Speckle.Converters.AutocadShared/ToSpeckle/Geometry/HatchToSpeckleConverter.cs
+++ Converters/Autocad/Speckle.Converters.AutocadShared/ToSpeckle/Geometry/HatchToSpeckleConverter.cs
@@ -17,19 +17,26 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
 
   public Base Convert(object target) => Convert((ADB.Hatch)target);
 
+  /// <summary>
+  /// Converting AutoCAD Hatch to Speckle Region. This method first converts Hatch to AutoCAD Region, and then uses RegionToSpeckle converter.
+  /// This is done because AutoCAD Region class allows to directly extract Brep representation (and convert it to display Mesh),
+  /// get distinct Outer and Inner loops, and filter out Hatches that consist of multiple disconnected parts (which our Region class is not designed for).
+  /// </summary>
+  /// <param name="target">AutoCAD Hatch object</param>
+  /// <returns>Speckle Region with property 'hasHatchPattern' as 'true'</returns>
   public SOG.Region Convert(ADB.Hatch target)
   {
     ADB.Region? regionToConvert = null;
 
     for (int i = 0; i < target.NumberOfLoops; i++)
     {
-      // Create 3d polyline from the HatchLoop
+      // Convert HatchLoop into DBObjectCollection for the subsequent construction of the Region (.CreateFromCurves())
       ADB.HatchLoop loop = target.GetLoopAt(i);
-      ADB.Curve polyline = PolylineFromLoop(loop);
+      List<ADB.Curve> polyline = ConvertHatchLoopToCurveEntityList(loop);
       ADB.DBObjectCollection objCollection = new();
-      objCollection.Add(polyline);
+      polyline.ForEach(x => objCollection.Add(x));
 
-      // Convert polyline into an individual Region
+      // Convert a loop (represented by DBObjectCollection) into an individual Region
       using (ADB.DBObjectCollection regionCollection = ADB.Region.CreateFromCurves(objCollection))
       {
         if (regionCollection.Count != 1)
@@ -76,123 +83,125 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
     return convertedRegion;
   }
 
-  private ADB.Curve PolylineFromLoop(ADB.HatchLoop loop)
+  /// <summary>
+  /// Converting AutoCAD HatchLoop into a list of AutoCAD Curves, so they can be used to construct an AutoCAD Region.
+  /// </summary>
+  private List<ADB.Curve> ConvertHatchLoopToCurveEntityList(ADB.HatchLoop loop)
   {
-    // collect vertices and construct a polyline simultaneously
-    ADB.Polyline polyline = new() { Closed = true };
-    // keep track of the vertices added to 'polyline', because to add new points we need to insert them via specific index (in method TryAddPointToPolyline)
-    int count = 0;
+    List<ADB.Curve> curveList = new();
 
     // disposable object, wrapping into "using"
-    using (AG.Point3dCollection vertices = new())
+    using (AG.Point3dCollection pts = new())
     {
       if (loop.IsPolyline)
       {
-        foreach (ADB.BulgeVertex bVertex in loop.Polyline)
+        // loop.Polyline doesn't actually return a Polyline, but a 'BulgeVertexCollection'. We need to construct a Polyline from it
+        ADB.Polyline polyline = new() { Closed = true };
+
+        // keep track of the number of vertices added to 'polyline', because to add new points we need to insert them via specific index (in method .AddVertexAt())
+        for (int i = 0; i < loop.Polyline.Count; i++)
         {
-          count = TryAddPointToPolyline(vertices, polyline, bVertex.Vertex, bVertex.Bulge, count);
+          ADB.BulgeVertex bVertex = loop.Polyline[i];
+          AG.Point3d point3d = new(bVertex.Vertex.X, bVertex.Vertex.Y, 0);
+
+          // add point only if it doesn't overlap with the first one ('closed' property is already set). Otherwise, polyline will be invalid.
+          if (pts.Count == 0 || pts[0].DistanceTo(point3d) > 0.001)
+          {
+            pts.Add(new AG.Point3d(bVertex.Vertex.X, bVertex.Vertex.Y, 0));
+            polyline.AddVertexAt(i, bVertex.Vertex, bVertex.Bulge, 0, 0);
+          }
         }
 
         // if only 2 points, that's a circle
-        if (vertices.Count == 2)
+        if (loop.Polyline.Count == 2)
         {
-          AG.Point3d centerPt =
-            new(
-              vertices[0].X + (vertices[1].X - vertices[0].X) / 2,
-              vertices[0].Y + (vertices[1].Y - vertices[0].Y) / 2,
-              0
-            );
-          return new ADB.Circle(centerPt, AG.Vector3d.ZAxis, vertices[0].DistanceTo(vertices[1]) / 2);
+          AG.Point3d centerPt = new(pts[0].X + (pts[1].X - pts[0].X) / 2, pts[0].Y + (pts[1].Y - pts[0].Y) / 2, 0);
+          curveList.Add(new ADB.Circle(centerPt, AG.Vector3d.ZAxis, pts[0].DistanceTo(pts[1]) / 2));
+          return curveList;
         }
-        return polyline;
-      }
 
-      // if .Polyline is null, read from .Curves
-      if (loop.Curves.Count == 0)
-      {
-        throw new ConversionException($"Hatch loop doesn't contain any segments.");
+        curveList.Add(polyline);
+        return curveList;
       }
+    }
 
-      if (loop.Curves.Count > 1) // handle the multi-segment case only with Line segments (not able to produce a Hatch Loop with multiple segments of other types)
+    // if .Polyline is null, read from .Curves
+    if (loop.Curves.Count == 0)
+    {
+      throw new ConversionException($"Hatch loop doesn't contain any segments.");
+    }
+
+    if (loop.Curves.Count > 1) // handle the multi-segment case only with Line segments (not able to produce a Hatch Loop with multiple segments of other types)
+    {
+      foreach (AG.Curve2d curve2d in loop.Curves)
       {
-        foreach (var lineSegment in loop.Curves)
+        if (curve2d is not AG.LineSegment2d l)
         {
-          // for each segment, skip the last point: it will be added as a start Point of the next segment. Otherwise, they will overlap and make the curve invalid
-          count = lineSegment is AG.LineSegment2d line
-            ? TryAddPointToPolyline(vertices, polyline, line.StartPoint, 0, count)
-            : throw new ConversionException($"Hatch segments of type {lineSegment.GetType()} are not supported");
+          throw new ConversionException($"Hatch segments of type {curve2d.GetType()} are not supported");
         }
-        return polyline;
+
+        ADB.Line line =
+          new(new AG.Point3d(l.StartPoint.X, l.StartPoint.Y, 0), new AG.Point3d(l.EndPoint.X, l.EndPoint.Y, 0));
+        curveList.Add(line);
       }
+      return curveList;
+    }
 
-      var segment = loop.Curves[0]; // if .Curve has only 1 segments, it can be a closed Circle, Ellipse or Nurb
-      switch (segment)
-      {
-        case AG.CircularArc2d arc:
-          if (Math.Abs(arc.EndAngle - arc.StartAngle) - 2 * Math.PI > 0.0001) // check if it's not a circle
-          {
-            throw new ConversionException($"Multiple hatch segments of type {segment.GetType()} are not supported");
-          }
-          return new ADB.Circle(new(arc.Center.X, arc.Center.Y, 0), AG.Vector3d.ZAxis, arc.Radius);
+    var segment = loop.Curves[0]; // if .Curve has only 1 segments, it can be a closed Circle, Ellipse or Nurb
+    switch (segment)
+    {
+      case AG.CircularArc2d arc:
+        if (Math.Abs(arc.EndAngle - arc.StartAngle) - 2 * Math.PI > 0.0001) // check if it's not a circle
+        {
+          throw new ConversionException($"Multiple hatch segments of type {segment.GetType()} are not supported");
+        }
 
-        case AG.EllipticalArc2d ellipse:
-          if (Math.Abs(ellipse.EndAngle - ellipse.StartAngle) - 2 * Math.PI > 0.0001) // check if it's not an ellipse
-          {
-            throw new ConversionException($"Multiple hatch segments of type {segment.GetType()} are not supported");
-          }
-          return new ADB.Ellipse(
+        curveList.Add(new ADB.Circle(new(arc.Center.X, arc.Center.Y, 0), AG.Vector3d.ZAxis, arc.Radius));
+        return curveList;
+
+      case AG.EllipticalArc2d ellipse:
+        if (Math.Abs(ellipse.EndAngle - ellipse.StartAngle) - 2 * Math.PI > 0.0001) // check if it's not an ellipse
+        {
+          throw new ConversionException($"Multiple hatch segments of type {segment.GetType()} are not supported");
+        }
+        curveList.Add(
+          new ADB.Ellipse(
             new(ellipse.Center.X, ellipse.Center.Y, 0),
             AG.Vector3d.ZAxis,
             new AG.Vector3d(ellipse.MajorAxis.X, ellipse.MajorAxis.Y, 0),
             ellipse.MinorRadius / ellipse.MajorRadius,
             ellipse.StartAngle,
             ellipse.EndAngle
-          );
+          )
+        );
+        return curveList;
 
-        case AG.NurbCurve2d nurb:
-
+      case AG.NurbCurve2d nurb:
+        using (AG.Point3dCollection ptsNurb = new())
+        {
           AG.DoubleCollection knotsCollection = new();
-          AG.Point3dCollection pts = new();
 
           nurb.Knots.Cast<double>().ToList().ForEach(x => knotsCollection.Add(x));
           nurb.DefinitionData.ControlPoints.Cast<AG.Point2d>()
             .ToList()
-            .ForEach(x => pts.Add(new AG.Point3d(x.X, x.Y, 0.0)));
-
-          return new ADB.Spline(
-            nurb.Degree,
-            nurb.IsRational,
-            nurb.IsClosed(),
-            nurb.IsPeriodic(out _),
-            pts,
-            knotsCollection,
-            nurb.DefinitionData.Weights,
-            0,
-            0
+            .ForEach(x => ptsNurb.Add(new AG.Point3d(x.X, x.Y, 0.0)));
+          curveList.Add(
+            new ADB.Spline(
+              nurb.Degree,
+              nurb.IsRational,
+              nurb.IsClosed(),
+              nurb.IsPeriodic(out _),
+              ptsNurb,
+              knotsCollection,
+              nurb.DefinitionData.Weights,
+              0,
+              0
+            )
           );
-        default:
-          throw new ConversionException($"Multiple hatch segments of type {segment.GetType()} are not supported");
-      }
+          return curveList;
+        }
+      default:
+        throw new ConversionException($"Multiple hatch segments of type {segment.GetType()} are not supported");
     }
-  }
-
-  private int TryAddPointToPolyline(
-    AG.Point3dCollection vertices,
-    ADB.Polyline polyline,
-    AG.Point2d pointToAdd,
-    double bulge,
-    int count
-  )
-  {
-    AG.Point3d point3d = new(pointToAdd.X, pointToAdd.Y, 0);
-    // add point only if it doesn't overlap with the first one
-    if (vertices.Count == 0 || vertices[0].DistanceTo(point3d) > 0.001)
-    {
-      vertices.Add(point3d);
-      polyline.AddVertexAt(count, pointToAdd, bulge, 0, 0);
-      return count + 1;
-    }
-
-    return count;
   }
 }
