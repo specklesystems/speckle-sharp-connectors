1a1c0f920f9ef7d348755ef018c8b82d23b1d40d
Adam Hathcock <adam@hathcock.uk>
2025-05-20 15:14:17 +0100

    New project to internalize Revit.Async

---
+++ Connectors/Revit/Speckle.Connectors.Revit.Common/RevitAsync.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.UI;
+
+namespace Speckle.Connectors.Revit.Common;
+
+public static class RevitAsync
+{
+  public static Task<TResult> RunAsync<TResult>(Func<TResult> function) => global::Revit.Async.RevitTask.RunAsync(function);
+
+  public static Task<TResult> RunAsync<TResult>(Func<Task<TResult>> function) => global::Revit.Async.RevitTask.RunAsync(function);
+
+  public static Task RunAsync(Action action) => global::Revit.Async.RevitTask.RunAsync(action);
+
+  public static Task RunAsync(Func<Task> handler) => global::Revit.Async.RevitTask.RunAsync(handler);
+
+  public static void Initialize(UIControlledApplication application) => global::Revit.Async.RevitTask.Initialize(application);
+
+  public static void Initialize(UIApplication application) => global::Revit.Async.RevitTask.Initialize(application);
+}
