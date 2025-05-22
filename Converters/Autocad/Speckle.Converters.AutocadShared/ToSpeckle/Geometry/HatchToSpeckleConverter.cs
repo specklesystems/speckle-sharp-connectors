81f2ed0a754b4b00c90fa59c65d82c3c3e6cda42
KatKatKateryna <89912278+KatKatKateryna@users.noreply.github.com>
2025-05-22 14:03:21 +0100

    Kateryna/cnx 1615 hatch loop conversion failed (#824)
    * handle hatch segments that are not a Polyline
    
    * send elliptical curves
    
    * support line segments
    
    * support sending nurb curves
    
    * send approximated Hatch with nurb2d segment
    
    * send nurb loops for hatches
    
    * optimize
    
    * comment
    
    * comments
    
    * cut down Nurbs converter
    
    * simplify hatch conversion
    
    * fix nurb3d
    
    * optimize
    
    * rearrange
    
    * fix ellipse bug on send
    
    * fix rhino curves on receive
    
    * fix single>multi conversion issue on receive
    
    * shorten nurbs converter on send
    
    * shorten statement
    
    * explain 'count' var
    
    * return multiple line segments without constructing a polyline
    
    * comment
    
    * refactor(autocad): refactors hatch and region conversion and fixes some bugs (#856)
    
    * refactors hatch to speckle converter, and also fixes elevation bug
    
    * refactors region converter
    
    * removes excessive nurb converter
    
    * Update Speckle.Converters.AutocadShared.projitems
    
    ---------
    
    Co-authored-by: Claire Kuang <kuang.claire@gmail.com>

--- Converters/Autocad/Speckle.Converters.AutocadShared/ToSpeckle/Geometry/HatchToSpeckleConverter.cs
+++ Converters/Autocad/Speckle.Converters.AutocadShared/ToSpeckle/Geometry/HatchToSpeckleConverter.cs
@@ -17,19 +17,25 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
 
   public Base Convert(object target) => Convert((ADB.Hatch)target);
 
+  /// <summary>
+  /// Converting AutoCAD Hatch to Speckle Region.
+  /// This method first converts Hatch to AutoCAD Region, and then uses RegionToSpeckle converter.
+  /// AutoCAD Region is a much simpler class than Hatch, and converting to region allows us to handle a bunch of unsupported conditions in Hatches.
+  /// </summary>
+  /// <param name="target">AutoCAD Hatch object</param>
+  /// <returns>Speckle Region with property 'hasHatchPattern' as 'true'</returns>
   public SOG.Region Convert(ADB.Hatch target)
   {
     ADB.Region? regionToConvert = null;
-
     for (int i = 0; i < target.NumberOfLoops; i++)
     {
-      // Create 3d polyline from the HatchLoop
+      // Convert HatchLoop into DBObjectCollection for the subsequent construction of the Region (.CreateFromCurves())
       ADB.HatchLoop loop = target.GetLoopAt(i);
-      ADB.Curve polyline = PolylineFromLoop(loop);
+      List<ADB.Curve> polyline = ConvertHatchLoopToCurveEntityList(loop);
       ADB.DBObjectCollection objCollection = new();
-      objCollection.Add(polyline);
+      polyline.ForEach(x => objCollection.Add(x));
 
-      // Convert polyline into an individual Region
+      // Convert a loop (represented by DBObjectCollection) into an individual Region
       using (ADB.DBObjectCollection regionCollection = ADB.Region.CreateFromCurves(objCollection))
       {
         if (regionCollection.Count != 1)
@@ -38,6 +44,7 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
             $"Hatch conversion failed {target}: unexpected number of regions generated from 1 hatch loop"
           );
         }
+
         ADB.Region loopRegion = (ADB.Region)regionCollection[0];
 
         // Assign first loop as the main Region, other Regions will be subtracted from it
@@ -51,6 +58,7 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
           {
             throw new ConversionException($"Hatch conversion failed: {target}");
           }
+
           // subtract region from Boundary region
           double areaBefore = regionToConvert.Area;
           regionToConvert.BooleanOperation(ADB.BooleanOperationType.BoolSubtract, loopRegion);
@@ -69,6 +77,10 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
       throw new ConversionException($"Hatch conversion failed: {target}");
     }
 
+    // move this region to the target elevation
+    // POC: I've tried passing this elevation to ConvertHatchLoopToCurveEntityList() for direct assignment when converting 2d to 3d points, but this results in non-planarity in splines for some reason.
+    regionToConvert.TransformBy(AG.Matrix3d.Displacement(new AG.Vector3d(0, 0, target.Elevation)));
+
     // convert and store Regions
     SOG.Region convertedRegion = _regionConverter.Convert(regionToConvert);
     convertedRegion.hasHatchPattern = true;
@@ -76,44 +88,105 @@ public class HatchToSpeckleConverter : IToSpeckleTopLevelConverter, ITypedConver
     return convertedRegion;
   }
 
-  private ADB.Curve PolylineFromLoop(ADB.HatchLoop loop)
+  /// <summary>
+  /// Converts Hatchloops to database-resident curve entities.
+  /// Curve entities are required by the Region create method.
+  /// </summary>
+  private List<ADB.Curve> ConvertHatchLoopToCurveEntityList(ADB.HatchLoop loop)
   {
+    List<ADB.Curve> curveList = new();
+
+    // 1 - handle the case of a polyline first
     if (loop.IsPolyline)
     {
-      // disposable object, wrapping into "using"
-      using (AG.Point3dCollection vertices = new())
+      // create a polyline from the loop.Polyline BulgeVertexCollection
+      ADB.Polyline polyline = new() { Closed = true };
+
+      for (int i = 0; i < loop.Polyline.Count; i++)
       {
-        // collect vertices and construct a polyline simultaneously, it will be clear what to use after iterating
-        ADB.Polyline polyline = new() { Closed = true };
+        var vertex = loop.Polyline[i];
 
-        int count = 0;
-        foreach (ADB.BulgeVertex bVertex in loop.Polyline)
+        // check if this is the last point, the closed property is already set and duplicated endpoints will result in an invalid polyline
+        if (i == loop.Polyline.Count - 1 && vertex.Vertex.GetDistanceTo(loop.Polyline[0].Vertex) < 0.001)
         {
-          // don't add the end point that's the same as the start point
-          AG.Point3d newPt = new(bVertex.Vertex.X, bVertex.Vertex.Y, 0);
-          if (count == 0 || vertices[0].DistanceTo(newPt) > 0.00001)
-          {
-            vertices.Add(newPt);
-            polyline.AddVertexAt(count, bVertex.Vertex, bVertex.Bulge, 0, 0);
-            count++;
-          }
+          continue;
         }
 
-        // if only 2 points, that's a circle
-        if (vertices.Count == 2)
-        {
-          AG.Point3d centerPt =
-            new(
-              vertices[0].X + (vertices[1].X - vertices[0].X) / 2,
-              vertices[0].Y + (vertices[1].Y - vertices[0].Y) / 2,
-              0
-            );
-          return new ADB.Circle(centerPt, AG.Vector3d.ZAxis, vertices[0].DistanceTo(vertices[1]) / 2);
-        }
-        return polyline;
+        polyline.AddVertexAt(i, vertex.Vertex, vertex.Bulge, 0, 0);
       }
+
+      curveList.Add(polyline);
+      return curveList;
     }
 
-    throw new ConversionException("Hatch loop conversion failed.");
+    // 2 - if the loop is not a polyline, handle the loop curves
+    // Notes: empirically, it seems that whenever the curve count is 1, it is a closed curve type like circle, ellipse, etc
+    // and when the curve count is > 1, they are line segments that will comprise of a closed area
+    // We'll process curves accordingly
+    if (loop.Curves.Count == 0)
+    {
+      throw new ConversionException($"Hatch loop doesn't contain any segments.");
+    }
+
+    foreach (AG.Curve2d curve in loop.Curves)
+    {
+      ADB.Curve? curveEntity = null;
+      switch (curve)
+      {
+        case AG.LineSegment2d l:
+          curveEntity = new ADB.Line(
+            new AG.Point3d(l.StartPoint.X, l.StartPoint.Y, 0),
+            new AG.Point3d(l.EndPoint.X, l.EndPoint.Y, 0)
+          );
+          break;
+
+        case AG.CircularArc2d c:
+          AG.Point3d cCenter = new(c.Center.X, c.Center.Y, 0);
+          curveEntity =
+            c.EndPoint == c.StartPoint
+              ? new ADB.Circle(cCenter, AG.Vector3d.ZAxis, c.Radius)
+              : new ADB.Arc(cCenter, c.Radius, c.StartAngle, c.EndAngle);
+          break;
+
+        case AG.EllipticalArc2d e:
+          curveEntity = new ADB.Ellipse(
+            new AG.Point3d(e.Center.X, e.Center.Y, 0),
+            AG.Vector3d.ZAxis,
+            new AG.Vector3d(e.MajorAxis.X, e.MajorAxis.Y, 0),
+            e.MinorRadius / e.MajorRadius,
+            e.StartAngle,
+            e.EndAngle
+          );
+          break;
+
+        case AG.NurbCurve2d n: // need to convert to spline, ew
+          AG.Point3dCollection controlPoints = new();
+          AG.DoubleCollection knots = new();
+          n.Knots.Cast<double>().ToList().ForEach(x => knots.Add(x));
+          n.DefinitionData.ControlPoints.Cast<AG.Point2d>()
+            .ToList()
+            .ForEach(x => controlPoints.Add(new AG.Point3d(x.X, x.Y, 0)));
+
+          curveEntity = new ADB.Spline(
+            n.Degree,
+            n.IsRational,
+            n.IsClosed(),
+            n.IsPeriodic(out _),
+            controlPoints,
+            knots,
+            n.DefinitionData.Weights,
+            0,
+            0
+          );
+          break;
+
+        default:
+          throw new ConversionException($"Segments of type {curve.GetType()} are not supported");
+      }
+
+      curveList.Add(curveEntity);
+    }
+
+    return curveList;
   }
 }
