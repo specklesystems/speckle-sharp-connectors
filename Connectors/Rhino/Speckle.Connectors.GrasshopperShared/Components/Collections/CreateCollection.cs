a2b671713322214f1966e00426e9f806e2e05b92
Björn <steinhagen.bjoern@gmail.com>
2025-05-29 20:27:02 +0200

    refactor: `CreateCollection`

--- Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Components/Collections/CreateCollection.cs
+++ Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Components/Collections/CreateCollection.cs
@@ -1,7 +1,7 @@
-using GH_IO.Serialization;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Speckle.Connectors.GrasshopperShared.Components.BaseComponents;
 using Speckle.Connectors.GrasshopperShared.HostApp;
-using Speckle.Connectors.GrasshopperShared.HostApp.Extras;
 using Speckle.Connectors.GrasshopperShared.Parameters;
 using Speckle.Connectors.GrasshopperShared.Properties;
 using Speckle.Sdk.Common;
@@ -9,27 +9,15 @@ using Speckle.Sdk.Models.Collections;
 
 namespace Speckle.Connectors.GrasshopperShared.Components.Collections;
 
+/// <summary>
+/// Simplified CreateCollection component using the base class pattern
+/// </summary>
 #pragma warning disable CA1711
-public class CreateCollection : GH_Component, IGH_VariableParameterComponent
+public class CreateCollection : VariableParameterComponentBase
 #pragma warning restore CA1711
 {
   public override Guid ComponentGuid => new("BDCE743E-7BDB-479B-AA81-19854AB5A254");
   protected override Bitmap Icon => Resources.speckle_collections_create;
-
-  private readonly DebounceDispatcher _debounceDispatcher = new();
-
-  private bool _alwaysInheritNames;
-
-  public bool AlwaysInheritNames
-  {
-    get => _alwaysInheritNames;
-    set
-    {
-      _alwaysInheritNames = value;
-
-      UpdateMessage();
-    }
-  }
 
   public CreateCollection()
     : base(
@@ -38,41 +26,22 @@ public class CreateCollection : GH_Component, IGH_VariableParameterComponent
       "Creates a new Collection",
       ComponentCategories.PRIMARY_RIBBON,
       ComponentCategories.COLLECTIONS
-    )
-  {
-    UpdateMessage();
-  }
-
-  private void UpdateMessage()
-  {
-    Message = AlwaysInheritNames ? "Inheriting names" : "";
-  }
+    ) { }
 
   protected override void RegisterInputParams(GH_InputParamManager pManager)
   {
-    var p = CreateParameter(GH_ParameterSide.Input, 0);
-    pManager.AddParameter(p);
+    var param = CreateParameter(GH_ParameterSide.Input, 0);
+    pManager.AddParameter(param);
   }
 
   protected override void RegisterOutputParams(GH_OutputParamManager pManager)
   {
-    pManager.AddGenericParameter("Collection", "C", "Created parent collection", GH_ParamAccess.tree);
+    pManager.AddGenericParameter("Collection", "C", "Created parent collection", GH_ParamAccess.item);
   }
 
   protected override void SolveInstance(IGH_DataAccess dataAccess)
   {
-    string rootName = "Unnamed";
-    Collection rootCollection = new();
-    SpeckleCollectionWrapper rootSpeckleCollectionWrapper =
-      new()
-      {
-        Base = rootCollection,
-        Name = rootName,
-        Path = new() { rootName },
-        Color = null,
-        Material = null,
-        ApplicationId = InstanceGuid.ToString()
-      };
+    var rootCollection = CreateRootCollection();
 
     foreach (var inputParam in Params.Input)
     {
@@ -82,196 +51,127 @@ public class CreateCollection : GH_Component, IGH_VariableParameterComponent
         continue;
       }
 
-      var inputCollections = data.OfType<SpeckleCollectionWrapperGoo>()
-        .Empty()
-        .Select(o => (SpeckleCollectionWrapperGoo)o.Duplicate())
-        .ToList();
-      var inputNonCollections = data.Where(t => t is not SpeckleCollectionWrapperGoo).Empty().ToList();
-
-      if (inputCollections.Count != 0 && inputNonCollections.Count != 0)
+      var childCollection = ProcessInputParameter(inputParam, data, rootCollection.Name);
+      if (childCollection != null)
       {
-        // error out! we want to disallow setting objects and collections in the same parent collection
-        AddRuntimeMessage(
-          GH_RuntimeMessageLevel.Error,
-          $"Parameter {inputParam.NickName} should not contain both objects and collections."
-        );
-        return;
+        rootCollection.Elements.Add(childCollection);
       }
-
-      List<string> childPath = new() { rootName };
-      childPath.Add(inputParam.NickName);
-      SpeckleCollectionWrapper childSpeckleCollectionWrapper =
-        new()
-        {
-          Base = new Collection(),
-          Name = inputParam.NickName,
-          Path = childPath,
-          Color = null,
-          Material = null,
-          Topology = GrasshopperHelpers.GetParamTopology(inputParam),
-          ApplicationId = inputParam.InstanceGuid.ToString(),
-        };
-
-      // handle collection inputs
-      // if on this port we're only receiving collections, we should become "pass-through" to not create
-      // needless nesting
-      if (inputCollections.Count == data.Count)
-      {
-        var nameTest = new HashSet<string>();
-        foreach (SpeckleCollectionWrapperGoo wrapperGoo in inputCollections)
-        {
-          // update the speckle collection path
-          wrapperGoo.Value.Path = childPath;
-
-          foreach (
-            string subCollectionName in wrapperGoo.Value.Elements.OfType<SpeckleCollectionWrapper>().Select(c => c.Name)
-          )
-          {
-            var hasNotSeenNameBefore = nameTest.Add(subCollectionName);
-            if (!hasNotSeenNameBefore)
-            {
-              AddRuntimeMessage(
-                GH_RuntimeMessageLevel.Error,
-                $"Duplicate collection name found: {subCollectionName} in input parameter {inputParam.NickName}. Please ensure collection names are unique per nesting level.\n See https://speckle.docs/grashopper/collections"
-              );
-              return;
-            }
-          }
-
-          childSpeckleCollectionWrapper.Elements.AddRange(wrapperGoo.Value.Elements);
-        }
-
-        rootSpeckleCollectionWrapper.Elements.Add(childSpeckleCollectionWrapper);
-        continue;
-      }
-
-      // handle object inputs
-      foreach (var obj in inputNonCollections)
-      {
-        SpeckleObjectWrapperGoo wrapperGoo = new();
-        if (wrapperGoo.CastFrom(obj))
-        {
-          wrapperGoo.Value.Path = childPath;
-          wrapperGoo.Value.Parent = childSpeckleCollectionWrapper;
-          childSpeckleCollectionWrapper.Elements.Add(wrapperGoo.Value);
-        }
-      }
-
-      rootSpeckleCollectionWrapper.Elements.Add(childSpeckleCollectionWrapper);
     }
 
-    dataAccess.SetData(0, new SpeckleCollectionWrapperGoo(rootSpeckleCollectionWrapper));
+    dataAccess.SetData(0, new SpeckleCollectionWrapperGoo(rootCollection));
   }
 
-  public bool CanInsertParameter(GH_ParameterSide side, int index) => side == GH_ParameterSide.Input;
-
-  public bool CanRemoveParameter(GH_ParameterSide side, int index) => side == GH_ParameterSide.Input;
-
-  public IGH_Param CreateParameter(GH_ParameterSide side, int index)
+  private SpeckleCollectionWrapper CreateRootCollection()
   {
-    var myParam = new SpeckleVariableParam
+    return new SpeckleCollectionWrapper
     {
-      Name = $"Sub-Collection {Params.Input.Count + 1}",
-      NickName = $"Sub-Collection {Params.Input.Count + 1}",
-      MutableNickName = true,
-      Optional = true,
-      Access = GH_ParamAccess.tree,
-      CanInheritNames = true,
-      AlwaysInheritNames = AlwaysInheritNames
-    };
-
-    return myParam;
-  }
-
-  public bool DestroyParameter(GH_ParameterSide side, int index) => side == GH_ParameterSide.Input;
-
-  public override void AddedToDocument(GH_Document document)
-  {
-    base.AddedToDocument(document);
-    Params.ParameterChanged += (_, args) =>
-    {
-      if (args.ParameterSide == GH_ParameterSide.Output)
-      {
-        return;
-      }
-      switch (args.OriginalArguments.Type)
-      {
-        case GH_ObjectEventType.NickName:
-          // This means the user is typing characters, debounce until it stops for 400ms before expiring the solution.
-          // Prevents UI from locking too soon while writing new names for inputs.
-          args.Parameter.Name = args.Parameter.NickName;
-          _debounceDispatcher.Debounce(500, _ => ExpireSolution(true));
-          break;
-        case GH_ObjectEventType.NickNameAccepted:
-          args.Parameter.Name = args.Parameter.NickName;
-          ExpireSolution(true);
-          break;
-        case GH_ObjectEventType.Sources:
-          // if this event is a source change, and param is the last input, then add a new param automatically
-          if (args.Parameter.SourceCount > 0 && args.ParameterIndex == Params.Input.Count - 1)
-          {
-            IGH_Param param = CreateParameter(GH_ParameterSide.Input, Params.Input.Count);
-            Params.RegisterInputParam(param);
-            Params.OnParametersChanged();
-          }
-          break;
-      }
+      Base = new Collection(),
+      Name = "Unnamed",
+      Path = new List<string> { "Unnamed" },
+      Color = null,
+      Material = null,
+      ApplicationId = InstanceGuid.ToString()
     };
   }
 
-  public void VariableParameterMaintenance()
+  private SpeckleCollectionWrapper? ProcessInputParameter(IGH_Param inputParam, List<IGH_Goo> data, string rootName)
   {
-    //todo
-  }
+    var collections = data.OfType<SpeckleCollectionWrapperGoo>().Empty().ToList();
+    var nonCollections = data.Where(t => t is not SpeckleCollectionWrapperGoo).Empty().ToList();
 
-  public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
-  {
-    base.AppendAdditionalMenuItems(menu);
-
-    Menu_AppendSeparator(menu);
-    ToolStripMenuItem alwaysInheritMenuItem = Menu_AppendItem(
-      menu,
-      "Always inherit names",
-      (_, _) =>
-      {
-        AlwaysInheritNames = !AlwaysInheritNames;
-        // update existing parameters - this will now trigger the property setter
-        foreach (var param in Params.Input.OfType<SpeckleVariableParam>())
-        {
-          param.AlwaysInheritNames = AlwaysInheritNames; // will trigger OnAlwaysInheritNamesChanged
-        }
-        OnDisplayExpired(true);
-      },
-      true,
-      AlwaysInheritNames
-    );
-    alwaysInheritMenuItem.ToolTipText =
-      "Toggle automatic name inheritance. If set, parameters will automatically inherit names from connected sources.";
-  }
-
-  public override bool Write(GH_IWriter writer) // NOTE: save state when closing and re-opening sessions
-  {
-    var result = base.Write(writer);
-    writer.SetBoolean("AlwaysInheritNames", AlwaysInheritNames);
-    return result;
-  }
-
-  public override bool Read(GH_IReader reader) // NOTE: save state when closing and re-opening sessions
-  {
-    var result = base.Read(reader);
-
-    bool alwaysInherit = false;
-    if (reader.TryGetBoolean("AlwaysInheritNames", ref alwaysInherit))
+    // Validate input - cannot mix collections and objects
+    if (collections.Count > 0 && nonCollections.Count > 0)
     {
-      AlwaysInheritNames = alwaysInherit;
-
-      // update existing parameters after reading
-      foreach (var param in Params.Input.OfType<SpeckleVariableParam>())
-      {
-        param.AlwaysInheritNames = AlwaysInheritNames;
-      }
-      UpdateMessage();
+      AddRuntimeMessage(
+        GH_RuntimeMessageLevel.Error,
+        $"Parameter {inputParam.NickName} cannot contain both objects and collections."
+      );
+      return null;
     }
-    return result;
+
+    var childPath = new List<string> { rootName, inputParam.NickName };
+    var childCollection = new SpeckleCollectionWrapper
+    {
+      Base = new Collection(),
+      Name = inputParam.NickName,
+      Path = childPath,
+      Color = null,
+      Material = null,
+      Topology = GrasshopperHelpers.GetParamTopology(inputParam),
+      ApplicationId = inputParam.InstanceGuid.ToString()
+    };
+
+    if (collections.Count > 0)
+    {
+      ProcessCollectionInputs(collections, childCollection, childPath);
+    }
+    else
+    {
+      ProcessObjectInputs(nonCollections, childCollection, childPath);
+    }
+
+    return childCollection;
+  }
+
+  private void ProcessCollectionInputs(
+    List<SpeckleCollectionWrapperGoo> collections,
+    SpeckleCollectionWrapper parentCollection,
+    List<string> childPath
+  )
+  {
+    var duplicateNames = new HashSet<string>();
+
+    foreach (var collectionGoo in collections.Select(c => (SpeckleCollectionWrapperGoo)c.Duplicate()))
+    {
+      collectionGoo.Value.Path = childPath;
+
+      // Check for duplicate names within this collection
+      foreach (
+        var subCollectionName in collectionGoo.Value.Elements.OfType<SpeckleCollectionWrapper>().Select(c => c.Name)
+      )
+      {
+        if (!duplicateNames.Add(subCollectionName))
+        {
+          AddRuntimeMessage(
+            GH_RuntimeMessageLevel.Error,
+            $"Duplicate collection name '{subCollectionName}' found. Collection names must be unique per level."
+          );
+          return;
+        }
+      }
+
+      parentCollection.Elements.AddRange(collectionGoo.Value.Elements);
+    }
+  }
+
+  private void ProcessObjectInputs(
+    List<IGH_Goo> objects,
+    SpeckleCollectionWrapper parentCollection,
+    List<string> childPath
+  )
+  {
+    foreach (var obj in objects)
+    {
+      var wrapperGoo = new SpeckleObjectWrapperGoo();
+      if (wrapperGoo.CastFrom(obj))
+      {
+        wrapperGoo.Value.Path = childPath;
+        wrapperGoo.Value.Parent = parentCollection;
+        parentCollection.Elements.Add(wrapperGoo.Value);
+      }
+    }
+  }
+
+  // IGH_VariableParameterComponent implementation
+  public override bool CanInsertParameter(GH_ParameterSide side, int index) => side == GH_ParameterSide.Input;
+
+  public override bool CanRemoveParameter(GH_ParameterSide side, int index) => side == GH_ParameterSide.Input;
+
+  public override bool DestroyParameter(GH_ParameterSide side, int index) => side == GH_ParameterSide.Input;
+
+  public override IGH_Param CreateParameter(GH_ParameterSide side, int index)
+  {
+    var param = CreateVariableParameter($"Sub-Collection {Params.Input.Count + 1}", GH_ParamAccess.tree);
+    return param;
   }
 }
