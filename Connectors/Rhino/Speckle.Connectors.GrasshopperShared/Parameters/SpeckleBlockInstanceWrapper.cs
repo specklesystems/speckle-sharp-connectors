942d27c1fc63e23e72f6a7daf84c72821ec8a6be
Björn <steinhagen.bjoern@gmail.com>
2025-06-02 14:04:40 +0200

    chore: adds `SpeckleBlockInstanceWrapperGoo`

--- Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Parameters/SpeckleBlockInstanceWrapper.cs
+++ Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Parameters/SpeckleBlockInstanceWrapper.cs
@@ -1,3 +1,6 @@
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
 using Speckle.Sdk.Models;
 
 namespace Speckle.Connectors.GrasshopperShared.Parameters;
@@ -8,4 +11,21 @@ namespace Speckle.Connectors.GrasshopperShared.Parameters;
 public class SpeckleBlockInstanceWrapper : SpeckleWrapper
 {
   public override required Base Base { get; set; }
+}
+
+public class SpeckleBlockInstanceWrapperGoo : GH_Goo<SpeckleBlockInstanceWrapper>, IGH_PreviewData, ISpeckleGoo
+{
+  public override IGH_Goo Duplicate() => throw new NotImplementedException();
+
+  public override string ToString() => $@"Speckle Block Instance Goo [{m_value.Base.speckle_type}]";
+
+  public override bool IsValid => true;
+  public override string TypeName => "Speckle block instance wrapper";
+  public override string TypeDescription => "A wrapper around speckle grasshopper block instances.";
+
+  public void DrawViewportWires(GH_PreviewWireArgs args) => throw new NotImplementedException();
+
+  public void DrawViewportMeshes(GH_PreviewMeshArgs args) => throw new NotImplementedException();
+
+  public BoundingBox ClippingBox { get; }
 }
