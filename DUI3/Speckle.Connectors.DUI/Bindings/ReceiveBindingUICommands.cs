4833fa017ba697d780673b311ffc08c0226e0816
Adam Hathcock <adam@hathcock.uk>
2025-06-04 16:44:18 +0100

    made ReceiveOperationManager with revit

--- DUI3/Speckle.Connectors.DUI/Bindings/ReceiveBindingUICommands.cs
+++ DUI3/Speckle.Connectors.DUI/Bindings/ReceiveBindingUICommands.cs
@@ -2,8 +2,19 @@ using Speckle.Connectors.Common.Conversion;
 using Speckle.Connectors.DUI.Bridge;
 
 namespace Speckle.Connectors.DUI.Bindings;
+public interface IReceiveBindingUICommands
+{
+  Task SetModelError(string modelCardId, Exception exception);
 
-public sealed class ReceiveBindingUICommands : BasicConnectorBindingCommands
+  Task SetModelReceiveResult(
+    string modelCardId,
+    IEnumerable<string> bakedObjectIds,
+    IEnumerable<ConversionResult> conversionResults
+  );
+
+  IBrowserBridge Bridge { get; }
+}
+public sealed class ReceiveBindingUICommands : BasicConnectorBindingCommands, IReceiveBindingUICommands
 {
   // POC: put here events once we needed for receive specific
   private const string SET_MODEL_RECEIVE_RESULT_UI_COMMAND_NAME = "setModelReceiveResult";
---
+++ DUI3/Speckle.Connectors.DUI/Bindings/ReceiveOperationManager.cs
@@ -0,0 +1,102 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Speckle.Connectors.Common.Builders;
+using Speckle.Connectors.Common.Cancellation;
+using Speckle.Connectors.Common.Operations;
+using Speckle.Connectors.DUI.Logging;
+using Speckle.Connectors.DUI.Models;
+using Speckle.Connectors.DUI.Models.Card;
+using Speckle.InterfaceGenerator;
+using Speckle.Sdk;
+
+namespace Speckle.Connectors.DUI.Bindings;
+
+public partial interface IReceiveOperationManager : IDisposable;
+
+[GenerateAutoInterface]
+public sealed class ReceiveOperationManager(
+  IServiceScope serviceScope,
+  ICancellationManager cancellationManager,
+  DocumentModelStore store,
+  ISpeckleApplication speckleApplication,
+  IOperationProgressManager operationProgressManager,
+  ILogger<ReceiveOperationManager> logger) : IReceiveOperationManager
+{
+  public async Task Process(
+    IReceiveBindingUICommands commands,
+    string modelCardId,
+    Action<IServiceProvider> initializeScope,
+    Func<Func<Task<HostObjectBuilderResult>>, Task<HostObjectBuilderResult?>>? processor
+  )
+  {
+    try
+    {
+      // Get receiver card
+      if (store.GetModelById(modelCardId) is not ReceiverModelCard modelCard)
+      {
+        // Handle as GLOBAL ERROR at BrowserBridge
+        throw new InvalidOperationException("No download model card was found.");
+      }
+      using var cancellationItem = cancellationManager.GetCancellationItem(modelCardId);
+
+      initializeScope(serviceScope.ServiceProvider);
+      var progress = operationProgressManager.CreateOperationProgressEventHandler(
+        commands.Bridge,
+        modelCardId,
+        cancellationItem.Token
+      );
+      var ro = serviceScope
+        .ServiceProvider.GetRequiredService<ReceiveOperation>();
+      HostObjectBuilderResult? conversionResults;
+      if (processor is null)
+      {
+        // Receive host objects
+        conversionResults = await
+          ro.Execute(
+            modelCard.GetReceiveInfo(speckleApplication.Slug),
+            progress,
+            cancellationItem.Token
+          );
+      }
+      else
+      {
+        conversionResults = await processor(() => ro.Execute(
+          modelCard.GetReceiveInfo(speckleApplication.Slug),
+          progress,
+          cancellationItem.Token
+        ));
+      }
+
+      if (conversionResults is null)
+      {
+        return;
+      }
+
+      modelCard.BakedObjectIds = conversionResults.BakedObjectIds.ToList();
+      await commands.SetModelReceiveResult(
+        modelCardId,
+        conversionResults.BakedObjectIds,
+        conversionResults.ConversionResults
+      );
+    }
+    catch (OperationCanceledException)
+    {
+      // SWALLOW -> UI handles it immediately, so we do not need to handle anything for now!
+      // Idea for later -> when cancel called, create promise from UI to solve it later with this catch block.
+      // So have 3 state on UI -> Cancellation clicked -> Cancelling -> Cancelled
+    }
+    catch (Exception ex) when (!ex.IsFatal()) // UX reasons - we will report operation exceptions as model card error. We may change this later when we have more exception documentation
+    {
+      logger.LogModelCardHandledError(ex);
+      await commands.SetModelError(modelCardId, ex);
+    }
+    finally
+    {
+      // otherwise the id of the operation persists on the cancellation manager and triggers 'Operations cancelled because of document swap!' message to UI.
+      cancellationManager.CancelOperation(modelCardId);
+    }
+  }
+
+  [AutoInterfaceIgnore]
+  public void Dispose() => serviceScope.Dispose();
+}
