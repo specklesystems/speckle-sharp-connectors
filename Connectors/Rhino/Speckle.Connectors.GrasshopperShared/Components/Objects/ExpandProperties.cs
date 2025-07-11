997760218572b514610311992c25a0cccf1b8ca4
Claire Kuang <kuang.claire@gmail.com>
2025-07-11 11:49:14 +0100

    adds expand properties node

---
+++ Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Components/Objects/ExpandProperties.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Runtime.InteropServices;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+using Speckle.Connectors.GrasshopperShared.Parameters;
+using Speckle.Connectors.GrasshopperShared.Properties;
+
+namespace Speckle.Connectors.GrasshopperShared.Components.Objects;
+
+[Guid("BD4E438F-70AF-4B9F-A786-B8C87CB1ACA2")]
+public class ExpandProperties : GH_Component, IGH_VariableParameterComponent
+{
+  public ExpandProperties()
+    : base(
+      "Expand Properties",
+      "eP",
+      "Expands Properties into its children",
+      ComponentCategories.PRIMARY_RIBBON,
+      ComponentCategories.OBJECTS
+    ) { }
+
+  public override Guid ComponentGuid => GetType().GUID;
+  protected override Bitmap Icon => Resources.speckle_collections_expand;
+  public override GH_Exposure Exposure => GH_Exposure.quarternary;
+
+  protected override void RegisterInputParams(GH_InputParamManager pManager)
+  {
+    pManager.AddParameter(
+      new SpecklePropertyGroupParam(GH_ParamAccess.item),
+      "Properties",
+      "C",
+      "The Properties you want to expand",
+      GH_ParamAccess.item
+    );
+  }
+
+  protected override void RegisterOutputParams(GH_OutputParamManager pManager) { }
+
+  protected override void SolveInstance(IGH_DataAccess da)
+  {
+    SpecklePropertyGroupGoo res = new();
+    if (!da.GetData(0, ref res))
+    {
+      return;
+    }
+
+    var outputParams = new List<OutputParamWrapper>();
+    foreach (var entry in res.Value)
+    {
+      var param = new Param_GenericObject()
+      {
+        Name = entry.Key,
+        NickName = entry.Key,
+        Access = GH_ParamAccess.item
+      };
+
+      outputParams.Add(new OutputParamWrapper(param, entry.Value));
+    }
+
+    if (da.Iteration == 0 && OutputMismatch(outputParams))
+    {
+      OnPingDocument()
+        .ScheduleSolution(
+          5,
+          _ =>
+          {
+            CreateOutputs(outputParams);
+          }
+        );
+    }
+    else
+    {
+      for (int i = 0; i < outputParams.Count; i++)
+      {
+        var outParam = Params.Output[i];
+        var outParamWrapper = outputParams[i];
+        switch (outParam.Access)
+        {
+          case GH_ParamAccess.item:
+            da.SetData(i, outParamWrapper.Values);
+            break;
+          case GH_ParamAccess.list:
+            da.SetDataList(i, outParamWrapper.Values as IList);
+            break;
+        }
+      }
+    }
+  }
+
+  private bool OutputMismatch(List<OutputParamWrapper> outputParams)
+  {
+    if (Params.Output.Count != outputParams.Count)
+    {
+      return true;
+    }
+
+    var count = 0;
+    foreach (var newParam in outputParams)
+    {
+      var oldParam = Params.Output[count];
+      if (
+        oldParam.NickName != newParam.Param.NickName
+        || oldParam.Name != newParam.Param.Name
+        || oldParam.Access != newParam.Param.Access
+      )
+      {
+        return true;
+      }
+      count++;
+    }
+
+    return false;
+  }
+
+  private void CreateOutputs(List<OutputParamWrapper> outputParams)
+  {
+    // TODO: better, nicer handling of creation/removal
+    while (Params.Output.Count > 0)
+    {
+      Params.UnregisterOutputParameter(Params.Output[^1]);
+    }
+
+    foreach (var newParam in outputParams)
+    {
+      var param = new Param_GenericObject
+      {
+        Name = newParam.Param.Name,
+        NickName = newParam.Param.NickName,
+        MutableNickName = false,
+        Access = newParam.Param.Access
+      };
+      Params.RegisterOutputParam(param);
+    }
+
+    Params.OnParametersChanged();
+    VariableParameterMaintenance();
+    ExpireSolution(false);
+  }
+
+  public void VariableParameterMaintenance() { }
+
+  public bool CanInsertParameter(GH_ParameterSide side, int index) => false;
+
+  public bool CanRemoveParameter(GH_ParameterSide side, int index) => false;
+
+  public IGH_Param CreateParameter(GH_ParameterSide side, int index)
+  {
+    var myParam = new Param_GenericObject
+    {
+      Name = GH_ComponentParamServer.InventUniqueNickname("ABCD", Params.Input),
+      MutableNickName = true,
+      Optional = true
+    };
+    myParam.NickName = myParam.Name;
+    return myParam;
+  }
+
+  public bool DestroyParameter(GH_ParameterSide side, int index) => side == GH_ParameterSide.Output;
+}
+
+public record OutputParamWrapper(Param_GenericObject Param, object Values);
