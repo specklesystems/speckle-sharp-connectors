c288ea00881ce45d923ae9e7d6e89c6f39c6d090
Adam Hathcock <adamhathcock@users.noreply.github.com>
2025-07-23 16:45:45 +0100

    Remove the trace around a single convert as it is spammy (#996)

--- Sdk/Speckle.Connectors.Common/Operations/ReceiveConversionHandler.cs
+++ Sdk/Speckle.Connectors.Common/Operations/ReceiveConversionHandler.cs
@@ -1,20 +1,17 @@
 using Speckle.InterfaceGenerator;
 using Speckle.Sdk;
 using Speckle.Sdk.Common.Exceptions;
-using Speckle.Sdk.Logging;
 
 namespace Speckle.Connectors.Common.Operations;
 
 [GenerateAutoInterface]
-public class ReceiveConversionHandler(ISdkActivityFactory activityFactory) : IReceiveConversionHandler
+public class ReceiveConversionHandler : IReceiveConversionHandler
 {
   public Exception? TryConvert(Action conversion)
   {
-    using var convertActivity = activityFactory.Start("Converting object");
     try
     {
       conversion();
-      convertActivity?.SetStatus(SdkActivityStatusCode.Ok);
       return null;
     }
     catch (ConversionException ce)
@@ -29,8 +26,6 @@ public class ReceiveConversionHandler(ISdkActivityFactory activityFactory) : IRe
     }
     catch (Exception ex) when (!ex.IsFatal())
     {
-      convertActivity?.SetStatus(SdkActivityStatusCode.Error);
-      convertActivity?.RecordException(ex);
       return ex;
     }
   }
