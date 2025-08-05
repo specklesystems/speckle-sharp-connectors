22b7f86d391f101ef6680c58cae9e40493082d96
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-08-05 22:27:49 +0100

    Add more logging

--- Importers/Rhino/Speckle.Importers.JobProcessor/Program.cs
+++ Importers/Rhino/Speckle.Importers.JobProcessor/Program.cs
@@ -21,35 +21,57 @@ public static class Program
   [STAThread]
   public static async Task Main()
   {
-    // Dapper doesn't understand how to handle JSON deserialization, so we need to tell it what types can be deserialzied
-    SqlMapper.AddTypeHandler(new JsonHandler<FileimportPayload>());
-
-    // DI setup
-    var serviceCollection = new ServiceCollection();
-    serviceCollection.AddJobProcessor();
-
-    serviceCollection.AddTransient<IJobHandler, RhinoJobHandler>();
-
-    var serviceProvider = serviceCollection.BuildServiceProvider();
-
-    var processor = serviceProvider.GetRequiredService<JobProcessorInstance>();
-    var logger = serviceProvider.GetRequiredService<ILogger<object>>();
-    using (new RhinoCore(["/netcore-8"], WindowStyle.NoWindow))
+    ILogger? logger = null;
+    try
     {
-      //What ever thread RhinoCore is created on it will grab as soon as it's available, and it will hog it forever.
-      //Right now, we're giving it the main STA thread (not 100% if it needs STA or if it could work on any thread)
-      await Task.Run(async () =>
+      // Dapper doesn't understand how to handle JSON deserialization, so we need to tell it what types can be deserialzied
+      SqlMapper.AddTypeHandler(new JsonHandler<FileimportPayload>());
+
+      // DI setup
+      var serviceCollection = new ServiceCollection();
+      serviceCollection.AddJobProcessor();
+
+      serviceCollection.AddTransient<IJobHandler, RhinoJobHandler>();
+
+      var serviceProvider = serviceCollection.BuildServiceProvider();
+
+      var processor = serviceProvider.GetRequiredService<JobProcessorInstance>();
+      logger = serviceProvider.GetRequiredService<ILogger<object>>();
+
+      TaskScheduler.UnobservedTaskException += (sender, args) =>
+        logger.LogCritical(args.Exception, "Unobserved Task Exception");
+
+      using (new RhinoCore(["/netcore-8"], WindowStyle.NoWindow))
       {
-        try
-        {
-          await processor.StartProcessing();
-        }
-        catch (Exception ex)
-        {
-          logger.LogCritical(ex, "Unhandled exception in Main");
-          throw;
-        }
-      }).ConfigureAwait(false);
+        //What ever thread RhinoCore is created on it will grab as soon as it's available, and it will hog it forever.
+        //Right now, we're giving it the main STA thread (not 100% if it needs STA or if it could work on any thread)
+        await Task.Run(async () =>
+          {
+            try
+            {
+              await processor.StartProcessing();
+            }
+            catch (Exception ex)
+            {
+              logger.LogCritical(ex, "Unhandled exception in Main");
+              throw;
+            }
+          })
+          .ConfigureAwait(false);
+      }
+    }
+    catch (Exception ex)
+    {
+      if (logger is not null)
+      {
+        logger.LogCritical(ex, "Unhandled exception reached entry point");
+      }
+      else
+      {
+        Console.WriteLine($"Unhandled exception reached entry point: {ex}");
+      }
+
+      throw;
     }
   }
 }
