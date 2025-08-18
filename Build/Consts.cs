2b8732bcace65505eb6363c7dc78efc3eeb06c2f
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-08-18 11:19:55 +0100

    remove consts

--- Build/Consts.cs
+++ Build/Consts.cs
@@ -13,7 +13,6 @@ public static class Consts
         new("Connectors/Rhino/Speckle.Connectors.Rhino8", "net48"),
       ]
     ),
-    new("rhino-importer", [new("Importers/Rhino/Speckle.Importers.JobProcessor", "net8.0-windows"),]),
     new(
       "revit",
       [
