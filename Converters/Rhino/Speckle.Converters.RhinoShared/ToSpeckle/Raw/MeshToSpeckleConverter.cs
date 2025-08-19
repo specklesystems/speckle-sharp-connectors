3e596cac2989f5962d8e970466343ab882cacf54
Claire Kuang <kuang.claire@gmail.com>
2025-08-19 10:00:10 +0100

    Update MeshToSpeckleConverter.cs

--- Converters/Rhino/Speckle.Converters.RhinoShared/ToSpeckle/Raw/MeshToSpeckleConverter.cs
+++ Converters/Rhino/Speckle.Converters.RhinoShared/ToSpeckle/Raw/MeshToSpeckleConverter.cs
@@ -1,7 +1,5 @@
 using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
-using Speckle.Converters.Rhino.Extensions;
-using Speckle.Converters.Rhino.ToSpeckle.Meshing;
 using Speckle.Sdk.Common.Exceptions;
 
 namespace Speckle.Converters.Rhino.ToSpeckle.Raw;
@@ -34,38 +32,35 @@ public class MeshToSpeckleConverter : ITypedConverter<RG.Mesh, SOG.Mesh>
       throw new ValidationException("Cannot convert a mesh with 0 vertices/faces");
     }
 
-    // Extracting Rhino Mesh and converting to Speckle with the most suitable settings (e.g. moving to origin first, if needed)
-    // This is needed because of Rhino using single precision numbers for Mesh vertices: https://wiki.mcneel.com/rhino/farfromorigin
-    RG.Mesh meshToConvert = target;
-    RG.Vector3d? vector = null;
-
-    // 1. If needed, move geometry to origin
-    if (_settingsStore.Current.ModelFarFromOrigin && target.IsFarFromOrigin(out RG.Vector3d vectorToGeometry))
-    {
-      meshToConvert = (RG.Mesh)target.Duplicate();
-      meshToConvert.Transform(RG.Transform.Translation(-vectorToGeometry));
-      vector = vectorToGeometry;
-    }
-    // 2. Convert extracted Mesh to Speckle. We don't move geometry back yet, because 'far from origin' geometry is causing Speckle conversion issues too
-    SOG.Mesh convertedMesh = ConvertMesh(meshToConvert);
-
-    // 3. Move Speckle geometry back from origin, if translation was applied
-    DisplayMeshExtractor.MoveSpeckleMeshes([convertedMesh], vector, _settingsStore.Current.SpeckleUnits);
+    SOG.Mesh convertedMesh = ConvertMesh(target);
 
     return convertedMesh;
   }
 
-  private SOG.Mesh ConvertMesh(RG.Mesh target)
+  // By default, Rhino meshes store vertices with single precision.
+  // For geometry very far away from the origin, this will result in jagged meshes: https://wiki.mcneel.com/rhino/farfromorigin
+  // Although the .ToPoint3dArray creates extra memory alloc, it's more important to prevent bad geometry for eg infrastructure models.
+  // An alternative would be to move the geometry back to the origin before conversion and move it back after (since transforms are double precision), but this creates complications for grasshopper since this would have to be evaluated per geometry and not once per model (therefore also expensive).
+  private double[] ConvertDoublePrecisionVertices(RG.Mesh target)
   {
     var vertexCoordinates = new double[target.Vertices.Count * 3];
+    target.Vertices.UseDoublePrecisionVertices = true;
+    RG.Point3d[] vertices = target.Vertices.ToPoint3dArray();
     var x = 0;
-    for (int i = 0; i < target.Vertices.Count; i++)
+    for (int i = 0; i < vertices.Length; i++)
     {
-      var v = target.Vertices[i];
+      var v = vertices[i];
       vertexCoordinates[x++] = v.X;
       vertexCoordinates[x++] = v.Y;
       vertexCoordinates[x++] = v.Z;
     }
+
+    return vertexCoordinates;
+  }
+
+  private SOG.Mesh ConvertMesh(RG.Mesh target)
+  {
+    var vertexCoordinates = ConvertDoublePrecisionVertices(target);
 
     List<int> faces = new();
 
@@ -81,7 +76,7 @@ public class MeshToSpeckleConverter : ITypedConverter<RG.Mesh, SOG.Mesh>
     }
 
     var colors = new int[target.VertexColors.Count];
-    x = 0;
+    int x = 0;
     foreach (var c in target.VertexColors)
     {
       colors[x++] = c.ToArgb();
