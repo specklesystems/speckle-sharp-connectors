cd6888868e999944f4423d47ecd50282bad47fe2
Björn <steinhagen.bjoern@gmail.com>
2025-08-21 10:10:07 +0200

    fix: flickering, dynamic outputs and docstrings

--- Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Components/Dev/DeconstructSpeckleParam.cs
+++ Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Components/Dev/DeconstructSpeckleParam.cs
@@ -16,9 +16,6 @@ namespace Speckle.Connectors.GrasshopperShared.Components.Dev;
 [Guid("C491D26C-84CB-4684-8BD2-AA78D0F2FE53")]
 public class DeconstructSpeckleParam : GH_Component, IGH_VariableParameterComponent
 {
-  // Store all unique field names discovered across all iterations
-  private readonly HashSet<string> _allDiscoveredFields = new();
-
   public DeconstructSpeckleParam()
     : base(
       "Deconstruct",
@@ -40,106 +37,152 @@ public class DeconstructSpeckleParam : GH_Component, IGH_VariableParameterCompon
 
   protected override void SolveInstance(IGH_DataAccess da)
   {
-    object data = new();
-    da.GetData(0, ref data);
+    // on first iteration, discover all fields from all objects to create stable output structure
+    if (da.Iteration == 0)
+    {
+      HashSet<string> allFields = DiscoverAllFieldsFromInput();
 
-    List<OutputParamWrapper>? outputParams = DeconstructObject(data);
+      if (allFields.Count > 0)
+      {
+        var requiredOutputs = CreateOutputParamsFromFieldNames(allFields);
+
+        if (OutputMismatch(requiredOutputs))
+        {
+          OnPingDocument()?.ScheduleSolution(5, _ => CreateOutputs(requiredOutputs));
+          return;
+        }
+      }
+    }
+
+    // process current object normally
+    object data = new();
+    if (!da.GetData(0, ref data))
+    {
+      return;
+    }
+
+    var outputParams = DeconstructObject(data);
     if (outputParams == null)
     {
       return;
     }
 
-    // update our discovered fields set
-    var currentFields = outputParams.Select(p => p.Param.Name).ToHashSet();
-    bool fieldsChanged = UpdateDiscoveredFields(currentFields);
-
     // set component name based on the current object
     NickName = Name;
 
-    // if this is the first iteration OR field set has changed, check if we need to update outputs
-    if (da.Iteration == 0 || fieldsChanged)
-    {
-      var requiredOutputs = CreateOutputsFromAllFields();
-
-      if (OutputMismatch(requiredOutputs))
-      {
-        OnPingDocument()?.ScheduleSolution(5, _ => CreateOutputs(requiredOutputs));
-        return;
-      }
-    }
-
-    // set output data - fill missing fields with nulls
+    // set output data - fill missing fields with nulls for objects that don't have all fields
     SetOutputData(da, outputParams);
   }
 
-  private List<OutputParamWrapper>? DeconstructObject(object data)
+  /// <summary>
+  /// Discovers all unique field names from all input objects by looking at volatile data directly.
+  /// </summary>
+  private HashSet<string> DiscoverAllFieldsFromInput()
   {
-    switch (data)
+    HashSet<string> allFields = new();
+
+    foreach (var item in Params.Input[0].VolatileData.AllData(true))
     {
-      case SpeckleCollectionWrapperGoo collectionGoo when collectionGoo.Value != null:
-        // get children elements from the wrapper to override the elements prop while parsing
-        var children = collectionGoo.Value.Elements.Select(o => ((SpeckleWrapper)o).CreateGoo()).ToList();
-        return ParseSpeckleWrapper(collectionGoo.Value, children);
-
-      case SpeckleDataObjectWrapperGoo dataObjectGoo when dataObjectGoo.Value != null:
-        // get geometries from the wrapper to override the displayvalue prop while parsing
-        var display = dataObjectGoo.Value.Geometries.Select(o => o.CreateGoo()).ToList();
-        return ParseSpeckleWrapper(dataObjectGoo.Value, null, display);
-
-      case SpeckleGeometryWrapperGoo objectGoo when objectGoo.Value != null:
-        return ParseSpeckleWrapper(objectGoo.Value);
-
-      case SpeckleBlockInstanceWrapperGoo blockInstanceGoo when blockInstanceGoo.Value != null:
-        return ParseSpeckleWrapper(blockInstanceGoo.Value);
-
-      case SpeckleBlockDefinitionWrapperGoo blockDef:
-        return ParseSpeckleWrapper(blockDef.Value);
-
-      case SpeckleMaterialWrapperGoo materialGoo when materialGoo.Value != null:
-        return ParseSpeckleWrapper(materialGoo.Value);
-
-      case SpecklePropertyGroupGoo propGoo:
-        Name = $"properties ({propGoo.Value.Count})";
-        List<OutputParamWrapper> objectOutputs = new();
-        foreach (var key in propGoo.Value.Keys)
-        {
-          ISpecklePropertyGoo value = propGoo.Value[key];
-          object? outputValue = value is SpecklePropertyGoo prop
-            ? prop.Value
-            : value is SpecklePropertyGroupGoo propGroup
-              ? propGroup
-              : value;
-          objectOutputs.Add(CreateOutputParamByKeyValue(key, outputValue, GH_ParamAccess.item));
-        }
-        return objectOutputs;
-
-      default:
-        AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"Type cannot be deconstructed: {data.GetType().Name}");
-        return null;
-    }
-  }
-
-  private bool UpdateDiscoveredFields(HashSet<string> currentFields)
-  {
-    bool changed = false;
-    foreach (string field in currentFields)
-    {
-      if (_allDiscoveredFields.Add(field))
+      var objectOutputs = DeconstructObject(item);
+      if (objectOutputs != null)
       {
-        changed = true;
+        foreach (var output in objectOutputs)
+        {
+          allFields.Add(output.Param.Name);
+        }
       }
     }
-    return changed;
+
+    return allFields;
   }
 
-  private List<OutputParamWrapper> CreateOutputsFromAllFields() =>
-    _allDiscoveredFields
+  /// <summary>
+  /// Creates output parameter wrappers from a set of field names, all with item access.
+  /// </summary>
+  private List<OutputParamWrapper> CreateOutputParamsFromFieldNames(HashSet<string> fieldNames) =>
+    fieldNames
       .OrderBy(name => name)
       .Select(fieldName => CreateOutputParamByKeyValue(fieldName, null, GH_ParamAccess.item))
       .ToList();
 
+  /// <summary>
+  /// Deconstructs a single object into its constituent fields/properties.
+  /// </summary>
+  private List<OutputParamWrapper>? DeconstructObject(object data) =>
+    data switch
+    {
+      // get children elements from wrapper to override elements prop while parsing
+      SpeckleCollectionWrapperGoo collectionGoo when collectionGoo.Value != null
+        => ParseSpeckleWrapper(
+          collectionGoo.Value,
+          collectionGoo.Value.Elements.Select(o => ((SpeckleWrapper)o).CreateGoo()).ToList()
+        ),
+
+      // get geometries from wrapper to override displayValue prop while parsing
+      SpeckleDataObjectWrapperGoo dataObjectGoo when dataObjectGoo.Value != null
+        => ParseSpeckleWrapper(
+          dataObjectGoo.Value,
+          null,
+          dataObjectGoo.Value.Geometries.Select(o => o.CreateGoo()).ToList()
+        ),
+
+      SpeckleGeometryWrapperGoo objectGoo when objectGoo.Value != null => ParseSpeckleWrapper(objectGoo.Value),
+
+      SpeckleBlockInstanceWrapperGoo blockInstanceGoo when blockInstanceGoo.Value != null
+        => ParseSpeckleWrapper(blockInstanceGoo.Value),
+
+      SpeckleBlockDefinitionWrapperGoo blockDef when blockDef.Value != null => ParseSpeckleWrapper(blockDef.Value),
+
+      SpeckleMaterialWrapperGoo materialGoo when materialGoo.Value != null => ParseSpeckleWrapper(materialGoo.Value),
+
+      SpecklePropertyGroupGoo propGoo when propGoo.Value != null => ParsePropertyGroup(propGoo),
+
+      _ => HandleUnsupportedType(data)
+    };
+
+  /// <summary>
+  /// Handles SpecklePropertyGroupGoo objects by extracting their key-value pairs.
+  /// </summary>
+  private List<OutputParamWrapper> ParsePropertyGroup(SpecklePropertyGroupGoo propGoo)
+  {
+    Name = $"properties ({propGoo.Value.Count})";
+    List<OutputParamWrapper> objectOutputs = new();
+
+    foreach (var key in propGoo.Value.Keys)
+    {
+      ISpecklePropertyGoo value = propGoo.Value[key];
+      object? outputValue = value switch
+      {
+        SpecklePropertyGoo prop => prop.Value,
+        SpecklePropertyGroupGoo propGroup => propGroup,
+        _ => value
+      };
+      objectOutputs.Add(CreateOutputParamByKeyValue(key, outputValue, GH_ParamAccess.item));
+    }
+
+    return objectOutputs;
+  }
+
+  /// <summary>
+  /// Handles unsupported object types by logging an error and returning null.
+  /// </summary>
+  private List<OutputParamWrapper>? HandleUnsupportedType(object data)
+  {
+    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"Type cannot be deconstructed: {data.GetType().Name}");
+    return null;
+  }
+
+  /// <summary>
+  /// Sets output data for the current iteration, filling missing fields with null values.
+  /// Uses a lookup dictionary for efficient field matching.
+  /// </summary>
   private void SetOutputData(IGH_DataAccess da, List<OutputParamWrapper> currentOutputs)
   {
+    if (Params.Output.Count == 0)
+    {
+      return;
+    }
+
     // create a lookup for current outputs by field name
     var outputLookup = currentOutputs.ToDictionary(o => o.Param.Name, o => o.Value);
 
@@ -147,10 +190,9 @@ public class DeconstructSpeckleParam : GH_Component, IGH_VariableParameterCompon
     for (int i = 0; i < Params.Output.Count; i++)
     {
       var outputParam = Params.Output[i];
-      string fieldName = outputParam.Name;
 
       // set the value if it exists, otherwise set null
-      object? value = outputLookup.TryGetValue(fieldName, out var fieldValue) ? fieldValue : null;
+      object? value = outputLookup.TryGetValue(outputParam.Name, out var fieldValue) ? fieldValue : null;
 
       switch (outputParam.Access)
       {
@@ -186,123 +228,144 @@ public class DeconstructSpeckleParam : GH_Component, IGH_VariableParameterCompon
       return result;
     }
 
-    // cycle through base props
+    // process each property of the Base object
     foreach (var prop in @base.GetMembers(DynamicBaseMemberType.Instance | DynamicBaseMemberType.Dynamic))
     {
-      // convert and add to corresponding output structure
-      var value = prop.Value;
-      switch (value)
+      // skip internal dynamic property keys
+      if (prop.Key == nameof(Base.DynamicPropertyKeys))
       {
-        case null:
-          result.Add(CreateOutputParamByKeyValue(prop.Key, null, GH_ParamAccess.item));
-          break;
+        continue;
+      }
 
-        case IList list:
-          List<object> nativeObjects = new();
-
-          // override list value if base is a collection and this is the elements prop
-          if (@base is Collection && prop.Key == "elements" && elements != null)
-          {
-            list = elements;
-          }
-
-          // override list value if base is a dataobject and this is the displayvalue prop
-          if (@base is Speckle.Objects.Data.DataObject && prop.Key == "displayValue" && displayValue != null)
-          {
-            list = displayValue;
-          }
-
-          foreach (var x in list)
-          {
-            switch (x)
-            {
-              case SpeckleWrapper wrapper:
-                nativeObjects.Add(wrapper.CreateGoo());
-                break;
-
-              case Base xBase:
-                nativeObjects.AddRange(ConvertOrCreateWrapper(xBase));
-                break;
-
-              default:
-                nativeObjects.Add(x);
-                break;
-            }
-          }
-
-          result.Add(CreateOutputParamByKeyValue(prop.Key, nativeObjects, GH_ParamAccess.list));
-          break;
-
-        case Dictionary<string, object?> dict: // this should be treated a properties dict
-          SpecklePropertyGroupGoo propertyGoo = new();
-          propertyGoo.CastFrom(dict);
-          result.Add(CreateOutputParamByKeyValue(prop.Key, propertyGoo, GH_ParamAccess.item));
-          break;
-
-        case SpeckleWrapper wrapper:
-          result.Add(CreateOutputParamByKeyValue(prop.Key, wrapper.CreateGoo(), GH_ParamAccess.item));
-          break;
-
-        case Base baseValue:
-          result.Add(CreateOutputParamByKeyValue(prop.Key, ConvertOrCreateWrapper(baseValue), GH_ParamAccess.list));
-          break;
-
-        default:
-          // we don't want to output dynamic property keys
-          if (prop.Key == nameof(Base.DynamicPropertyKeys))
-          {
-            continue;
-          }
-
-          result.Add(CreateOutputParamByKeyValue(prop.Key, prop.Value, GH_ParamAccess.item));
-          break;
+      var outputParam = CreateOutputParamForProperty(prop, @base, elements, displayValue);
+      if (outputParam != null)
+      {
+        result.Add(outputParam);
       }
     }
 
     return result;
   }
 
+  /// <summary>
+  /// Creates an output parameter for a single property, handling different value types appropriately.
+  /// </summary>
+  private OutputParamWrapper CreateOutputParamForProperty(
+    KeyValuePair<string, object?> prop,
+    Base @base,
+    List<IGH_Goo>? elements,
+    List<IGH_Goo>? displayValue
+  ) =>
+    prop.Value switch
+    {
+      null => CreateOutputParamByKeyValue(prop.Key, null, GH_ParamAccess.item),
+      IList list => CreateListOutputParam(prop.Key, list, @base, elements, displayValue),
+      Dictionary<string, object?> dict => CreateDictionaryOutputParam(prop.Key, dict),
+      SpeckleWrapper wrapper => CreateOutputParamByKeyValue(prop.Key, wrapper.CreateGoo(), GH_ParamAccess.item),
+      Base baseValue => CreateOutputParamByKeyValue(prop.Key, ConvertOrCreateWrapper(baseValue), GH_ParamAccess.list),
+      _ => CreateOutputParamByKeyValue(prop.Key, prop.Value, GH_ParamAccess.item)
+    };
+
+  /// <summary>
+  /// Creates an output parameter for list properties, with special handling for collection elements and display values.
+  /// </summary>
+  private OutputParamWrapper CreateListOutputParam(
+    string key,
+    IList list,
+    Base @base,
+    List<IGH_Goo>? elements,
+    List<IGH_Goo>? displayValue
+  )
+  {
+    // override list value for special cases
+    IList actualList = key switch
+    {
+      "elements" when @base is Collection && elements != null => elements,
+      "displayValue" when @base is Speckle.Objects.Data.DataObject && displayValue != null => displayValue,
+      _ => list
+    };
+
+    List<object> nativeObjects = new();
+    foreach (var item in actualList)
+    {
+      switch (item)
+      {
+        case SpeckleWrapper wrapper:
+          nativeObjects.Add(wrapper.CreateGoo());
+          break;
+        case Base baseItem:
+          nativeObjects.AddRange(ConvertOrCreateWrapper(baseItem));
+          break;
+        default:
+          nativeObjects.Add(item);
+          break;
+      }
+    }
+
+    return CreateOutputParamByKeyValue(key, nativeObjects, GH_ParamAccess.list);
+  }
+
+  /// <summary>
+  /// Creates an output parameter for dictionary properties, converting them to SpecklePropertyGroupGoo.
+  /// </summary>
+  private OutputParamWrapper CreateDictionaryOutputParam(string key, Dictionary<string, object?> dict)
+  {
+    SpecklePropertyGroupGoo propertyGoo = new();
+    propertyGoo.CastFrom(dict);
+    return CreateOutputParamByKeyValue(key, propertyGoo, GH_ParamAccess.item);
+  }
+
+  /// <summary>
+  /// Converts a Speckle Base object to host geometry or creates a wrapper if conversion fails.
+  /// Returns a list of SpeckleGeometryWrapperGoo objects.
+  /// </summary>
   private List<SpeckleGeometryWrapperGoo> ConvertOrCreateWrapper(Base @base)
   {
     try
     {
-      // convert the base and create a wrapper for each result
+      // attempt conversion to host geometry
       List<(object, Base)> convertedBase = SpeckleConversionContext.Current.ConvertToHost(@base);
-      List<SpeckleGeometryWrapperGoo> convertedWrappers = new();
-      foreach ((object o, Base b) in convertedBase)
-      {
-        GeometryBase? g = o as GeometryBase;
-        SpeckleGeometryWrapper convertedWrapper =
-          new()
-          {
-            Base = b,
-            GeometryBase = g,
-            Name = b["name"] as string ?? "",
-            Color = null,
-            Material = null
-          };
-
-        convertedWrappers.Add(new(convertedWrapper));
-      }
-
-      return convertedWrappers;
+      return convertedBase.Select(CreateGeometryWrapper).ToList();
     }
     catch (ConversionException)
     {
-      // some classes, like RawEncoding, have no direct conversion or fallback value.
-      // when this is the case, wrap it to allow users to further expand the object.
-      SpeckleGeometryWrapper convertedWrapper =
-        new()
-        {
-          Base = @base,
-          GeometryBase = null,
-          Name = @base[Constants.NAME_PROP] as string ?? "",
-          Color = null,
-          Material = null
-        };
-
-      return new() { new SpeckleGeometryWrapperGoo(convertedWrapper) };
+      // fallback: create wrapper without conversion for objects that can't be converted
+      return new List<SpeckleGeometryWrapperGoo> { CreateFallbackWrapper(@base) };
     }
+  }
+
+  /// <summary>
+  /// Creates a SpeckleGeometryWrapperGoo from a converted geometry and base object pair.
+  /// </summary>
+  private SpeckleGeometryWrapperGoo CreateGeometryWrapper((object geometry, Base @base) converted)
+  {
+    SpeckleGeometryWrapper wrapper =
+      new()
+      {
+        Base = converted.@base,
+        GeometryBase = converted.geometry as GeometryBase,
+        Name = converted.@base["name"] as string ?? "",
+        Color = null,
+        Material = null
+      };
+    return new SpeckleGeometryWrapperGoo(wrapper);
+  }
+
+  /// <summary>
+  /// Creates a fallback wrapper for Base objects that cannot be converted to host geometry.
+  /// </summary>
+  private SpeckleGeometryWrapperGoo CreateFallbackWrapper(Base @base)
+  {
+    SpeckleGeometryWrapper wrapper =
+      new()
+      {
+        Base = @base,
+        GeometryBase = null,
+        Name = @base[Constants.NAME_PROP] as string ?? "",
+        Color = null,
+        Material = null
+      };
+    return new SpeckleGeometryWrapperGoo(wrapper);
   }
 
   private OutputParamWrapper CreateOutputParamByKeyValue(string key, object? value, GH_ParamAccess access)
@@ -341,13 +404,13 @@ public class DeconstructSpeckleParam : GH_Component, IGH_VariableParameterCompon
 
   private void CreateOutputs(List<OutputParamWrapper> outputParams)
   {
-    // clear existing outputs
+    // remove all existing output parameters
     while (Params.Output.Count > 0)
     {
       Params.UnregisterOutputParameter(Params.Output[^1]);
     }
 
-    // create new outputs
+    // add new output parameters
     foreach (var newParam in outputParams)
     {
       var param = new Param_GenericObject
@@ -360,11 +423,15 @@ public class DeconstructSpeckleParam : GH_Component, IGH_VariableParameterCompon
       Params.RegisterOutputParam(param);
     }
 
+    // notify Grasshopper of parameter changes
     Params.OnParametersChanged();
     VariableParameterMaintenance();
     ExpireSolution(false);
   }
 
+  /// <summary>
+  /// Determines if the current output parameter structure differs from the required structure.
+  /// </summary>
   private bool OutputMismatch(List<OutputParamWrapper> outputParams)
   {
     if (Params.Output.Count != outputParams.Count)
@@ -387,16 +454,6 @@ public class DeconstructSpeckleParam : GH_Component, IGH_VariableParameterCompon
     }
 
     return false;
-  }
-
-  // NOTE: Override ExpirePreview to reset discovered fields when component is expired
-  public override void ExpirePreview(bool recompute)
-  {
-    if (recompute)
-    {
-      _allDiscoveredFields.Clear();
-    }
-    base.ExpirePreview(recompute);
   }
 }
 
