20bc675ad2ec1428dbf834650993eb5fb7d66792
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-09-02 16:54:33 +0100

    Disable 3dm config due to bug in OpenHeadless (#1062)

--- Importers/Rhino/Speckle.Importers.Rhino/Internal/FileTypeConfig/Rhino3dmConfig.cs
+++ Importers/Rhino/Speckle.Importers.Rhino/Internal/FileTypeConfig/Rhino3dmConfig.cs
@@ -8,8 +8,14 @@ namespace Speckle.Importers.Rhino.Internal.FileTypeConfig;
 /// </summary>
 public sealed class Rhino3dmConfig : IFileTypeConfig
 {
+  [Obsolete("Bugged, don't use until fixed")]
   public RhinoDoc OpenInHeadlessDocument(string filePath)
   {
+    // There is a bug in `OpenHeadless`
+    // that creates UI dismissable popups about missing font warnings
+    // see https://discourse.mcneel.com/t/rhino-inside-headless-displays-popup-about-missing-fonts/209173/4
+    // For this reason, this function should not be used until it's fixed
+
     RhinoDoc? doc = RhinoDoc.OpenHeadless(filePath);
     if (doc is null)
     {
