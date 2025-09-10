30dcabd58d78078012debaef3415a66368157706
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-09-10 11:52:20 +0100

    Refactor metrics to avoid fallbacks

--- Sdk/Speckle.Connectors.Common/Analytics/MixPanelManager.cs
+++ Sdk/Speckle.Connectors.Common/Analytics/MixPanelManager.cs
@@ -1,5 +1,4 @@
 using System.Net.Http.Headers;
-using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Web;
@@ -16,7 +15,7 @@ using System.Net.Http;
 namespace Speckle.Connectors.Common.Analytics;
 
 /// <summary>
-///  Anonymous telemetry to help us understand how to make a better Speckle.
+///  Lightweight Telemetry to help us understand how to make a better Speckle.
 ///  This really helps us to deliver a better open source project and product!
 /// </summary>
 [GenerateAutoInterface]
@@ -25,16 +24,6 @@ public class MixPanelManager(ISpeckleApplication application, ISpeckleHttp speck
 {
   private const string MIXPANEL_TOKEN = "acd87c5a50b56df91a795e999812a3a4";
   private static readonly Uri s_mixpanelServer = new("https://analytics.speckle.systems");
-
-  /// <summary>
-  /// Cached email
-  /// </summary>
-  private string? LastEmail { get; set; }
-
-  /// <summary>
-  /// Cached server URL
-  /// </summary>
-  private string? LastServer { get; set; }
 
   /// <summary>
   /// <see langword="false"/> when the DEBUG pre-processor directive is <see langword="true"/>, <see langword="false"/> otherwise
@@ -47,77 +36,36 @@ public class MixPanelManager(ISpeckleApplication application, ISpeckleHttp speck
     true;
 #endif
 
-  /// <summary>
-  /// Tracks an event without specifying the email and server.
-  /// It's not always possible to know which account the user has selected, especially in visual programming.
-  /// Therefore we are caching the email and server values so that they can be used also when nodes such as "Serialize" are used.
-  /// If no account info is cached, we use the default account data.
-  /// </summary>
-  /// <param name="eventName">Name of the even</param>
-  /// <param name="customProperties">Additional parameters to pass in to event</param>
-  /// <param name="isAction">True if it's an action performed by a logged user</param>
   public async Task TrackEvent(
     MixPanelEvents eventName,
-    Account? account,
+    Account account,
     Dictionary<string, object>? customProperties = null,
     bool isAction = true
   )
   {
-    string? email = account?.userInfo.email;
-    string? hashedEmail;
-    string? server;
-
-    if (LastEmail != null && LastServer != null && LastServer != "no-account-server")
-    {
-      hashedEmail = LastEmail;
-      server = LastServer;
-    }
-    else
-    {
-      if (account == null)
-      {
-        var macAddr = NetworkInterface
-          .GetAllNetworkInterfaces()
-          .Where(nic =>
-            nic.OperationalStatus == OperationalStatus.Up && nic.NetworkInterfaceType != NetworkInterfaceType.Loopback
-          )
-          .Select(nic => nic.GetPhysicalAddress().ToString())
-          .FirstOrDefault();
-
-        hashedEmail = macAddr;
-        server = "no-account-server";
-        isAction = false;
-      }
-      else
-      {
-        hashedEmail = account.GetHashedEmail();
-        server = account.GetHashedServer();
-      }
-    }
-
-    await TrackEvent(hashedEmail, server, eventName, email, customProperties, isAction);
+    var hashedEmail = account.GetHashedEmail();
+    var hashedServer = account.GetHashedServer();
+    await TrackEvent(hashedEmail, hashedServer, eventName, account.userInfo.email, customProperties, isAction);
   }
 
   /// <summary>
-  /// Tracks an event from a specified email and server, anonymizes personal information
+  /// Tracks an event from a specified email and server
   /// </summary>
   /// <param name="hashedEmail">Email of the user anonymized</param>
   /// <param name="hashedServer">Server URL anonymized</param>
   /// <param name="eventName">Name of the event</param>
+  /// <param name="email">Email user</param>
   /// <param name="customProperties">Additional parameters to pass to the event</param>
   /// <param name="isAction">True if it's an action performed by a logged user</param>
   private async Task TrackEvent(
-    string? hashedEmail,
+    string hashedEmail,
     string hashedServer,
     MixPanelEvents eventName,
-    string? email,
+    string email,
     Dictionary<string, object>? customProperties = null,
     bool isAction = true
   )
   {
-    LastEmail = hashedEmail;
-    LastServer = hashedServer;
-
     if (!IsReleaseMode)
     {
       //only track in prod
@@ -128,20 +76,16 @@ public class MixPanelManager(ISpeckleApplication application, ISpeckleHttp speck
     {
       var properties = new Dictionary<string, object>
       {
-        { "distinct_id", hashedEmail ?? string.Empty },
+        { "distinct_id", hashedEmail },
         { "server_id", hashedServer },
         { "token", MIXPANEL_TOKEN },
         { "hostApp", application.Slug },
         { "ui", "dui3" }, // this is the convention we use with next gen
         { "hostAppVersion", application.HostApplicationVersion },
         { "core_version", application.SpeckleVersion },
-        { "$os", GetOs() }
+        { "$os", GetOs() },
+        { "email", email },
       };
-
-      if (email != null)
-      {
-        properties.Add("email", email);
-      }
 
       if (isAction)
       {
