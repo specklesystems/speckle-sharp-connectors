16c3a49933fd209279bda36311d964b32b207902
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-09-15 16:03:36 +0100

    Windows Serviceing

--- Importers/Rhino/Speckle.Importers.JobProcessor/Program.cs
+++ Importers/Rhino/Speckle.Importers.JobProcessor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Speckle.Importers.JobProcessor.Domain;
 using Speckle.Importers.JobProcessor.JobHandlers;
@@ -9,41 +10,38 @@ namespace Speckle.Importers.JobProcessor;
 
 public static class Program
 {
-  public static async Task Main()
+  public static async Task Main(string[] args)
   {
-    ILogger? logger = null;
-    try
+    // Dapper doesn't understand how to handle JSON deserialization, so we need to tell it what types can be deserialzied
+    SqlMapper.AddTypeHandler(new JsonHandler<FileimportPayload>());
+
+    var host = ConfigureAppHost(args);
+
+    ConfigureTopLevelLogs(host.Services.GetRequiredService<ILogger<object>>());
+
+    await host.RunAsync();
+  }
+
+  private static IHost ConfigureAppHost(string[] args)
+  {
+    // DI setup
+    var builder = Host.CreateApplicationBuilder(args);
+
+    builder.Services.AddJobProcessor();
+    builder.Services.AddWindowsService();
+    builder.Services.AddTransient<IJobHandler, RhinoJobHandler>();
+
+    return builder.Build();
+  }
+
+  private static void ConfigureTopLevelLogs(ILogger logger)
+  {
+    TaskScheduler.UnobservedTaskException += (_, eventArgs) =>
+      logger.LogCritical(eventArgs.Exception, "Unobserved Task Exception");
+
+    AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
     {
-      // Dapper doesn't understand how to handle JSON deserialization, so we need to tell it what types can be deserialzied
-      SqlMapper.AddTypeHandler(new JsonHandler<FileimportPayload>());
-
-      // DI setup
-      var serviceCollection = new ServiceCollection();
-      serviceCollection.AddJobProcessor();
-
-      serviceCollection.AddTransient<IJobHandler, RhinoJobHandler>();
-
-      var serviceProvider = serviceCollection.BuildServiceProvider();
-      logger = serviceProvider.GetRequiredService<ILogger<object>>();
-      TaskScheduler.UnobservedTaskException += (_, eventArgs) =>
-        logger.LogCritical(eventArgs.Exception, "Unobserved Task Exception");
-
-      var processor = serviceProvider.GetRequiredService<JobProcessorInstance>();
-
-      await processor.StartProcessing();
-    }
-    catch (Exception ex)
-    {
-      const string MESSAGE = "Unhandled exception reached entry point";
-      if (logger is not null)
-      {
-        logger.LogCritical(ex, MESSAGE);
-      }
-      else
-      {
-        Console.WriteLine(MESSAGE);
-      }
-      throw;
-    }
+      logger.LogCritical(eventArgs.ExceptionObject as Exception, "Unhandled exception occurred in the AppDomain");
+    };
   }
 }
