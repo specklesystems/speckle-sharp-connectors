b3705d58db8f7325160cedcd98fa3df2c4b7dd1e
Adam Hathcock <adam@hathcock.uk>
2025-09-17 10:52:26 +0100

    move doc null check

--- Connectors/Revit/Speckle.Connectors.RevitShared/Bindings/RevitSendBinding.cs
+++ Connectors/Revit/Speckle.Connectors.RevitShared/Bindings/RevitSendBinding.cs
@@ -236,17 +236,16 @@ internal sealed class RevitSendBinding : RevitBaseBinding, ISendBinding
   private void DocChangeHandler(Autodesk.Revit.DB.Events.DocumentChangedEventArgs e)
   {
     ICollection<ElementId> modifiedElementIds = e.GetModifiedElementIds();
-
+    var doc = e.GetDocument();
+    if (doc == null)
+    {
+      return;
+    }
     // NOTE: Whenever we save data into file this event also trigger changes on its DataStorage.
     // On every add/remove/update model attempt triggers this handler and was causing unnecessary calls on `RunExpirationChecks`
     // Re-check it once we implement Linked Documents
     if (modifiedElementIds.Count == 1)
     {
-      var doc = e.GetDocument();
-      if (doc == null)
-      {
-        return;
-      }
       if (modifiedElementIds.All(el => doc.GetElement(el) is DataStorage))
       {
         return;
@@ -276,7 +275,7 @@ internal sealed class RevitSendBinding : RevitBaseBinding, ISendBinding
       _idleManager.SubscribeToIdle(nameof(PostSetObjectIds), PostSetObjectIds);
     }
 
-    if (HaveUnitsChanged(e.GetDocument()))
+    if (HaveUnitsChanged(doc))
     {
       var objectIds = new List<string>();
       foreach (var sender in _store.GetSenders().ToList())
@@ -289,7 +288,7 @@ internal sealed class RevitSendBinding : RevitBaseBinding, ISendBinding
         var selectedObjects = sender.SendFilter.NotNull().SelectedObjectIds;
         objectIds.AddRange(selectedObjects);
       }
-      var unpackedObjectIds = _elementUnpacker.GetUnpackedElementIds(objectIds, e.GetDocument());
+      var unpackedObjectIds = _elementUnpacker.GetUnpackedElementIds(objectIds, doc);
       _sendConversionCache.EvictObjects(unpackedObjectIds);
     }
 
