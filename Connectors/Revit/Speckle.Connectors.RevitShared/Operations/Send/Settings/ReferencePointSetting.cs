d3b50b9aa77ecd0ba8e9ac724347bbeb38ef4e4a
Björn <steinhagen.bjoern@gmail.com>
2025-09-17 15:45:47 +0200

    fix: cleanup

--- Connectors/Revit/Speckle.Connectors.RevitShared/Operations/Send/Settings/ReferencePointSetting.cs
+++ Connectors/Revit/Speckle.Connectors.RevitShared/Operations/Send/Settings/ReferencePointSetting.cs
@@ -1,7 +1,5 @@
-using System.ComponentModel;
 using Speckle.Connectors.DUI.Settings;
 using Speckle.Converters.RevitShared.Settings;
-using Speckle.Newtonsoft.Json;
 
 namespace Speckle.Connectors.Revit.Operations.Send.Settings;
 
@@ -11,9 +9,6 @@ public class ReferencePointSetting(ReferencePointType value) : ICardSetting
   public string? Title { get; set; } = "Reference Point";
   public string? Type { get; set; } = "string";
   public List<string>? Enum { get; set; } = System.Enum.GetNames(typeof(ReferencePointType)).ToList();
-
-  [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-  [DefaultValue("InternalOrigin")]
   public object? Value { get; set; } = value.ToString();
 
   public static readonly Dictionary<string, ReferencePointType> ReferencePointMap = System
