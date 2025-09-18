16770a38d6f92f4d388017839bbc014640f75d36
Björn <steinhagen.bjoern@gmail.com>
2025-09-18 12:08:48 +0200

    chore: docs

--- Converters/Rhino/Speckle.Converters.RhinoShared/ToHost/Raw/IntervalToHostConverter.cs
+++ Converters/Rhino/Speckle.Converters.RhinoShared/ToHost/Raw/IntervalToHostConverter.cs
@@ -11,12 +11,14 @@ public class IntervalToHostConverter : ITypedConverter<SOP.Interval, RG.Interval
   /// </summary>
   /// <param name="target">The Speckle Interval to convert.</param>
   /// <returns>The converted Rhino.Geometry.Interval object.</returns>
-  /// <exception cref="ArgumentException">Thrown when the start or end value of the Interval is null.</exception>
+  /// <exception cref="ConversionException"> Throws when the Interval is null.</exception>
   /// <remarks>⚠️ This conversion does NOT perform scaling.</remarks>
   public RG.Interval Convert([NotNull] SOP.Interval? target)
   {
     if (target == null)
     {
+      // assuming user trying to receive old (v2) data without domains. Or other (non speckle-sharp) connectors aren't
+      // enforcing domains (Sketchup, ArchiCAD)?
       throw new ConversionException("Cannot convert a null Interval.  Check your Rhino model.");
     }
 
