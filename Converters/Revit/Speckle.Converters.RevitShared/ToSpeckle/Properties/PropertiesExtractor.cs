84e6118e1ee2dc383569e2ef33fdb6a1edbdcef6
Adam Hathcock <adam@hathcock.uk>
2025-09-18 13:59:59 +0100

    Revit: update mat quantites for ODA

--- Converters/Revit/Speckle.Converters.RevitShared/ToSpeckle/Properties/PropertiesExtractor.cs
+++ Converters/Revit/Speckle.Converters.RevitShared/ToSpeckle/Properties/PropertiesExtractor.cs
@@ -1,38 +1,24 @@
-using Speckle.Converters.Common.Objects;
-
 namespace Speckle.Converters.RevitShared.ToSpeckle.Properties;
 
-public class PropertiesExtractor
+public class PropertiesExtractor(
+  ClassPropertiesExtractor classPropertiesExtractor,
+  ParameterExtractor parameterExtractor,
+  IMaterialQuantitiesToSpeckleLite materialQuantityConverter)
 {
-  private readonly ClassPropertiesExtractor _classPropertiesExtractor;
-  private readonly ParameterExtractor _parameterExtractor;
-  private readonly ITypedConverter<DB.Element, Dictionary<string, object>> _materialQuantityConverter;
-
-  public PropertiesExtractor(
-    ClassPropertiesExtractor classPropertiesExtractor,
-    ParameterExtractor parameterExtractor,
-    ITypedConverter<DB.Element, Dictionary<string, object>> materialQuantityConverter
-  )
-  {
-    _classPropertiesExtractor = classPropertiesExtractor;
-    _parameterExtractor = parameterExtractor;
-    _materialQuantityConverter = materialQuantityConverter;
-  }
-
   public Dictionary<string, object?> GetProperties(DB.Element element)
   {
     // by default, always get class properties first
-    Dictionary<string, object?> properties = _classPropertiesExtractor.GetClassProperties(element);
+    Dictionary<string, object?> properties = classPropertiesExtractor.GetClassProperties(element);
 
     // add material quantities
-    Dictionary<string, object> matQuantities = _materialQuantityConverter.Convert(element);
+    Dictionary<string, object> matQuantities = materialQuantityConverter.Convert(element);
     if (matQuantities.Count > 0)
     {
       properties.Add("Material Quantities", matQuantities);
     }
 
     // add parameters
-    Dictionary<string, object?> parameters = _parameterExtractor.GetParameters(element);
+    Dictionary<string, object?> parameters = parameterExtractor.GetParameters(element);
     if (parameters.Count > 0)
     {
       properties.Add("Parameters", parameters);
