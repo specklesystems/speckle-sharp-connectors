4ef9656692ca7b3a35e0ebafa88cc6ae2afb4ecd
Claire Kuang <kuang.claire@gmail.com>
2025-09-30 18:08:54 +0100

    fixes transform matrix

--- Converters/Autocad/Speckle.Converters.AutocadShared/ReferencePointConverter.cs
+++ Converters/Autocad/Speckle.Converters.AutocadShared/ReferencePointConverter.cs
@@ -109,21 +109,21 @@ public class ReferencePointConverter(IConverterSettingsStore<AutocadConversionSe
 
   private Matrix4x4 ConvertToSpeckle(Matrix3d m) =>
     new(
+      m[0, 0],
+      m[0, 1],
+      m[0, 2],
+      m[0, 3],
+      m[1, 0],
       m[1, 1],
       m[1, 2],
       m[1, 3],
-      m[1, 4],
+      m[2, 0],
       m[2, 1],
       m[2, 2],
       m[2, 3],
-      m[2, 4],
+      m[3, 0],
       m[3, 1],
       m[3, 2],
-      m[3, 3],
-      m[3, 4],
-      m[4, 1],
-      m[4, 2],
-      m[4, 3],
-      m[4, 4]
+      m[3, 3]
     );
 }
