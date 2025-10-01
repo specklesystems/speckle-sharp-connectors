2d0b1a3a241156d13033438170053148e9465f76
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-10-01 12:06:43 +0100

    chore: Remove legacy .NET based IFC importer (#1118)
    * remove IFC source files
    
    * re-generate slnx files

--- Importers/Ifc/Speckle.Importers.Ifc/Types/IfcLine.cs
+++
@@ -1,9 +0,0 @@
-namespace Speckle.Importers.Ifc.Types;
-
-public sealed class IfcLine(IntPtr line)
-{
-  public uint Id => Importers.Ifc.Native.WebIfc.GetLineId(line);
-  public IfcSchemaType Type => (IfcSchemaType)Importers.Ifc.Native.WebIfc.GetLineType(line);
-
-  public string Arguments() => Importers.Ifc.Native.WebIfc.GetLineArguments(line);
-}
