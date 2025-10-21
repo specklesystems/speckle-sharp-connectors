c6b5aa2c920a29fb8b8a1479d61b3358ade838db
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-10-21 22:53:07 +0100

    Use main thread

---
+++ Importers/Rhino/Speckle.Importers.Rhino/Internal/ImporterInstanceFactory.cs
@@ -0,0 +1,9 @@
+using Microsoft.Extensions.Logging;
+
+namespace Speckle.Importers.Rhino.Internal;
+
+internal sealed class ImporterInstanceFactory(Sender sender, ILogger<ImporterInstance> logger)
+{
+
+  public ImporterInstance Create(ImporterArgs args) => new(args, sender, logger);
+}
