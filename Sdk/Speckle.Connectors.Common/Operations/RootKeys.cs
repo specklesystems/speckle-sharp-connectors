a36b5a0315f37d338f4a9ab869c88b4ed5d8fdd3
Björn Steinhagen <88777268+bjoernsteinhagen@users.noreply.github.com>
2025-11-12 10:28:26 +0300

    refactor: reference point transfrom to root keys

--- Sdk/Speckle.Connectors.Common/Operations/RootKeys.cs
+++ Sdk/Speckle.Connectors.Common/Operations/RootKeys.cs
@@ -3,4 +3,5 @@ namespace Speckle.Connectors.Common.Operations;
 public static class RootKeys
 {
   public const string ANALYSIS_RESULTS = "analysisResults";
+  public const string REFERENCE_POINT_TRANSFORM = "referencePointTransform";
 }
