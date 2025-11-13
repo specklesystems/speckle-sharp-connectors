db559b6d7ee2dd5ddf12761677a2f69d664d1bcb
Claire Kuang <kuang.claire@gmail.com>
2025-11-13 21:05:12 +0000

    cleans up root keys and package locks

--- Sdk/Speckle.Connectors.Common/Operations/ProxyKeys.cs
+++ Sdk/Speckle.Connectors.Common/Operations/ProxyKeys.cs
@@ -12,9 +12,3 @@ public static class ProxyKeys
   public const string SECTION = "sectionProxies";
   public const string LEVEL = "levelProxies";
 }
-
-public static class RootKeys
-{
-  public const string VIEW = "views";
-  public const string REFERENCE_POINT = "referencePointTransform";
-}
