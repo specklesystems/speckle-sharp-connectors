9f6f582ba5c11fc34b7518425f70a0154877752f
Jonathon Broughton <jonathon@stardotbmp.com>
2026-01-07 10:13:58 +0000

    replaces DisplayValueExtractor with a new implementation
    Redefines the DisplayValueExtractor to simplify dependencies by removing unnecessary components.
    
    Updates GetDisplayValue method for cleaner logic and ensures it handles null model items more gracefully.

--- Converters/Navisworks/Speckle.Converters.NavisworksShared/DataExtractors/DisplayValueExtractor.cs
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/DataExtractors/DisplayValueExtractor.cs
@@ -1,38 +1,17 @@
-﻿using Microsoft.Extensions.Logging;
-using Speckle.Converter.Navisworks.Settings;
-using Speckle.Converters.Common;
-using Speckle.Sdk.Models;
+﻿using Speckle.Sdk.Models;
 using static Speckle.Converter.Navisworks.Helpers.ElementSelectionHelper;
 
 namespace Speckle.Converter.Navisworks.ToSpeckle;
 
-public class DisplayValueExtractor
+public class DisplayValueExtractor(GeometryToSpeckleConverter geometryConverter)
 {
-  private readonly IConverterSettingsStore<NavisworksConversionSettings> _converterSettings;
-  private readonly ILogger<DisplayValueExtractor> _logger;
-  private readonly GeometryToSpeckleConverter _geometryConverter;
+  private readonly GeometryToSpeckleConverter _geometryConverter =
+    geometryConverter ?? throw new ArgumentNullException(nameof(geometryConverter));
 
-  public DisplayValueExtractor(
-    IConverterSettingsStore<NavisworksConversionSettings> converterSettings,
-    ILogger<DisplayValueExtractor> logger
-  )
-  {
-    _converterSettings = converterSettings;
-    _logger = logger;
-    _geometryConverter = new GeometryToSpeckleConverter(_converterSettings.Current);
-  }
-
-  internal List<Base> GetDisplayValue(NAV.ModelItem modelItem)
-  {
-    if (modelItem == null)
-    {
-      throw new ArgumentNullException(nameof(modelItem));
-    }
-    if (!modelItem.HasGeometry)
-    {
-      return [];
-    }
-
-    return !IsElementVisible(modelItem) ? [] : _geometryConverter.Convert(modelItem);
-  }
+  internal List<Base> GetDisplayValue(NAV.ModelItem modelItem) =>
+    modelItem == null
+      ? throw new ArgumentNullException(nameof(modelItem))
+      : !modelItem.HasGeometry || !IsElementVisible(modelItem)
+        ? []
+        : _geometryConverter.Convert(modelItem);
 }
