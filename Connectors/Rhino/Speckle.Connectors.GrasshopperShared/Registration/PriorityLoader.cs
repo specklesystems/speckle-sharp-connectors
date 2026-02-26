7b7b2e337feceff30f74f884addc3d7a1de7f071
oguzhankoral <oguzhankoral@gmail.com>
2026-02-26 15:02:26 +0300

    Fix registration for new builder

--- Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Registration/PriorityLoader.cs
+++ Connectors/Rhino/Speckle.Connectors.GrasshopperShared/Registration/PriorityLoader.cs
@@ -66,7 +66,7 @@ public class PriorityLoader : GH_AssemblyPriority
 
       // send
       services.AddTransient<IRootObjectBuilder<SpeckleCollectionWrapperGoo>, GrasshopperRootObjectBuilder>();
-      services.AddScoped<
+      services.AddTransient<
         IRootContinuousTraversalBuilder<SpeckleCollectionWrapperGoo>,
         GrasshopperContinuousTraversalBuilder
       >();
