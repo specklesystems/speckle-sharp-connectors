453192ae118d0653f1c9f8c0384405fb096ad079
Björn Steinhagen <steinhagen.bjoern@gmail.com>
2026-05-05 17:45:20 +0200

    feat(rhino): adds settings tooltips

--- Connectors/Rhino/Speckle.Connectors.RhinoShared/Operations/Send/Settings/AddVisualizationProperties.cs
+++ Connectors/Rhino/Speckle.Connectors.RhinoShared/Operations/Send/Settings/AddVisualizationProperties.cs
@@ -5,7 +5,8 @@ namespace Speckle.Connectors.Rhino.Operations.Send.Settings;
 public class AddVisualizationProperties(bool value) : ICardSetting
 {
   public string? Id { get; set; } = "addVisualizationProperties";
-  public string? Title { get; set; } = "Add visualization properties for meshes (will increase model size)";
+  public string? Title { get; set; } = "Add Mesh Visualization Properties";
+  public string? Description { get; set; } = "Embeds vertex colours and texture coordinates so meshes render as they do in Rhino. Will increase model size.";
   public string? Type { get; set; } = "boolean";
   public object? Value { get; set; } = value;
   public List<string>? Enum { get; set; }
