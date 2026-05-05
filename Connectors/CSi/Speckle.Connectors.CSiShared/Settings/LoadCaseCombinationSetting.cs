9b8e6b986de6b82273a3cb8ad5b06e8e2c132bd3
Björn Steinhagen <steinhagen.bjoern@gmail.com>
2026-05-05 18:02:51 +0200

    feat(csi): adds settings tooltips

--- Connectors/CSi/Speckle.Connectors.CSiShared/Settings/LoadCaseCombinationSetting.cs
+++ Connectors/CSi/Speckle.Connectors.CSiShared/Settings/LoadCaseCombinationSetting.cs
@@ -7,6 +7,7 @@ public class LoadCaseCombinationSetting(List<string> values, cSapModel sapModel)
 {
   public string? Id { get; set; } = "loadCasesAndCombinations";
   public string? Title { get; set; } = "Load Cases & Combinations";
+  public string? Description { get; set; }
   public string? Type { get; set; } = "array";
   public object? Value { get; set; } = values;
   public List<string>? Enum { get; set; } = LoadCaseHelper.GetLoadCasesAndCombinations(sapModel);
