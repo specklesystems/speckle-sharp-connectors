80d8974e04cc23af078da13e6329b1816c81b067
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2026-05-13 16:35:44 +0100

    autocad 2027

--- Build/Consts.cs
+++ Build/Consts.cs
@@ -31,6 +31,7 @@ internal static class Consts
         new("Connectors/Autocad/Speckle.Connectors.Autocad2024", "net48"),
         new("Connectors/Autocad/Speckle.Connectors.Autocad2025", "net8.0-windows"),
         new("Connectors/Autocad/Speckle.Connectors.Autocad2026", "net8.0-windows"),
+        new("Connectors/Autocad/Speckle.Connectors.Autocad2027", "net10.0-windows"),
       ]
     ),
     new(
@@ -41,9 +42,16 @@ internal static class Consts
         new("Connectors/Autocad/Speckle.Connectors.Civil3d2024", "net48"),
         new("Connectors/Autocad/Speckle.Connectors.Civil3d2025", "net8.0-windows"),
         new("Connectors/Autocad/Speckle.Connectors.Civil3d2026", "net8.0-windows"),
+        new("Connectors/Autocad/Speckle.Connectors.Civil3d2027", "net10.0-windows"),
       ]
     ),
-    new("plant3d", [new("Connectors/Autocad/Speckle.Connectors.Plant3d2026", "net8.0-windows")]),
+    new(
+      "plant3d",
+      [
+        new("Connectors/Autocad/Speckle.Connectors.Plant3d2026", "net8.0-windows"),
+        new("Connectors/Autocad/Speckle.Connectors.Plant3d2027", "net10.0-windows"),
+      ]
+    ),
     new(
       "navisworks",
       [
--- Connectors/Autocad/Speckle.Connectors.AutocadShared/Plugin/AppUtils.cs
+++ Connectors/Autocad/Speckle.Connectors.AutocadShared/Plugin/AppUtils.cs
@@ -12,11 +12,13 @@ public static class AppUtils
 #elif AUTOCAD
     HostApplications.AutoCAD;
 #else
-    throw new NotSupportedException();
+#error Compiler directives misconfigured or the autocad vertical is not supported
 #endif
 
   public static HostAppVersion Version =>
-#if AUTOCAD2026 || CIVIL3D2026 || PLANT3D2026
+#if AUTOCAD2027 || CIVIL3D2027 || PLANT3D2027
+    HostAppVersion.v2027;
+#elif AUTOCAD2026 || CIVIL3D2026 || PLANT3D2026
     HostAppVersion.v2026;
 #elif AUTOCAD2025 || CIVIL3D2025 || PLANT3D2025
     HostAppVersion.v2025;
@@ -27,6 +29,6 @@ public static class AppUtils
 #elif AUTOCAD2022 || CIVIL3D2022
     HostAppVersion.v2022;
 #else
-    throw new NotSupportedException();
+#error Compiler directives misconfigured or the autocad version is not supported
 #endif
 }
