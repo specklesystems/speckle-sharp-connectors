3d91691830c3c455f8691f7cc2cc5791341fbb06
Jonathon Broughton <jonathon@stardotbmp.com>
2026-05-17 17:28:20 +0100

    include class properties in essential detail level

--- Converters/Navisworks/Speckle.Converters.NavisworksShared/DataHandlers/StandardPropertyHandler.cs
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/DataHandlers/StandardPropertyHandler.cs
@@ -10,8 +10,47 @@ public class StandardPropertyHandler(
   PropertySetsExtractor propertySetsExtractor,
   ModelPropertiesExtractor modelPropertiesExtractor,
   InternalPropertiesExtractor internalPropertiesExtractor,
+  ClassPropertiesExtractor classPropertiesExtractor,
   IConverterSettingsStore<NavisworksConversionSettings> settingsStore
 ) : BasePropertyHandler(propertySetsExtractor, modelPropertiesExtractor, internalPropertiesExtractor, settingsStore)
 {
-  public override Dictionary<string, object?> GetProperties(NAV.ModelItem modelItem) => ProcessPropertySets(modelItem);
+  private static readonly HashSet<string> s_essentialClassProperties = new(StringComparer.Ordinal)
+  {
+    "InstanceGuid",
+    "DisplayName",
+    "ClassName",
+  };
+
+  public override Dictionary<string, object?> GetProperties(NAV.ModelItem modelItem)
+  {
+    var properties = ProcessPropertySets(modelItem);
+    var classProperties = classPropertiesExtractor.GetClassProperties(modelItem);
+    if (classProperties.Count == 0)
+    {
+      return properties;
+    }
+
+    var propertyDetailLevel = settingsStore.Current.User.PropertyDetailLevel;
+    if (propertyDetailLevel == PropertyDetailLevel.Essential)
+    {
+      foreach (var kvp in classProperties)
+      {
+        if (!s_essentialClassProperties.Contains(kvp.Key))
+        {
+          continue;
+        }
+
+        properties[kvp.Key] = kvp.Value;
+      }
+
+      return properties;
+    }
+
+    foreach (var kvp in classProperties)
+    {
+      properties[kvp.Key] = kvp.Value;
+    }
+
+    return properties;
+  }
 }
