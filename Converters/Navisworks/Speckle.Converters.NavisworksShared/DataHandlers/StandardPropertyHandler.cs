ed08fd5963c4d0ce20cc5afda6232d49cb80d03b
Jonathon Broughton <jonathon@stardotbmp.com>
2026-05-17 16:56:30 +0100

    support internal properties in standard handler

--- Converters/Navisworks/Speckle.Converters.NavisworksShared/DataHandlers/StandardPropertyHandler.cs
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/DataHandlers/StandardPropertyHandler.cs
@@ -1,12 +1,17 @@
-﻿namespace Speckle.Converter.Navisworks.ToSpeckle.PropertyHandlers;
+﻿using Speckle.Converter.Navisworks.Settings;
+using Speckle.Converters.Common;
+
+namespace Speckle.Converter.Navisworks.ToSpeckle.PropertyHandlers;
 
 /// <summary>
 /// Handles standard property assignment without any merging or hierarchy processing.
 /// </summary>
 public class StandardPropertyHandler(
   PropertySetsExtractor propertySetsExtractor,
-  ModelPropertiesExtractor modelPropertiesExtractor
-) : BasePropertyHandler(propertySetsExtractor, modelPropertiesExtractor)
+  ModelPropertiesExtractor modelPropertiesExtractor,
+  InternalPropertiesExtractor internalPropertiesExtractor,
+  IConverterSettingsStore<NavisworksConversionSettings> settingsStore
+) : BasePropertyHandler(propertySetsExtractor, modelPropertiesExtractor, internalPropertiesExtractor, settingsStore)
 {
   public override Dictionary<string, object?> GetProperties(NAV.ModelItem modelItem) => ProcessPropertySets(modelItem);
 }
