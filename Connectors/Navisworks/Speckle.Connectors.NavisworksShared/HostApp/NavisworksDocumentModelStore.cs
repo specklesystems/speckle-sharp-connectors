882afea1583220e2f6888c63f062b8cda31bbecb
Jonathon Broughton <jonathon@stardotbmp.com>
2026-05-17 17:54:59 +0100

    introduce threading context

--- Connectors/Navisworks/Speckle.Connectors.NavisworksShared/HostApp/NavisworksDocumentModelStore.cs
+++ Connectors/Navisworks/Speckle.Connectors.NavisworksShared/HostApp/NavisworksDocumentModelStore.cs
@@ -1,5 +1,6 @@
 using System.Data;
 using Microsoft.Extensions.Logging;
+using Speckle.Connectors.Common.Threading;
 using Speckle.Connectors.DUI.Bridge;
 using Speckle.Connectors.DUI.Models;
 using Speckle.Connectors.DUI.Utils;
@@ -17,16 +18,19 @@ public sealed class NavisworksDocumentModelStore : DocumentModelStore
   private const string KEY_NAME = "Speckle_DUI3";
 
   private readonly ITopLevelExceptionHandler _topLevelExceptionHandler;
+  private readonly IThreadContext _threadContext;
   private string _lastSavedState = string.Empty;
 
   public NavisworksDocumentModelStore(
     ILogger<DocumentModelStore> logger,
     IJsonSerializer jsonSerializer,
-    ITopLevelExceptionHandler topLevelExceptionHandler
+    ITopLevelExceptionHandler topLevelExceptionHandler,
+    IThreadContext threadContext
   )
     : base(logger, jsonSerializer)
   {
     _topLevelExceptionHandler = topLevelExceptionHandler;
+    _threadContext = threadContext;
     LoadState();
   }
 
