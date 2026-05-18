7a0ba4388878f6a9b29a373421e1437229b88d8d
Jonathon Broughton <jonathon@stardotbmp.com>
2026-05-18 18:42:05 +0100

    implement RoundMeshVertexDoublesSetting class

---
+++ Connectors/Navisworks/Speckle.Connectors.NavisworksShared/Operations/Send/Settings/RoundMeshVertexDoublesSetting.cs
@@ -0,0 +1,14 @@
+using Speckle.Connectors.DUI.Settings;
+
+namespace Speckle.Connector.Navisworks.Operations.Send.Settings;
+
+public class RoundMeshVertexDoublesSetting(bool value) : ICardSetting
+{
+  public string? Id { get; set; } = "roundMeshVertexDoubles";
+  public string? Title { get; set; } = "Round Mesh Vertex Doubles";
+  public string? Description { get; set; } =
+    "QA/QC toggle for unit-aware vertex rounding before serialization.";
+  public string? Type { get; set; } = "boolean";
+  public List<string>? Enum { get; set; }
+  public object? Value { get; set; } = value;
+}
