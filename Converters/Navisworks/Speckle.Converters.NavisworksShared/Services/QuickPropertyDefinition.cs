5b040eb941a8108274a8ec7575e5fab5faeda216
Jonathon Broughton <jonathon@stardotbmp.com>
2026-05-31 14:58:07 +0100

    add QuickPropertyDefinition class

---
+++ Converters/Navisworks/Speckle.Converters.NavisworksShared/Services/QuickPropertyDefinition.cs
@@ -0,0 +1,25 @@
+namespace Speckle.Converter.Navisworks.Services;
+
+public sealed class QuickPropertyDefinition
+{
+  public QuickPropertyDefinition(
+    string categoryDisplayName,
+    string propertyDisplayName,
+    string? categoryInternalName,
+    string? propertyInternalName
+  )
+  {
+    CategoryDisplayName = categoryDisplayName;
+    PropertyDisplayName = propertyDisplayName;
+    CategoryInternalName = categoryInternalName;
+    PropertyInternalName = propertyInternalName;
+  }
+
+  public string CategoryDisplayName { get; }
+
+  public string PropertyDisplayName { get; }
+
+  public string? CategoryInternalName { get; }
+
+  public string? PropertyInternalName { get; }
+}
