13ebbf36e093305ba52177dea8b0f8d5cde27def
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2026-09-07 14:28:17 +0100

    Bump STJ to avoid dll conflict in Navisworks

--- Build/Consts.cs
+++ Build/Consts.cs
@@ -6,50 +6,50 @@ internal static class Consts
 
   public static readonly ProjectGroup[] ProjectGroups =
   {
-    new(
-      "rhino",
-      [
-        new("Connectors/Rhino/Speckle.Connectors.Rhino7", "net48"),
-        new("Connectors/Rhino/Speckle.Connectors.Rhino8", "net48"),
-      ]
-    ),
-    new(
-      "revit",
-      [
-        new("Connectors/Revit/Speckle.Connectors.Revit2023", "net48"),
-        new("Connectors/Revit/Speckle.Connectors.Revit2024", "net48"),
-        new("Connectors/Revit/Speckle.Connectors.Revit2025", "net8.0-windows"),
-        new("Connectors/Revit/Speckle.Connectors.Revit2026", "net8.0-windows"),
-        new("Connectors/Revit/Speckle.Connectors.Revit2027", "net10.0-windows"),
-      ]
-    ),
-    new(
-      "autocad",
-      [
-        new("Connectors/Autocad/Speckle.Connectors.Autocad2023", "net48"),
-        new("Connectors/Autocad/Speckle.Connectors.Autocad2024", "net48"),
-        new("Connectors/Autocad/Speckle.Connectors.Autocad2025", "net8.0-windows"),
-        new("Connectors/Autocad/Speckle.Connectors.Autocad2026", "net8.0-windows"),
-        new("Connectors/Autocad/Speckle.Connectors.Autocad2027", "net10.0-windows"),
-      ]
-    ),
-    new(
-      "civil3d",
-      [
-        new("Connectors/Autocad/Speckle.Connectors.Civil3d2023", "net48"),
-        new("Connectors/Autocad/Speckle.Connectors.Civil3d2024", "net48"),
-        new("Connectors/Autocad/Speckle.Connectors.Civil3d2025", "net8.0-windows"),
-        new("Connectors/Autocad/Speckle.Connectors.Civil3d2026", "net8.0-windows"),
-        new("Connectors/Autocad/Speckle.Connectors.Civil3d2027", "net10.0-windows"),
-      ]
-    ),
-    new(
-      "plant3d",
-      [
-        new("Connectors/Autocad/Speckle.Connectors.Plant3d2026", "net8.0-windows"),
-        new("Connectors/Autocad/Speckle.Connectors.Plant3d2027", "net10.0-windows"),
-      ]
-    ),
+    // new(
+    //   "rhino",
+    //   [
+    //     new("Connectors/Rhino/Speckle.Connectors.Rhino7", "net48"),
+    //     new("Connectors/Rhino/Speckle.Connectors.Rhino8", "net48"),
+    //   ]
+    // ),
+    // new(
+    //   "revit",
+    //   [
+    //     new("Connectors/Revit/Speckle.Connectors.Revit2023", "net48"),
+    //     new("Connectors/Revit/Speckle.Connectors.Revit2024", "net48"),
+    //     new("Connectors/Revit/Speckle.Connectors.Revit2025", "net8.0-windows"),
+    //     new("Connectors/Revit/Speckle.Connectors.Revit2026", "net8.0-windows"),
+    //     new("Connectors/Revit/Speckle.Connectors.Revit2027", "net10.0-windows"),
+    //   ]
+    // ),
+    // new(
+    //   "autocad",
+    //   [
+    //     new("Connectors/Autocad/Speckle.Connectors.Autocad2023", "net48"),
+    //     new("Connectors/Autocad/Speckle.Connectors.Autocad2024", "net48"),
+    //     new("Connectors/Autocad/Speckle.Connectors.Autocad2025", "net8.0-windows"),
+    //     new("Connectors/Autocad/Speckle.Connectors.Autocad2026", "net8.0-windows"),
+    //     new("Connectors/Autocad/Speckle.Connectors.Autocad2027", "net10.0-windows"),
+    //   ]
+    // ),
+    // new(
+    //   "civil3d",
+    //   [
+    //     new("Connectors/Autocad/Speckle.Connectors.Civil3d2023", "net48"),
+    //     new("Connectors/Autocad/Speckle.Connectors.Civil3d2024", "net48"),
+    //     new("Connectors/Autocad/Speckle.Connectors.Civil3d2025", "net8.0-windows"),
+    //     new("Connectors/Autocad/Speckle.Connectors.Civil3d2026", "net8.0-windows"),
+    //     new("Connectors/Autocad/Speckle.Connectors.Civil3d2027", "net10.0-windows"),
+    //   ]
+    // ),
+    // new(
+    //   "plant3d",
+    //   [
+    //     new("Connectors/Autocad/Speckle.Connectors.Plant3d2026", "net8.0-windows"),
+    //     new("Connectors/Autocad/Speckle.Connectors.Plant3d2027", "net10.0-windows"),
+    //   ]
+    // ),
     new(
       "navisworks",
       [
@@ -63,23 +63,23 @@ internal static class Consts
         new("Connectors/Navisworks/Speckle.Connectors.Navisworks2027", "net48"),
       ]
     ),
-    new(
-      "teklastructures",
-      [
-        new("Connectors/Tekla/Speckle.Connector.Tekla2023", "net48"),
-        new("Connectors/Tekla/Speckle.Connector.Tekla2024", "net48"),
-        new("Connectors/Tekla/Speckle.Connector.Tekla2025", "net48"),
-      ]
-    ),
-    new(
-      "etabs",
-      [
-        new("Connectors/CSi/Speckle.Connectors.ETABS21", "net48"),
-        new("Connectors/CSi/Speckle.Connectors.ETABS22", "net8.0-windows"),
-        new("Connectors/CSi/Speckle.Connectors.ETABS23", "net8.0-windows"),
-      ]
-    ),
-    new("rhino-importer", [new("Importers/Rhino/Speckle.Importers.JobProcessor", "net8.0-windows")]),
+    // new(
+    //   "teklastructures",
+    //   [
+    //     new("Connectors/Tekla/Speckle.Connector.Tekla2023", "net48"),
+    //     new("Connectors/Tekla/Speckle.Connector.Tekla2024", "net48"),
+    //     new("Connectors/Tekla/Speckle.Connector.Tekla2025", "net48"),
+    //   ]
+    // ),
+    // new(
+    //   "etabs",
+    //   [
+    //     new("Connectors/CSi/Speckle.Connectors.ETABS21", "net48"),
+    //     new("Connectors/CSi/Speckle.Connectors.ETABS22", "net8.0-windows"),
+    //     new("Connectors/CSi/Speckle.Connectors.ETABS23", "net8.0-windows"),
+    //   ]
+    // ),
+    // new("rhino-importer", [new("Importers/Rhino/Speckle.Importers.JobProcessor", "net8.0-windows")]),
   };
 }
 
