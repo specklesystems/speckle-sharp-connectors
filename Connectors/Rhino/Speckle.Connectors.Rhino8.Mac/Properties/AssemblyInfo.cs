a180bf461eed0bdc24531bc82e18e28992762520
Jonathon Broughton <jonathon@stardotbmp.com>
2026-09-11 23:55:13 +0100

    Set assembly metadata for macOS connector

---
+++ Connectors/Rhino/Speckle.Connectors.Rhino8.Mac/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Resources;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+[assembly: PlugInDescription(DescriptionType.Address, "")]
+[assembly: PlugInDescription(DescriptionType.Country, "")]
+[assembly: PlugInDescription(DescriptionType.Email, "hello@speckle.systems")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Fax, "")]
+[assembly: PlugInDescription(DescriptionType.Organization, "Speckle Systems Ltd.")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "https://speckle.systems")]
+
+// Same plug-in id as the Windows head: one Speckle plug-in, two platform builds.
+[assembly: Guid("2153799A-0CEC-40DE-BC3A-01E5055222FF")]
+
+[assembly: NeutralResourcesLanguage("en")]
