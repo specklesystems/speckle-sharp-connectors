a8d9f02c43e6d03d587112f3987d080ffefacda3
Jonathon Broughton <jonathon@stardotbmp.com>
2026-09-12 00:24:31 +0100

    Add Rhino8.Mac connector to Consts.cs

--- Build/Consts.cs
+++ Build/Consts.cs
@@ -12,6 +12,7 @@ internal static class Consts
         new("Connectors/Rhino/Speckle.Connectors.Rhino7", "net48"),
         new("Connectors/Rhino/Speckle.Connectors.Rhino8", "net48"),
         new("Connectors/Rhino/Speckle.Connectors.Grasshopper8.Mac", "net8.0"),
+        new("Connectors/Rhino/Speckle.Connectors.Rhino8.Mac", "net8.0"),
       ]
     ),
     new(
