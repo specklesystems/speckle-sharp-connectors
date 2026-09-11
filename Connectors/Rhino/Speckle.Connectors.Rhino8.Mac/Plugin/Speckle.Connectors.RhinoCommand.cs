668a2d6809f91d88c2d94de48ef9b7abe5dbf834
Jonathon Broughton <jonathon@stardotbmp.com>
2026-09-11 23:55:13 +0100

    Introduce Speckle command integration for Rhino macOS

---
+++ Connectors/Rhino/Speckle.Connectors.Rhino8.Mac/Plugin/Speckle.Connectors.RhinoCommand.cs
@@ -0,0 +1,98 @@
+using Rhino;
+using Rhino.Commands;
+using Rhino.Input.Custom;
+using Rhino.UI;
+using Speckle.Connectors.Rhino.HostApp;
+
+namespace Speckle.Connectors.Rhino.Plugin;
+
+public class SpeckleConnectorsRhinoCommand : Command
+{
+  public SpeckleConnectorsRhinoCommand()
+  {
+    // Rhino only creates one instance of each command class defined in a
+    // plug-in, so it is safe to store a reference in a static property.
+    Instance = this;
+    // macOS head of the Windows command (Speckle.Connectors.RhinoShared/Plugin): Eto panel, no WPF reinit dance.
+    // TODO(rhino-mac): ship a proper panel icon; SystemIcons is served by McNeel's System.Drawing shim.
+    Panels.RegisterPanel(
+      SpeckleConnectorsRhinoPlugin.Instance,
+      typeof(SpeckleRhinoPanelHost),
+      "Speckle",
+      SystemIcons.Application,
+      PanelType.System
+    );
+  }
+
+  ///<summary>The only instance of this command.</summary>
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+  public static SpeckleConnectorsRhinoCommand Instance { get; private set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+  ///<returns>The command name as it appears on the Rhino command line.</returns>
+  public override string EnglishName => "Speckle";
+
+  protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+  {
+    Guid panelId = typeof(SpeckleRhinoPanelHost).GUID;
+
+    if (mode == RunMode.Interactive)
+    {
+      Panels.OpenPanel(panelId);
+      return Result.Success;
+    }
+
+    bool panelVisible = Panels.IsPanelVisible(panelId);
+
+    string prompt = panelVisible
+      ? "SpeckleWebUIWebView2 panel is visible. New value"
+      : "SpeckleWebUIWebView2 panel is hidden. New value";
+
+    using GetOption go = new();
+    go.SetCommandPrompt(prompt);
+    int hideIndex = go.AddOption("Hide");
+    int showIndex = go.AddOption("Show");
+    int toggleIndex = go.AddOption("Toggle");
+    go.Get();
+
+    if (go.CommandResult() != Result.Success)
+    {
+      return go.CommandResult();
+    }
+
+    CommandLineOption option = go.Option();
+    if (null == option)
+    {
+      return Result.Failure;
+    }
+
+    int index = option.Index;
+    if (index == hideIndex)
+    {
+      if (panelVisible)
+      {
+        Panels.ClosePanel(panelId);
+      }
+    }
+    else if (index == showIndex)
+    {
+      if (!panelVisible)
+      {
+        Panels.OpenPanel(panelId);
+      }
+    }
+    else if (index == toggleIndex)
+    {
+      switch (panelVisible)
+      {
+        case true:
+          Panels.ClosePanel(panelId);
+          break;
+        default:
+          Panels.OpenPanel(panelId);
+          break;
+      }
+    }
+    return Result.Success;
+  }
+}
